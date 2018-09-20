bc8b1678f4b0f331065278ebb00acbc55d267afe
Evan You <yyx990803@gmail.com>
2018-09-19 21:52:07 -0400

    workflow: allow build as commit type

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -6,7 +6,7 @@ const msg = require('fs')
   .readFileSync(msgPath, 'utf-8')
   .trim()
 
-const commitRE = /^(revert: )?(feat|fix|docs|style|refactor|perf|test|workflow|ci|chore|types)(\(.+\))?: .{1,50}/
+const commitRE = /^(revert: )?(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|types)(\(.+\))?: .{1,50}/
 
 if (!commitRE.test(msg)) {
   console.log()
