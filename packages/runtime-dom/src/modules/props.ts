13f506765696446df2093bd703d68dd700ca8a54
Evan You <yyx990803@gmail.com>
2019-08-21 16:17:02 -0400

    chore: comments

--- packages/runtime-dom/src/modules/props.ts
+++ packages/runtime-dom/src/modules/props.ts
@@ -2,6 +2,9 @@ export function patchDOMProp(
   el: any,
   key: string,
   value: any,
+  // the next 3 args are passed only due to potential innerHTML/textContent
+  // overriding existing VNodes, in which case the old tree must be properly
+  // unmounted.
   prevChildren: any,
   parentComponent: any,
   unmountChildren: any
