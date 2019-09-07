ca265650663903b743d789188ca5eb08438a461b
Haoqun Jiang <haoqunjiang@gmail.com>
2019-09-07 22:41:34 +0800

    test: [wip] add tests for rendererChildren

--- packages/runtime-core/__tests__/rendererChildren.spec.ts
+++ packages/runtime-core/__tests__/rendererChildren.spec.ts
@@ -1,7 +1,52 @@
 // reference: https://github.com/vuejs/vue/blob/dev/test/unit/modules/vdom/patch/children.spec.js
+import { h, render, nodeOps, TestElement } from '@vue/runtime-test'
 
-describe('renderer: unkeyed children', () => {
-  test.todo('append')
+function toSpan(content: any) {
+  if (typeof content === 'string') {
+    return h('span', content.toString())
+  } else {
+    return h('span', { key: content }, content.toString())
+  }
+}
+
+it('should patch previously empty children', () => {
+  const root = nodeOps.createElement('div')
+  render(h('div', []), root)
+
+  let elm = root.children[0] as TestElement
+  expect(elm.children.length).toBe(0)
+
+  render(h('div', ['hello']), root)
+  expect(elm.children.length).toBe(1)
+})
+
+it('should patch previously null children', () => {
+  const root = nodeOps.createElement('div')
+  render(h('div'), root)
+
+  let elm = root.children[0] as TestElement
+  expect(elm.children.length).toBe(0)
+
+  render(h('div', ['hello']), root)
+  expect(elm.children.length).toBe(1)
+})
+
+describe('renderer: keyed children', () => {
+  const root = nodeOps.createElement('div')
+
+  beforeEach(() => {
+    render(h('div', { id: 1 }, 'hello'), root)
+  })
+
+  test('append', () => {
+    render(h('div', { id: 1 }, [1].map(toSpan)), root)
+    let elm = root.children[0] as TestElement
+    expect(elm.children.length).toBe(1)
+
+    render(h('div', { id: 1 }, [5, 6, 7].map(toSpan)), root)
+    elm = root.children[0] as TestElement
+    expect(elm.children.length).toBe(3)
+  })
 
   test.todo('prepend')
 
@@ -34,4 +79,4 @@ describe('renderer: unkeyed children', () => {
   test.todo('should not de-opt when both head and tail change')
 })
 
-describe('renderer: keyed children', () => {})
+describe('renderer: unkeyed children', () => {})
