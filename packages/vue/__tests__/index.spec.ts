7d1ed5b2b9f6512ad12fd32f66c56bea3a376324
Miguel Elizondo <miguelaea015@gmail.com>
2019-10-04 14:32:55 -0700

    remove the return sentence

--- packages/vue/__tests__/index.spec.ts
+++ packages/vue/__tests__/index.spec.ts
@@ -7,11 +7,9 @@ it('should support on-the-fly template compilation', () => {
   const container = document.createElement('div')
   const App = {
     template: `{{ count }}`,
-    data() {
-      return {
-        count: 0
-      }
-    }
+    data: () => ({
+      count: 0
+    })
   }
   createApp().mount(App, container)
   expect(container.innerHTML).toBe(`0`)
