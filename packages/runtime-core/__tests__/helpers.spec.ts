1b7404f3a5e7292d1bf13748ba7125eaaba7bc02
Jordan Pittman <jordan@cryptica.me>
2019-10-05 00:19:54 -0400

    test: Add test for runtime helper renderList
    This acts a generalized forEach that works on arrays, strings, numbers (presented as 1…N),  objects (one entry per key), and general iterables.

--- packages/runtime-core/__tests__/helpers.spec.ts
+++ packages/runtime-core/__tests__/helpers.spec.ts
@@ -1,4 +1,4 @@
-import { toHandlers } from '@vue/runtime-core'
+import { toHandlers, renderList } from '@vue/runtime-core'
 import { mockWarn } from '@vue/runtime-test'
 
 describe('toHandlers', () => {
@@ -21,5 +21,48 @@ describe('toHandlers', () => {
       oninput: input,
       onchange: change
     })
+  })
+})
+
+describe('renderList', () => {
+  it('should render nodes in an array', () => {
+    expect(
+      renderList(['1', '2', '3'], (item, index) => `node ${index}: ${item}`)
+    ).toEqual(['node 0: 1', 'node 1: 2', 'node 2: 3'])
+  })
+
+  it('should render characters of a string', () => {
+    expect(
+      renderList('123', (item, index) => `node ${index}: ${item}`)
+    ).toEqual(['node 0: 1', 'node 1: 2', 'node 2: 3'])
+  })
+
+  it('should render N+1 items when given a number', () => {
+    expect(renderList(3, (item, index) => `node ${index}: ${item}`)).toEqual([
+      'node 0: 1',
+      'node 1: 2',
+      'node 2: 3'
+    ])
+  })
+
+  it('should render an item for every key in an object', () => {
+    expect(
+      renderList(
+        { a: 1, b: 2, c: 3 },
+        (item, key, index) => `node ${index}/${key}: ${item}`
+      )
+    ).toEqual(['node 0/a: 1', 'node 1/b: 2', 'node 2/c: 3'])
+  })
+
+  it('should render an item for entry in an iterable', () => {
+    const iterable = function*() {
+      yield 1
+      yield 2
+      yield 3
+    }
+
+    expect(
+      renderList(iterable(), (item, index) => `node ${index}: ${item}`)
+    ).toEqual(['node 0: 1', 'node 1: 2', 'node 2: 3'])
   })
 })
