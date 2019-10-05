8c0288f50e7ebd1bbf831c5b56f0c1d1bae46439
Rahul Kadyan <hi@znck.me>
2019-10-06 02:46:44 +0530

    test(reactivity): isRef works with truthy primitives

--- packages/reactivity/__tests__/ref.spec.ts
+++ packages/reactivity/__tests__/ref.spec.ts
@@ -78,6 +78,7 @@ describe('reactivity/ref', () => {
     expect(isRef(computed(() => 1))).toBe(true)
 
     expect(isRef(0)).toBe(false)
+    expect(isRef(1)).toBe(false)
     // an object that looks like a ref isn't necessarily a ref
     expect(isRef({ value: 0 })).toBe(false)
   })
