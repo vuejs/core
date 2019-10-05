fc958ee2b107cb3a1e40c59dd5435c824decb9ab
Miguel Elizondo <miguelaea015@gmail.com>
2019-10-05 07:12:46 -0700

    refactor: add return statement

--- packages/vue/__tests__/index.spec.ts
+++ packages/vue/__tests__/index.spec.ts
@@ -7,9 +7,11 @@ it('should support on-the-fly template compilation', () => {
   const container = document.createElement('div')
   const App = {
     template: `{{ count }}`,
-    data: () => ({
-      count: 0
-    })
+    data() {
+      return {
+        count: 0
+      }
+    }
   }
   createApp().mount(App, container)
   expect(container.innerHTML).toBe(`0`)
