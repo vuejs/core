6f48afa5c27752d5278294145d2e8eae902af5ce
Kyle Hall <hallkd47@gmail.com>
2019-10-05 15:01:34 -0500

    test(runtime-core): figured out Record but still do not understand Slot

--- packages/runtime-core/__tests__/helpers/createSlots.spec.ts
+++ packages/runtime-core/__tests__/helpers/createSlots.spec.ts
@@ -1,10 +1,13 @@
+import { Slot } from '../../src/componentSlots'
 import { createSlots } from '../../src/helpers/createSlots'
+import { createVNode } from 'vue'
 
 describe('createSlot', () => {
   it('should return a slot', () => {
-    const slot = () => [{}]
-    const slotDescriptor = 'descriptor'
-    const actual = createSlots(slot, slotDescriptor)
+    const record: Record<string, Slot> = {}
+    const slot = () => createVNode(Object)
+    const slotDescriptor = [{ name: 'descriptor', fn: slot }]
+    const actual = createSlots(record, slotDescriptor)
 
     expect(actual).toHaveLength(1)
   })
