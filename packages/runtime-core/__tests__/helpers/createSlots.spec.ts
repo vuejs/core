5916db961168e3e0d8c9420f2d28feaf6101298e
Kyle Hall <hallkd47@gmail.com>
2019-10-05 17:21:44 -0500

    test(runtime-core): add test for single, non-array dynamic slot

--- packages/runtime-core/__tests__/helpers/createSlots.spec.ts
+++ packages/runtime-core/__tests__/helpers/createSlots.spec.ts
@@ -1,14 +1,13 @@
 import { Slot } from '../../src/componentSlots'
 import { createSlots } from '../../src/helpers/createSlots'
-import { createVNode } from 'vue'
 
 describe('createSlot', () => {
   it('should return a slot', () => {
     const record: Record<string, Slot> = {}
-    const slot = () => createVNode(Object)
-    const slotDescriptor = [{ name: 'descriptor', fn: slot }]
-    const actual = createSlots(record, slotDescriptor)
+    const slot = () => []
+    const dynamicSlot = [{ name: 'descriptor', fn: slot }]
+    const actual = createSlots(record, dynamicSlot)
 
-    expect(actual).toHaveLength(1)
+    expect(actual).toEqual({ descriptor: slot })
   })
 })
