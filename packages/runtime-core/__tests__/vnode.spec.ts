9de3a86f8733fff00282dea3aae546ae2dfc4aa9
Tim van den Eijnden <tvandeneijnden@libertyglobal.com>
2019-10-05 21:50:05 +0200

    test(vnode): first set of testss

--- packages/runtime-core/__tests__/vnode.spec.ts
+++ packages/runtime-core/__tests__/vnode.spec.ts
@@ -1,15 +1,49 @@
+import { createVNode } from '@vue/runtime-test'
+
 describe('vnode', () => {
-  test.todo('create with just tag')
+  test('create with just tag', () => {
+    const vnode = createVNode('p')
+    expect(vnode.type).toBe('p')
+    expect(vnode.props).toBe(null)
+  });
 
-  test.todo('create with tag and props')
+  test('create with tag and props', () => {
+    const vnode = createVNode('p', {})
+    expect(vnode.type).toBe('p')
+    expect(vnode.props).toMatchObject({})
+  })
 
-  test.todo('create with tag, props and children')
+  test('create with tag, props and children', () => {
+    const vnode = createVNode('p', {}, ['foo'])
+    expect(vnode.type).toBe('p')
+    expect(vnode.props).toMatchObject({})
+    expect(vnode.children).toMatchObject(["foo"])
+  })
 
-  test.todo('create with 0 as props')
+  test('create with 0 as props', () => {
+    const vnode = createVNode('p', null)
+    expect(vnode.type).toBe('p')
+    expect(vnode.props).toBe(null)
+  })
 
-  test.todo('class normalization')
+  test('class normalization', () => {
+    let vnode = createVNode('p', { class: 'foo baz'})
+    expect(vnode.props).toMatchObject({ class: 'foo baz'})
 
-  test.todo('style normalization')
+    vnode = createVNode('p', { class: ['foo', 'baz']})
+    expect(vnode.props).toMatchObject({ class: 'foo baz'})
+
+    vnode = createVNode('p', { class: {foo: 'foo', baz: 'baz'}})
+    expect(vnode.props).toMatchObject({ class: 'foo baz'})
+  })
+
+  test('style normalization', () => {
+    let vnode = createVNode('p', { style: [{ foo: 'foo'}, { baz: 'baz'}]})
+    expect(vnode.props).toMatchObject({ style: { foo: 'foo', baz: 'baz'}})
+
+    vnode = createVNode('p', { style: {foo: 'foo', baz: 'baz'}})
+    expect(vnode.props).toMatchObject({ style: { foo: 'foo', baz: 'baz'}})
+  })
 
   test.todo('children normalization')
 
