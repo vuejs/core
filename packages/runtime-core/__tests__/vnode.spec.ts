3ed1e68a227972243c887b73f55bce2c1025a81e
Tim van den Eijnden <tvandeneijnden@libertyglobal.com>
2019-10-06 08:02:05 +0200

    test(node): breakdown into multiple tests

--- packages/runtime-core/__tests__/vnode.spec.ts
+++ packages/runtime-core/__tests__/vnode.spec.ts
@@ -5,7 +5,7 @@ describe('vnode', () => {
     const vnode = createVNode('p')
     expect(vnode.type).toBe('p')
     expect(vnode.props).toBe(null)
-  });
+  })
 
   test('create with tag and props', () => {
     const vnode = createVNode('p', {})
@@ -17,7 +17,7 @@ describe('vnode', () => {
     const vnode = createVNode('p', {}, ['foo'])
     expect(vnode.type).toBe('p')
     expect(vnode.props).toMatchObject({})
-    expect(vnode.children).toMatchObject(["foo"])
+    expect(vnode.children).toMatchObject(['foo'])
   })
 
   test('create with 0 as props', () => {
@@ -26,23 +26,35 @@ describe('vnode', () => {
     expect(vnode.props).toBe(null)
   })
 
-  test('class normalization', () => {
-    let vnode = createVNode('p', { class: 'foo baz'})
-    expect(vnode.props).toMatchObject({ class: 'foo baz'})
+  describe('class normalization', () => {
+    test('string', () => {
+      const vnode = createVNode('p', { class: 'foo baz' })
+      expect(vnode.props).toMatchObject({ class: 'foo baz' })
+    })
 
-    vnode = createVNode('p', { class: ['foo', 'baz']})
-    expect(vnode.props).toMatchObject({ class: 'foo baz'})
+    test('array', () => {
+      const vnode = createVNode('p', { class: ['foo', 'baz'] })
+      expect(vnode.props).toMatchObject({ class: 'foo baz' })
+    })
 
-    vnode = createVNode('p', { class: {foo: 'foo', baz: 'baz'}})
-    expect(vnode.props).toMatchObject({ class: 'foo baz'})
+    test('object', () => {
+      const vnode = createVNode('p', { class: { foo: 'foo', baz: 'baz' } })
+      expect(vnode.props).toMatchObject({ class: 'foo baz' })
+    })
   })
 
-  test('style normalization', () => {
-    let vnode = createVNode('p', { style: [{ foo: 'foo'}, { baz: 'baz'}]})
-    expect(vnode.props).toMatchObject({ style: { foo: 'foo', baz: 'baz'}})
+  describe('style normalization', () => {
+    test('array', () => {
+      const vnode = createVNode('p', {
+        style: [{ foo: 'foo' }, { baz: 'baz' }]
+      })
+      expect(vnode.props).toMatchObject({ style: { foo: 'foo', baz: 'baz' } })
+    })
 
-    vnode = createVNode('p', { style: {foo: 'foo', baz: 'baz'}})
-    expect(vnode.props).toMatchObject({ style: { foo: 'foo', baz: 'baz'}})
+    test('object', () => {
+      const vnode = createVNode('p', { style: { foo: 'foo', baz: 'baz' } })
+      expect(vnode.props).toMatchObject({ style: { foo: 'foo', baz: 'baz' } })
+    })
   })
 
   test.todo('children normalization')
