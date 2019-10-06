4c66e7060ec88bd175d4e512279019202717f3f8
pikax <carlos@hypermob.co.uk>
2019-10-06 08:49:25 +0100

    feat: implementing v-show transform

--- packages/compiler-dom/src/transforms/vShow.ts
+++ packages/compiler-dom/src/transforms/vShow.ts
@@ -1,1 +1,45 @@
-// TODO
+import {
+  createStructuralDirectiveTransform,
+  createCompilerError,
+  ErrorCodes,
+  createSimpleExpression,
+  SimpleExpressionNode,
+  NodeTypes,
+  createConditionalExpression,
+  createObjectExpression
+} from '@vue/compiler-core'
+
+export const transformShow = createStructuralDirectiveTransform(
+  'show',
+  (node, dir, context) => {
+    if (!dir.exp || !(dir.exp as SimpleExpressionNode).content.trim()) {
+      const loc = dir.exp ? dir.exp.loc : node.loc
+      context.onError(
+        createCompilerError(ErrorCodes.X_V_SHOW_NO_EXPRESSION, dir.loc)
+      )
+      dir.exp = createSimpleExpression(`true`, false, loc)
+    }
+
+    // v-show can't be used outside of an element
+    if (node.type !== NodeTypes.ELEMENT) {
+      createCompilerError(ErrorCodes.X_V_SHOW_UNEXPECTED_USAGE, dir.loc)
+    }
+
+    const exp = context.hoist(
+      createSimpleExpression('{"display":none}', false, dir.loc)
+    )
+
+    node.props.push({
+      type: NodeTypes.DIRECTIVE,
+      name: `bind`,
+      arg: createSimpleExpression(`style`, true, dir.loc),
+      exp: createConditionalExpression(
+        dir.exp,
+        exp,
+        createSimpleExpression('', true, dir.loc)
+      ),
+      modifiers: [],
+      loc: dir.loc
+    })
+  }
+)
