ea823e94984b298387a72064c8a9abe78edc6dfc
himself65 <himself65@outlook.com>
2019-10-06 19:46:15 +0800

    test: add test for cloneVNode

--- packages/runtime-core/__tests__/vnode.spec.ts
+++ packages/runtime-core/__tests__/vnode.spec.ts
@@ -1,3 +1,5 @@
+import { createVNode, cloneVNode } from '../src/vnode'
+
 describe('vnode', () => {
   test.todo('create with just tag')
 
@@ -17,7 +19,19 @@ describe('vnode', () => {
 
   test.todo('node type/shapeFlag inference')
 
-  test.todo('cloneVNode')
+  test('cloneNode', () => {
+    const node1 = createVNode('div', { foo: 1 }, null)
+    expect(cloneVNode(node1)).toEqual(node1)
+
+    const node2 = createVNode('div', null, [node1])
+    expect(cloneVNode(node2)).toEqual(node2)
+
+    let node = null
+    for (let i = 1; i <= 100; ++i) {
+      node = createVNode('div', null, [node])
+      expect(cloneVNode(node)).toEqual(node)
+    }
+  })
 
   test.todo('mergeProps')
 })
