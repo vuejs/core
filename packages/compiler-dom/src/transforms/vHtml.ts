616aea97877efeb4cfc53b50e8ed16638f91377e
pikax <carlos@hypermob.co.uk>
2019-10-06 10:31:32 +0100

    feat: add v-html transformer

--- packages/compiler-dom/src/transforms/vHtml.ts
+++ packages/compiler-dom/src/transforms/vHtml.ts
@@ -1,1 +1,39 @@
-// TODO
+import {
+  createStructuralDirectiveTransform,
+  createCompilerError,
+  ErrorCodes,
+  createSimpleExpression,
+  SimpleExpressionNode,
+  NodeTypes
+} from '@vue/compiler-core'
+
+export const transformHtml = createStructuralDirectiveTransform(
+  'html',
+  (node, dir, context) => {
+    if (!dir.exp || !(dir.exp as SimpleExpressionNode).content.trim()) {
+      const loc = dir.exp ? dir.exp.loc : node.loc
+      context.onError(
+        createCompilerError(ErrorCodes.X_V_HTML_NO_EXPRESSION, dir.loc)
+      )
+      dir.exp = createSimpleExpression('', true, loc)
+    }
+
+    // v-show can't be used outside of an element
+    if (node.type !== NodeTypes.ELEMENT) {
+      createCompilerError(ErrorCodes.X_V_HTML_UNEXPECTED_USAGE, dir.loc)
+    }
+
+    // add prop innerHTML
+    node.props.push({
+      type: NodeTypes.DIRECTIVE,
+      name: `bind`,
+      arg: createSimpleExpression(`innerHTML`, true, dir.loc),
+      exp: dir.exp,
+      modifiers: [],
+      loc: dir.loc
+    })
+
+    // remove all the children, since they will be overridden by the `innerHTML`
+    node.children = []
+  }
+)
