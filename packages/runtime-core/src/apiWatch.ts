80e5b16f995240a87cdfbb5da0ca3476864d6e0d
扩散性百万甜面包 <himself65@outlook.com>
2019-10-09 01:48:13 +0800

    types: improve type 'ReactiveEffect' with generics (#99)

--- packages/runtime-core/src/apiWatch.ts
+++ packages/runtime-core/src/apiWatch.ts
@@ -91,7 +91,7 @@ function doWatch(
   const instance = currentInstance
   const suspense = currentSuspense
 
-  let getter: Function
+  let getter: () => any
   if (isArray(source)) {
     getter = () =>
       source.map(
