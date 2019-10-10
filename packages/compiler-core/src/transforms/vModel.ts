8794a4ff61d9fd06a81f69d36ed9da8890c9f592
Rahul Kadyan <hi@znck.me>
2019-10-10 08:08:20 +0530

    refactor: inline default values and remove unnecessary if/else

--- packages/compiler-core/src/transforms/vModel.ts
+++ packages/compiler-core/src/transforms/vModel.ts
@@ -15,38 +15,38 @@ export const transformModel: DirectiveTransform = (dir, node, context) => {
     context.onError(createCompilerError(ErrorCodes.X_V_MODEL_NO_EXPRESSION))
 
     return createTransformProps()
-  } else if (isEmptyExpression(exp)) {
+  }
+
+  if (isEmptyExpression(exp)) {
     context.onError(
       createCompilerError(ErrorCodes.X_V_MODEL_MALFORMED_EXPRESSION)
     )
 
     return createTransformProps()
-  } else {
-    const defaultPropName = createSimpleExpression('modelValue', true)
-    const defaultEventName = createSimpleExpression('onUpdate:modelValue', true)
-    const propName = arg ? arg : defaultPropName
-    const eventName = arg
-      ? arg.type === NodeTypes.SIMPLE_EXPRESSION && arg.isStatic
-        ? createSimpleExpression('onUpdate:' + arg.content, true)
-        : createCompoundExpression([
-            createSimpleExpression('onUpdate:', true),
-            '+',
-            ...(arg.type === NodeTypes.SIMPLE_EXPRESSION ? [arg] : arg.children)
-          ])
-      : defaultEventName
-
-    return createTransformProps([
-      createObjectProperty(propName, dir.exp!),
-      createObjectProperty(
-        eventName,
-        createCompoundExpression([
-          `$event => (`,
-          ...(exp.type === NodeTypes.SIMPLE_EXPRESSION ? [exp] : exp.children),
-          ` = $event)`
-        ])
-      )
-    ])
   }
+
+  const propName = arg ? arg : createSimpleExpression('modelValue', true)
+  const eventName = arg
+    ? arg.type === NodeTypes.SIMPLE_EXPRESSION && arg.isStatic
+      ? createSimpleExpression('onUpdate:' + arg.content, true)
+      : createCompoundExpression([
+          createSimpleExpression('onUpdate:', true),
+          '+',
+          ...(arg.type === NodeTypes.SIMPLE_EXPRESSION ? [arg] : arg.children)
+        ])
+    : createSimpleExpression('onUpdate:modelValue', true)
+
+  return createTransformProps([
+    createObjectProperty(propName, dir.exp!),
+    createObjectProperty(
+      eventName,
+      createCompoundExpression([
+        `$event => (`,
+        ...(exp.type === NodeTypes.SIMPLE_EXPRESSION ? [exp] : exp.children),
+        ` = $event)`
+      ])
+    )
+  ])
 }
 
 function createTransformProps(props: Property[] = []) {
