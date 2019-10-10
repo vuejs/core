899cd0b52c922e84cebcfc25314c9fc852e6a673
pikax <david-181@hotmail.com>
2019-10-10 21:44:04 +0100

    test(compiler-dom): replace snapshot to only check code

--- packages/compiler-dom/__tests__/index.spec.ts
+++ packages/compiler-dom/__tests__/index.spec.ts
@@ -2,12 +2,12 @@ import { compile } from '../src'
 
 describe('compile', () => {
   it('should contain standard transforms', () => {
-    const result = compile(`<div v-text="text"></div>
+    const { code } = compile(`<div v-text="text"></div>
         <div v-html="html"></div>
         <div v-cloak>test</div>
         <div style="color=red">red</div>
         <div :style="{color: 'green'}"></div>`)
 
-    expect(result).toMatchSnapshot()
+    expect(code).toMatchSnapshot()
   })
 })
