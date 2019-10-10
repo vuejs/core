e21405c5bb337bc1722bc870b2e91e332ea5ab1f
唐道海 <tangdaohai@outlook.com>
2019-10-10 14:47:10 +0800

    test(effect): add test for lazy option

--- packages/reactivity/__tests__/effect.spec.ts
+++ packages/reactivity/__tests__/effect.spec.ts
@@ -505,15 +505,13 @@ describe('reactivity/effect', () => {
     expect(dummy).toBe(1)
   })
 
-
   it('lazy', () => {
     const obj = reactive({ foo: 1 })
     let dummy
-    const runner = effect(() => {
-      dummy = obj.foo
-    }, { lazy: true })
+    const runner = effect(() => (dummy = obj.foo), { lazy: true })
     expect(dummy).toBe(undefined)
-    runner()
+
+    expect(runner()).toBe(1)
     expect(dummy).toBe(1)
     obj.foo = 2
     expect(dummy).toBe(2)
