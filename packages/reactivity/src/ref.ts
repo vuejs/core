9551b9fd1c0342cbdf701e42d46f261d7735e11a
ChesterBu <291164091@qq.com>
2019-10-11 19:34:00 +0800

    refactor: use void 0 instead of undefined

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -3,7 +3,7 @@ import { OperationTypes } from './operations'
 import { isObject } from '@vue/shared'
 import { reactive } from './reactive'
 
-export const refSymbol = Symbol(__DEV__ ? 'refSymbol' : undefined)
+export const refSymbol = Symbol(__DEV__ ? 'refSymbol' : void 0)
 
 export interface Ref<T = any> {
   [refSymbol]: true
