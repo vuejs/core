a7eefaff1ed8954cf36d9a2417538849d2d15445
Evan You <yyx990803@gmail.com>
2019-10-12 19:25:04 -0400

    refactor(runtime-dom): remove xlink support since it's been deprecated

--- packages/runtime-dom/src/modules/attrs.ts
+++ packages/runtime-dom/src/modules/attrs.ts
@@ -1,31 +1,7 @@
-const xlinkNS = 'http://www.w3.org/1999/xlink'
-
-function isXlink(name: string): boolean {
-  return name.charAt(5) === ':' && name.slice(0, 5) === 'xlink'
-}
-
-function getXlinkProp(name: string): string {
-  return isXlink(name) ? name.slice(6, name.length) : ''
-}
-
-export function patchAttr(
-  el: Element,
-  key: string,
-  value: any,
-  isSVG: boolean
-) {
-  // isSVG short-circuits isXlink check
-  if (isSVG && isXlink(key)) {
-    if (value == null) {
-      el.removeAttributeNS(xlinkNS, getXlinkProp(key))
-    } else {
-      el.setAttributeNS(xlinkNS, key, value)
-    }
+export function patchAttr(el: Element, key: string, value: any) {
+  if (value == null) {
+    el.removeAttribute(key)
   } else {
-    if (value == null) {
-      el.removeAttribute(key)
-    } else {
-      el.setAttribute(key, value)
-    }
+    el.setAttribute(key, value)
   }
 }
