5cfb548c487c7d453e4b781303f786fe395ea0a2
fisker <lionkay@gmail.com>
2019-10-12 21:15:43 +0800

    style: use `earlier return` rewrite `style.ts`

--- packages/runtime-dom/src/modules/style.ts
+++ packages/runtime-dom/src/modules/style.ts
@@ -2,20 +2,26 @@ import { isString } from '@vue/shared'
 
 export function patchStyle(el: any, prev: any, next: any) {
   const { style } = el
+
   if (!next) {
     el.removeAttribute('style')
-  } else if (isString(next)) {
+    return
+  }
+
+  if (isString(next)) {
     style.cssText = next
-  } else {
-    for (const key in next) {
-      style[key] = next[key]
-    }
-    if (prev && !isString(prev)) {
-      for (const key in prev) {
-        if (!next[key]) {
-          style[key] = ''
-        }
-      }
+    return
+  }
+
+  Object.assign(style, next)
+
+  if (!prev || isString(prev)) {
+    return
+  }
+  
+  for (const key in prev) {
+    if (!next[key]) {
+      style[key] = ''
     }
   }
 }
