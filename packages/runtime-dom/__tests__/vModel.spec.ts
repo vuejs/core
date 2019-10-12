afb9fc7ffcb8ec67d098801b32b73499bdb9a7cf
Stas Lashmanov <stasvarenkin@gmail.com>
2019-10-13 01:47:27 +0300

    test: array model

--- packages/runtime-dom/__tests__/vModel.spec.ts
+++ packages/runtime-dom/__tests__/vModel.spec.ts
@@ -123,6 +123,80 @@ describe('vModel', () => {
     expect(input.checked).toEqual(false)
   })
 
+  it(`should support array as a checkbox model`, async () => {
+    const component = createComponent({
+      data() {
+        return { value: [] }
+      },
+      render() {
+        return [
+          withVModel(
+            h('input', {
+              type: 'checkbox',
+              class: 'foo',
+              value: 'foo',
+              vModel: this.value,
+              'onUpdate:modelValue': () => {
+                this.value.push('foo' as never)
+              }
+            })
+          ),
+          withVModel(
+            h('input', {
+              type: 'checkbox',
+              class: 'bar',
+              value: 'bar',
+              vModel: this.value,
+              'onUpdate:modelValue': () => {
+                this.value.push('bar' as never)
+              }
+            })
+          )
+        ]
+      }
+    })
+    app.mount(component, root)
+
+    const foo = root.querySelector('.foo')
+    const bar = root.querySelector('.bar')
+    const data = root._vnode.component.data
+
+    foo.checked = true
+    triggerEvent('change', foo)
+    await nextTick()
+    expect(data.value).toMatchObject(['foo'])
+
+    bar.checked = true
+    triggerEvent('change', bar)
+    await nextTick()
+    expect(data.value).toMatchObject(['foo', 'bar'])
+
+    bar.checked = false
+    triggerEvent('change', bar)
+    await nextTick()
+    expect(data.value).toMatchObject(['foo'])
+
+    foo.checked = false
+    triggerEvent('change', foo)
+    await nextTick()
+    expect(data.value).toMatchObject([])
+
+    data.value = ['foo']
+    await nextTick()
+    expect(bar.checked).toEqual(false)
+    expect(foo.checked).toEqual(true)
+
+    data.value = ['bar']
+    await nextTick()
+    expect(foo.checked).toEqual(false)
+    expect(bar.checked).toEqual(true)
+
+    data.value = []
+    await nextTick()
+    expect(foo.checked).toEqual(false)
+    expect(bar.checked).toEqual(false)
+  })
+
   it('should work with radio', async () => {
     const component = createComponent({
       data() {
