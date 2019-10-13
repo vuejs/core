013c3b10a849bbefb740018a9a6471bc8510d9f7
fnlCtrl <fnlctrl@gmail.com>
2019-10-13 22:24:29 +0800

    feat(v-on): add runtime tests (wip)

--- packages/runtime-dom/src/directives/vOn.ts
+++ packages/runtime-dom/src/directives/vOn.ts
@@ -2,25 +2,25 @@ const modifierGuards: Record<string, (e: Event) => void | boolean> = {
   stop: e => e.stopPropagation(),
   prevent: e => e.preventDefault(),
   self: e => e.target !== e.currentTarget,
-  ctrl: e => e instanceof KeyboardEvent && !e.ctrlKey,
-  shift: e => e instanceof KeyboardEvent && !e.shiftKey,
-  alt: e => e instanceof KeyboardEvent && !e.altKey,
-  meta: e => e instanceof KeyboardEvent && !e.metaKey,
-  left: e => e instanceof MouseEvent && 'button' in e && e.button !== 0,
-  middle: e => e instanceof MouseEvent && 'button' in e && e.button !== 1,
-  right: e => e instanceof MouseEvent && 'button' in e && e.button !== 2
+  ctrl: e => !(e as any).ctrlKey,
+  shift: e => !(e as any).shiftKey,
+  alt: e => !(e as any).altKey,
+  meta: e => !(e as any).metaKey,
+  left: e => 'button' in e && (e as any).button !== 0,
+  middle: e => 'button' in e && (e as any).button !== 1,
+  right: e => 'button' in e && (e as any).button !== 2
 }
 
 const keyNames: Record<string, string | string[]> = {
-  esc: 'Escape',
+  esc: 'escape',
   // IE11 uses `Spacebar` for Space key name.
-  space: [' ', 'Spacebar'],
-  up: 'ArrowUp',
-  left: 'ArrowLeft',
-  right: 'ArrowRight',
-  down: 'ArrowDown',
+  space: [' ', 'epacebar'],
+  up: 'arrowup',
+  left: 'arrowleft',
+  right: 'arrowright',
+  down: 'arrowdown',
   // IE11 uses `Del` for Delete key name.
-  delete: ['Backspace', 'Del']
+  delete: ['backspace', 'del']
 }
 
 const modifierKeys = ['ctrl', 'shift', 'alt', 'meta']
@@ -30,7 +30,7 @@ type ModifierKeysProperty = 'ctrlKey' | 'shiftKey' | 'altKey' | 'metaKey'
 
 export const vOnModifiersGuard = (fn: Function, modifiers: string[]) => {
   return (event: Event) => {
-    for (let modifier in modifiers) {
+    for (let modifier of modifiers) {
       if (
         modifier === 'exact' &&
         event instanceof KeyboardEvent &&
@@ -45,8 +45,8 @@ export const vOnModifiersGuard = (fn: Function, modifiers: string[]) => {
       const guard = modifierGuards[modifier]
       if (guard && guard(event)) return
     }
-    if (event instanceof KeyboardEvent && 'key' in event) {
-      const eventKey = event.key.toLowerCase()
+    if ('key' in event) {
+      const eventKey = (event as any).key.toLowerCase()
       const keysToMatch = modifiers.filter(m => !modifierGuards[m])
       if (
         // None of the provided key modifiers match the current event key
