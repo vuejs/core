78c0adc80df043157aadd93d380c392ad4d0175b
fisker <lionkay@gmail.com>
2019-10-13 14:39:27 +0800

    refactor: add `isIterable`

--- packages/runtime-core/src/helpers/renderList.ts
+++ packages/runtime-core/src/helpers/renderList.ts
@@ -1,32 +1,28 @@
 import { VNodeChild } from '../vnode'
 import { isArray, isString, isObject } from '@vue/shared'
 
+const isIterable = (object: any) =>
+  isArray(object) ||
+  isString(object) ||
+  (isObject(object) && object[Symbol.iterator as any])
+
 export function renderList(
   source: any,
   renderItem: (value: any, key: string | number, index?: number) => VNodeChild
 ): VNodeChild[] {
   let ret: VNodeChild[] = []
-  if (isArray(source) || isString(source)) {
-    for (let i = 0, l = source.length; i < l; i++) {
-      ret.push(renderItem(source[i], i))
-    }
+  if (isIterable(source)) {
+    ret = Array.from(source as Iterable<any>, renderItem)
   } else if (typeof source === 'number') {
     for (let i = 0; i < source; i++) {
       ret.push(renderItem(i + 1, i))
     }
   } else if (isObject(source)) {
-    if (source[Symbol.iterator as any]) {
-      ret = Array.from(
-        source as Iterable<any>, 
-        renderItem
-      )
-    } else {
-      const keys = Object.keys(source)
-      ret = new Array(keys.length)
-      for (let i = 0, l = keys.length; i < l; i++) {
-        const key = keys[i]
-        ret[i] = renderItem(source[key], key, i)
-      }
+    const keys = Object.keys(source)
+    ret = new Array(keys.length)
+    for (let i = 0, l = keys.length; i < l; i++) {
+      const key = keys[i]
+      ret[i] = renderItem(source[key], key, i)
     }
   }
   return ret
