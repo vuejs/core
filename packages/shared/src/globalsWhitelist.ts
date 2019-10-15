230f105e1174f09fb811a4023f5e9b9c27723aa8
Evan You <yyx990803@gmail.com>
2019-10-15 12:05:21 -0400

    Update globalsWhitelist.ts

--- packages/shared/src/globalsWhitelist.ts
+++ packages/shared/src/globalsWhitelist.ts
@@ -1,6 +1,8 @@
 import { makeMap } from './makeMap'
 
 const GLOBALS_WHITE_LISTED =
-  'Infinity,undefined,NaN,isFinite,isNaN,parseFloat,parseInt,decodeURI,decodeURIComponent,encodeURI,encodeURIComponent,Math,Number,Date,Array,Object,Boolean,String,RegExp,Map,Set,JSON,Intl'
+  'Infinity,undefined,NaN,isFinite,isNaN,parseFloat,parseInt,decodeURI,' +
+  'decodeURIComponent,encodeURI,encodeURIComponent,Math,Number,Date,Array,' +
+  'Object,Boolean,String,RegExp,Map,Set,JSON,Intl'
 
 export const isGloballyWhitelisted = makeMap(GLOBALS_WHITE_LISTED)
