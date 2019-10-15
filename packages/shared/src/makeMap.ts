7de3aee31798e3f2c008d4f3f4f9f25b54142eb7
Evan You <yyx990803@gmail.com>
2019-10-15 12:19:46 -0400

    fix: ensure makeMap calls are tree-shakable

--- packages/shared/src/makeMap.ts
+++ packages/shared/src/makeMap.ts
@@ -1,7 +1,8 @@
-/**
- * Make a map and return a function for checking if a key
- * is in that map.
- */
+// Make a map and return a function for checking if a key
+// is in that map.
+//
+// IMPORTANT: all calls of this function must be prefixed with /*@__PURE__*/
+// So that rollup can tree-shake them if necessary.
 export function makeMap(
   str: string,
   expectsLowerCase?: boolean
