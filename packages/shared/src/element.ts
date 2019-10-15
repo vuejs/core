7c07942e3de0d5de815b265b6313cb08b77747f8
Ayush Goyal <ayush987goyal@gmail.com>
2019-10-15 08:54:20 +0530

    refactor(shread): rename the utils to makeMap

--- packages/shared/src/element.ts
+++ packages/shared/src/element.ts
@@ -1,4 +1,4 @@
-import { makeMap } from './utils'
+import { makeMap } from './makeMap'
 
 const HTML_TAGS =
   'html,body,base,head,link,meta,style,title,address,article,aside,footer,header,h1,h2,h3,h4,h5,h6,hgroup,nav,section,div,dd,dl,dt,figcaption,figure,picture,hr,img,li,main,ol,p,pre,ul,a,b,abbr,bdi,bdo,br,cite,code,data,dfn,em,i,kbd,mark,q,rp,rt,rtc,ruby,s,samp,small,span,strong,sub,sup,time,u,var,wbr,area,audio,map,track,video,embed,object,param,source,canvas,script,noscript,del,ins,caption,col,colgroup,table,thead,tbody,td,th,tr,button,datalist,fieldset,form,input,label,legend,meter,optgroup,option,output,progress,select,textarea,details,dialog,menu,menuitem,summary,content,element,shadow,template,blockquote,iframe,tfoot'
--- packages/shared/src/globalsWhitelist.ts
+++ packages/shared/src/globalsWhitelist.ts
@@ -1,4 +1,4 @@
-import { makeMap } from './utils'
+import { makeMap } from './makeMap'
 
 const GLOBALS_WHITE_LISTED =
   'Infinity,undefined,NaN,isFinite,isNaN,parseFloat,parseInt,decodeURI,decodeURIComponent,encodeURI,encodeURIComponent,Math,Number,Date,Array,Object,Boolean,String,RegExp,Map,Set,JSON,Intl'
