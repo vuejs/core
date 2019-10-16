e739b08bfb4812f94659f6c2ab48a01ff7ef282e
xiaoboost <xiaoboost@qq.com>
2019-10-16 10:55:45 +0800

    fix(type): fix patchStyle description

--- packages/runtime-dom/src/modules/style.ts
+++ packages/runtime-dom/src/modules/style.ts
@@ -2,8 +2,8 @@ import { isString } from '@vue/shared'
 
 type Style = string | Partial<CSSStyleDeclaration>
 
-export function patchStyle(el: HTMLElement, prev: Style, next: Style) {
-  const { style } = el
+export function patchStyle(el: Element, prev: Style, next: Style) {
+  const style = (el as HTMLElement).style
   if (!next) {
     el.removeAttribute('style')
   } else if (isString(next)) {
