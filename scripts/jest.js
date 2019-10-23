07abb2b06d7088c03d654d4796edb86d8d521358
wangtao0101 <yuecjn@gmail.com>
2019-10-23 11:19:09 +0800

    chore: add commnet for why using jest.js

--- scripts/jest.js
+++ scripts/jest.js
@@ -1,3 +1,10 @@
+/**
+ * This file is the entry for debug single test file in vscode
+ *
+ * Why not use node_modules/.bin/jest ?
+ * The node_modules/.bin/jest is not work for all operating system, see this issue https://github.com/microsoft/vscode-recipes/issues/107
+ */
+
 const jest = require('jest')
 
 const argv = process.argv
