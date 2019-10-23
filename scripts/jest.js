c0d7e305b955fd97f91d4eeb88fa2d696c70fded
Evan You <yyx990803@gmail.com>
2019-10-23 11:19:20 -0400

    Update jest.js

--- scripts/jest.js
+++ scripts/jest.js
@@ -1,14 +1,7 @@
 /**
  * This file is the entry for debug single test file in vscode
  *
- * Why not use node_modules/.bin/jest ?
- * The node_modules/.bin/jest is not work for all operating system, see this issue https://github.com/microsoft/vscode-recipes/issues/107
+ * Not using node_modules/.bin/jest due to cross platform issues, see
+ * https://github.com/microsoft/vscode-recipes/issues/107
  */
-
-const jest = require('jest')
-
-const argv = process.argv
-
-argv.push('--config', 'jest.config.js')
-
-jest.run(argv)
+require('jest').run(process.argv)
