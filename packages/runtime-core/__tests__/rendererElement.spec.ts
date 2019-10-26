2991144d77df6f3a6434c9e0e27fba44c216e094
sh7dm <d3dx12.xx@gmail.com>
2019-10-26 09:56:10 +0300

    test(createRenderer): add tests for element rendering

--- packages/runtime-core/__tests__/rendererElement.spec.ts
+++ packages/runtime-core/__tests__/rendererElement.spec.ts
@@ -1,9 +1,55 @@
+import { nodeOps, render, h, serialize, Text } from '@vue/runtime-test'
+
 describe('renderer: element', () => {
-  test.todo('with props')
+  test('simple', () => {
+    const root = nodeOps.createElement('div')
 
-  test.todo('with direct text children')
+    render(h('div'), root)
 
-  test.todo('with text node children')
+    expect(serialize(root)).toMatchSnapshot()
+  })
 
-  test.todo('handle already mounted VNode')
+  test('with props', () => {
+    const root = nodeOps.createElement('div')
+
+    render(
+      h('div', {
+        id: 'test'
+      }),
+      root
+    )
+
+    expect(serialize(root)).toMatchSnapshot()
+  })
+
+  test('with direct text children', () => {
+    const root = nodeOps.createElement('div')
+
+    render(h('div', ['I ❤ Vue']), root)
+
+    expect(serialize(root)).toMatchSnapshot()
+  })
+
+  test('with text node children', () => {
+    const root = nodeOps.createElement('div')
+
+    render(h('div', [h(Text, ['I ❤ Vue'])]), root)
+
+    expect(serialize(root)).toMatchSnapshot()
+  })
+
+  test('handle already mounted VNode', () => {
+    const root1 = nodeOps.createElement('div')
+    const root2 = nodeOps.createElement('div')
+    const vnode = h('div')
+
+    render(vnode, root1)
+    expect(vnode.el).toBe(root1.children[0])
+
+    render(vnode, root2)
+    expect(vnode.el).toBe(root2.children[0])
+
+    expect(serialize(root1)).toMatchSnapshot('root 1')
+    expect(serialize(root2)).toMatchSnapshot('root 2')
+  })
 })
