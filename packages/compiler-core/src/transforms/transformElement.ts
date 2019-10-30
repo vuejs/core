538e11fb100685bd23d39b4c2d0c3dcaebe04c6f
sh7dm <d3dx12.xx@gmail.com>
2019-10-30 19:07:43 +0300

    fix(transformElement): build suspense slots

--- packages/compiler-core/src/transforms/transformElement.ts
+++ packages/compiler-core/src/transforms/transformElement.ts
@@ -120,7 +120,7 @@ export const transformElement: NodeTransform = (node, context) => {
       if (!hasProps) {
         args.push(`null`)
       }
-      if (isComponent) {
+      if (isComponent || node.tagType === ElementTypes.SUSPENSE) {
         const { slots, hasDynamicSlots } = buildSlots(node, context)
         args.push(slots)
         if (hasDynamicSlots) {
