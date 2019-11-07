afdca88bf3358ec4e7b5acf1a947ab138a5fff70
Evan You <yyx990803@gmail.com>
2019-11-06 22:35:56 -0500

    chore: remove useless type [ci skip]

--- packages/compiler-sfc/src/templateUtils.ts
+++ packages/compiler-sfc/src/templateUtils.ts
@@ -1,13 +1,3 @@
-export interface Attr {
-  name: string
-  value: string
-}
-
-export interface ASTNode {
-  tag: string
-  attrs: Attr[]
-}
-
 import { UrlWithStringQuery, parse as uriParse } from 'url'
 
 // TODO use imports instead.
