9f75f9c18762290db025d4bf555477dc34b6cdcc
likui <2218301630@qq.com>
2019-11-19 21:55:55 +0800

    fix(compiler-sfc): should judgment if not children

--- packages/compiler-sfc/src/parse.ts
+++ packages/compiler-sfc/src/parse.ts
@@ -74,6 +74,9 @@ export function parse(
     if (node.type !== NodeTypes.ELEMENT) {
       return
     }
+    if (!node.children.length) {
+      return
+    }
     switch (node.tag) {
       case 'template':
         if (!sfc.template) {
