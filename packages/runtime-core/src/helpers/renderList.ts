41119552d963e8863f9c9a2a19cf32175df2074f
Gabriel Loiácono <gabriel.vloiacono@gmail.com>
2019-11-23 02:32:04 -0300

    perf(runtime-core): removed unnecessary new Array calls

--- packages/runtime-core/src/helpers/renderList.ts
+++ packages/runtime-core/src/helpers/renderList.ts
@@ -9,14 +9,12 @@ export function renderList(
     index?: number
   ) => VNodeChild
 ): VNodeChild[] {
-  let ret: VNodeChild[]
+  let ret: VNodeChild[] = []
   if (isArray(source) || isString(source)) {
-    ret = new Array(source.length)
     for (let i = 0, l = source.length; i < l; i++) {
       ret[i] = renderItem(source[i], i)
     }
   } else if (typeof source === 'number') {
-    ret = new Array(source)
     for (let i = 0; i < source; i++) {
       ret[i] = renderItem(i + 1, i)
     }
@@ -25,7 +23,6 @@ export function renderList(
       ret = Array.from(source as Iterable<any>, renderItem)
     } else {
       const keys = Object.keys(source)
-      ret = new Array(keys.length)
       for (let i = 0, l = keys.length; i < l; i++) {
         const key = keys[i]
         ret[i] = renderItem(source[key], key, i)
