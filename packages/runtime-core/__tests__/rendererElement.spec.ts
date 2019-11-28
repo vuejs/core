cb332c7ec9150e73c3ec6ac9dabd30e1df4c7d5f
jizhi <theniceangel@163.com>
2019-11-28 08:59:38 +0800

    test(rendererElement): add rendererElement tests

--- packages/runtime-core/__tests__/rendererElement.spec.ts
+++ packages/runtime-core/__tests__/rendererElement.spec.ts
@@ -1,9 +1,54 @@
+import { h, render, Text } from '@vue/runtime-dom'
+
 describe('renderer: element', () => {
-  test.todo('with props')
+  test('with props', () => {
+    const root = document.createElement('div')
 
-  test.todo('with direct text children')
+    render(
+      h('div', {
+        id: 'test',
+        class: 'c1',
+        style: { color: 'red' }
+      }),
+      root
+    )
 
-  test.todo('with text node children')
+    const node = root.children[0] as HTMLElement
 
-  test.todo('handle already mounted VNode')
+    expect(node.tagName).toBe('DIV')
+    expect(node.getAttribute('id')).toBe('test')
+    expect(node.getAttribute('class')).toBe('c1')
+    expect(node.style.color).toBe('red')
+  })
+
+  test('with direct text children', () => {
+    const root = document.createElement('div')
+
+    render(h('div', 'foo'), root)
+
+    const node = root.children[0] as HTMLElement
+
+    expect(node.textContent).toBe('foo')
+  })
+
+  test('with text node children', () => {
+    const root = document.createElement('div')
+
+    render(h('div', null, h(Text, null, 'foo')), root)
+
+    const node = root.children[0] as HTMLElement
+
+    expect(node.textContent).toBe('foo')
+  })
+
+  test('patch already mounted VNode', () => {
+    const root = document.createElement('div')
+
+    render(h('div', 'foo'), root)
+    // patch
+    render(h('div', 'baz'), root)
+
+    const node = root.children[0] as HTMLElement
+    expect(node.textContent).toBe('baz')
+  })
 })
