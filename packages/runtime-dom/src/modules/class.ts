52239d137c07750c3fb599c904e6865106ac42cb
Evan You <yyx990803@gmail.com>
2019-11-29 11:53:54 -0500

    types: fix class module transition class typing

--- packages/runtime-dom/src/modules/class.ts
+++ packages/runtime-dom/src/modules/class.ts
@@ -2,20 +2,17 @@ import { ElementWithTransition } from '../components/Transition'
 
 // compiler should normalize class + :class bindings on the same element
 // into a single binding ['staticClass', dynamic]
-export function patchClass(
-  el: ElementWithTransition,
-  value: string,
-  isSVG: boolean
-) {
-  // if this is an element during a transition, take the temporary transition
-  // classes into account.
-  if (el._vtc) {
-    value = [value, ...el._vtc].join(' ')
-  }
+export function patchClass(el: Element, value: string, isSVG: boolean) {
   // directly setting className should be faster than setAttribute in theory
   if (isSVG) {
     el.setAttribute('class', value)
   } else {
+    // if this is an element during a transition, take the temporary transition
+    // classes into account.
+    const transtionClasses = (el as ElementWithTransition)._vtc
+    if (transtionClasses) {
+      value = [value, ...transtionClasses].join(' ')
+    }
     el.className = value
   }
 }
