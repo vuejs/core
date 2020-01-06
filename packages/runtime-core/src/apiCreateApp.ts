c008d965006528dfca8a438290215fcce8d50460
Kael <kaelwd@gmail.com>
2020-01-07 10:19:45 +1100

    fix(core): allow classes to be passed as plugins

--- packages/runtime-core/src/apiCreateApp.ts
+++ packages/runtime-core/src/apiCreateApp.ts
@@ -103,12 +103,16 @@ export function createAppAPI<HostNode, HostElement>(
       use(plugin: Plugin, ...options: any[]) {
         if (installedPlugins.has(plugin)) {
           __DEV__ && warn(`Plugin has already been applied to target app.`)
+        } else if (
+          plugin &&
+          'install' in plugin &&
+          isFunction(plugin.install)
+        ) {
+          installedPlugins.add(plugin)
+          plugin.install(app, ...options)
         } else if (isFunction(plugin)) {
           installedPlugins.add(plugin)
           plugin(app, ...options)
-        } else if (plugin && isFunction(plugin.install)) {
-          installedPlugins.add(plugin)
-          plugin.install(app, ...options)
         } else if (__DEV__) {
           warn(
             `A plugin must either be a function or an object with an "install" ` +
