59393dd75766720330cb69e22086c97a392dbbe4
djy0 <krivergo3@gmail.com>
2020-02-26 23:13:07 +0800

    fix(template-explorer): rename watch -> watchEffect (#780)

--- packages/template-explorer/src/index.ts
+++ packages/template-explorer/src/index.ts
@@ -2,7 +2,7 @@ import * as m from 'monaco-editor'
 import { compile, CompilerError, CompilerOptions } from '@vue/compiler-dom'
 import { compile as ssrCompile } from '@vue/compiler-ssr'
 import { compilerOptions, initOptions, ssrMode } from './options'
-import { watch } from '@vue/runtime-dom'
+import { watchEffect } from '@vue/runtime-dom'
 import { SourceMapConsumer } from 'source-map'
 
 declare global {
@@ -221,7 +221,7 @@ window.init = () => {
   )
 
   initOptions()
-  watch(reCompile)
+  watchEffect(reCompile)
 }
 
 function debounce<T extends (...args: any[]) => any>(
