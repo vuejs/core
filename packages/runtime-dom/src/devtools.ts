88c9f83ed4ce5ace30c7286802336bc4fb0e701e
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-03-15 16:38:03 +0100

    fix: bracket

--- packages/runtime-dom/src/devtools.ts
+++ packages/runtime-dom/src/devtools.ts
@@ -1,5 +1,5 @@
 import { setDevtoolsHook } from '@vue/runtime-core'
 
 setDevtoolsHook(
-  (__BROWSER__ ? window : (global as any)).__VUE_DEVTOOLS_GLOBAL_HOOK__
+  ((__BROWSER__ ? window : global) as any).__VUE_DEVTOOLS_GLOBAL_HOOK__
 )
