84391e0268cb69e34a463d050305227fcb86a47f
Eduardo San Martin Morote <posva13@gmail.com>
2020-03-31 18:45:39 +0200

    fix(transition): warn only when there is more than one rendered child

--- packages/compiler-dom/src/transforms/warnTransitionChildren.ts
+++ packages/compiler-dom/src/transforms/warnTransitionChildren.ts
@@ -1,4 +1,10 @@
-import { NodeTransform, NodeTypes, ElementTypes } from '@vue/compiler-core'
+import {
+  NodeTransform,
+  NodeTypes,
+  ElementTypes,
+  ComponentNode,
+  TemplateChildNode
+} from '@vue/compiler-core'
 import { TRANSITION } from '../runtimeHelpers'
 import { createDOMCompilerError, DOMErrorCodes } from '../errors'
 
@@ -8,10 +14,7 @@ export const warnTransitionChildren: NodeTransform = (node, context) => {
     node.tagType === ElementTypes.COMPONENT
   ) {
     const component = context.isBuiltInComponent(node.tag)
-    if (
-      component === TRANSITION &&
-      (node.children.length > 1 || node.children[0].type === NodeTypes.FOR)
-    ) {
+    if (component === TRANSITION && hasMultipleChildren(node)) {
       context.onError(
         createDOMCompilerError(DOMErrorCodes.X_TRANSITION_INVALID_CHILDREN, {
           start: node.children[0].loc.start,
@@ -21,4 +24,42 @@ export const warnTransitionChildren: NodeTransform = (node, context) => {
       )
     }
   }
+}
+
+function hasMultipleChildren(node: ComponentNode): boolean {
+  if (node.children.length > 1) {
+    return !node.children.every(hasValidDirectives)
+  }
+
+  const child = node.children[0]
+  return (
+    // no child
+    !!child &&
+    (child.type === NodeTypes.FOR ||
+      (child.type === NodeTypes.ELEMENT &&
+        // disallow template
+        (child.tag === 'template' ||
+          // disallow v-for
+          child.props.some(
+            prop => prop.type === NodeTypes.DIRECTIVE && prop.name === 'for'
+          ))))
+  )
+}
+
+function hasValidDirectives(node: TemplateChildNode, i: number): boolean {
+  return (
+    node.type === NodeTypes.ELEMENT &&
+    node.tag !== 'template' &&
+    node.props.length > 0 &&
+    // disallow multiple v-for
+    !node.props.some(
+      prop => prop.type === NodeTypes.DIRECTIVE && prop.name === 'for'
+    ) &&
+    node.props.some(
+      prop =>
+        prop.type === NodeTypes.DIRECTIVE &&
+        ((i === 0 && prop.name === 'if') ||
+          (i > 0 && (prop.name === 'else' || prop.name === 'else-if')))
+    )
+  )
 }
