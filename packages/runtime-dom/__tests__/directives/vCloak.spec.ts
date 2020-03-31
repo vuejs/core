0ed147d33610b86af72cbadcc4b32e6069bcaf08
likui <2218301630@qq.com>
2020-04-01 06:13:59 +0800

    fix(runtime-dom): v-cloak should be removed after compile on the root element (#893)
    fix #890

---
+++ packages/runtime-dom/__tests__/directives/vCloak.spec.ts
@@ -0,0 +1,10 @@
+import { createApp } from '@vue/runtime-dom'
+
+describe('vCloak', () => {
+  test('should be removed after compile', () => {
+    const root = document.createElement('div')
+    root.setAttribute('v-cloak', '')
+    createApp({}).mount(root)
+    expect(root.hasAttribute('v-cloak')).toBe(false)
+  })
+})
