ffb8f7baa2fb055e2bcffef50382ad4b64615ad5
HiiTea <luohaojing@gmail.com>
2020-05-01 03:06:57 +0800

    chore: code comment formatting

--- packages/compiler-core/src/parse.ts
+++ packages/compiler-core/src/parse.ts
@@ -56,9 +56,9 @@ export const defaultParserOptions: MergedParserOptions = {
 
 export const enum TextModes {
   //          | Elements | Entities | End sign              | Inside of
-  DATA, //    | ✔       | ✔       | End tags of ancestors |
-  RCDATA, //  | ✘       | ✔       | End tag of the parent | <textarea>
-  RAWTEXT, // | ✘       | ✘       | End tag of the parent | <style>,<script>
+  DATA, //    | ✔        | ✔        | End tags of ancestors |
+  RCDATA, //  | ✘        | ✔        | End tag of the parent | <textarea>
+  RAWTEXT, // | ✘        | ✘        | End tag of the parent | <style>,<script>
   CDATA,
   ATTRIBUTE_VALUE
 }
