d8304cf271c6a6b277dcdfa41b4ed5aa8ed6f0dc
shengxinjing <316783812@qq.com>
2020-05-02 09:38:44 +0800

    refactor(shared): replace switch with config in escapeHtml

--- packages/shared/src/escapeHtml.ts
+++ packages/shared/src/escapeHtml.ts
@@ -12,35 +12,24 @@ export function escapeHtml(string: unknown) {
   let escaped: string
   let index: number
   let lastIndex = 0
-  for (index = match.index; index < str.length; index++) {
-    switch (str.charCodeAt(index)) {
-      case 34: // "
-        escaped = '&quot;'
-        break
-      case 38: // &
-        escaped = '&amp;'
-        break
-      case 39: // '
-        escaped = '&#39;'
-        break
-      case 60: // <
-        escaped = '&lt;'
-        break
-      case 62: // >
-        escaped = '&gt;'
-        break
-      default:
-        continue
-    }
-
-    if (lastIndex !== index) {
-      html += str.substring(lastIndex, index)
-    }
-
-    lastIndex = index + 1
-    html += escaped
+  const codeToEscapes:any = {
+    34:'&quot;', // "
+    38:'&amp;', // &
+    39:'&#39;', // '
+    60:'&lt;', // <
+    62:'&gt;'  // >
   }
-
+  for (index = match.index; index < str.length; index++) {
+    const code = str.charCodeAt(index)
+    if(codeToEscapes[code]){
+      escaped = codeToEscapes[code]
+      if (lastIndex !== index) {
+        html += str.substring(lastIndex, index)
+      }
+      lastIndex = index + 1
+      html += escaped
+    }
+  }
   return lastIndex !== index ? html + str.substring(lastIndex, index) : html
 }
 
