5f15d9aa4b9024b3764b962bee042d72f94dee91
Evan You <yyx990803@gmail.com>
2020-05-26 14:27:01 -0400

    fix(compiler-ssr): should escape template string interpolation chars in generated code

--- packages/compiler-ssr/__tests__/ssrText.spec.ts
+++ packages/compiler-ssr/__tests__/ssrText.spec.ts
@@ -6,6 +6,21 @@ describe('ssr: text', () => {
     expect(getCompiledString(`foo`)).toMatchInlineSnapshot(`"\`foo\`"`)
   })
 
+  test('static text with template string special chars', () => {
+    expect(getCompiledString(`\`\${foo}\``)).toMatchInlineSnapshot(
+      `"\`\\\\\`\\\\\${foo}\\\\\`\`"`
+    )
+  })
+
+  test('static text with char escape', () => {
+    // the desired generated code should be `\\\$foo`
+    // snapshot -> inline snapshot goes through two escapes
+    // so that makes a total of 3 * 2 * 2 = 12 back slashes
+    expect(getCompiledString(`\\$foo`)).toMatchInlineSnapshot(
+      `"\`\\\\\\\\\\\\$foo\`"`
+    )
+  })
+
   test('comments', () => {
     expect(getCompiledString(`<!--bar-->`)).toMatchInlineSnapshot(
       `"\`<!--bar-->\`"`
