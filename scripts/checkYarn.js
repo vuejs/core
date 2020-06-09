4c542d50338551694762f1366bcc3fac126c2155
Evan You <yyx990803@gmail.com>
2020-06-09 17:39:48 -0400

    chore: adjust yarn warning [ci skip]

--- scripts/checkYarn.js
+++ scripts/checkYarn.js
@@ -1,5 +1,6 @@
 if (!/yarn\.js$/.test(process.env.npm_execpath || '')) {
   console.warn(
-    "\u001b[33mYou don't seem to be using yarn. This could produce unexpected results.\u001b[39m"
+    '\u001b[33mThis repository requires Yarn 1.x for scripts to work properly.\u001b[39m\n'
   )
+  process.exit(1)
 }
