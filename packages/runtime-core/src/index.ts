68e2d6c68a4e8a95d112597b82d40efb8571d9c0
Cédric Exbrayat <cexbrayat@users.noreply.github.com>
2020-06-15 17:13:00 +0200

    fix(types): export ComponentOptionsMixin (#1361)
    The newly introduced `ComponentOptionsMixin` was not re-exported.

--- packages/runtime-core/src/index.ts
+++ packages/runtime-core/src/index.ts
@@ -159,6 +159,7 @@ export {
 } from './component'
 export {
   ComponentOptions,
+  ComponentOptionsMixin,
   ComponentOptionsWithoutProps,
   ComponentOptionsWithObjectProps,
   ComponentOptionsWithArrayProps,
