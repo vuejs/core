9b93f99a9dc4292edc766353ab0776507a6427ef
hanmingyang <hanmingyang2009@gmail.com>
2020-06-16 13:33:24 +0800

    test(compiler-sfc):add test isExternalUrl and blank lines

--- packages/compiler-sfc/__tests__/templateUtils.spec.ts
+++ packages/compiler-sfc/__tests__/templateUtils.spec.ts
@@ -1,4 +1,7 @@
-import { isRelativeUrl } from '../../compiler-sfc/src/templateUtils'
+import {
+  isRelativeUrl,
+  isExternalUrl
+} from '../../compiler-sfc/src/templateUtils'
 
 describe('compiler sfc:templateUtils isRelativeUrl', () => {
   test('should return true when The first character of the string path is .', () => {
@@ -16,6 +19,20 @@ describe('compiler sfc:templateUtils isRelativeUrl', () => {
   test('should return true when The first character of the string path is @', () => {
     const url = '@/xx.vue'
     const result = isRelativeUrl(url)
+    expect(result).toBe(true)
+  })
+})
+
+describe('compiler sfc:templateUtils isExternalUrl', () => {
+  test('should return true when String starts with http://', () => {
+    const url = 'http://vuejs.org/'
+    const result = isExternalUrl(url)
+    expect(result).toBe(true)
+  })
+
+  test('should return true when String starts with https://', () => {
+    const url = 'https://vuejs.org/'
+    const result = isExternalUrl(url)
     expect(result).toBe(true)
   })
 })
