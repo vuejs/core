0e0955fdc6fd9848e23dbb09e41d627c073970f7
shengxinjing <316783812@qq.com>
2020-06-16 17:06:08 +0800

    test(shared): test coverage for looseEqual

--- packages/shared/__tests__/looseEqual.spec.ts
+++ packages/shared/__tests__/looseEqual.spec.ts
@@ -3,6 +3,11 @@ import { looseEqual } from '../src'
 test('looseEqual should compare Date object', () => {
   const date1 = new Date(1592297844875)
   const date2 = new Date(1592297844875)
-
   expect(looseEqual(date1, date2)).toBe(true)
+})
+
+test('looseEqual should compare deep object', () => {
+  const obj1 = { a: { b: 123 } }
+  const obj2 = { a: { b: 123 } }
+  expect(looseEqual(obj1, obj2)).toBe(true)
 })
