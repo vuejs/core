bdec2f668f7bf47f131b8c1c2a385fd091e65570
pikax <carlos@hypermob.co.uk>
2020-07-03 11:10:47 +0100

    types(runtime-core): provide valid type for `$emit` when used with `ComponentPublicInstance`

--- packages/runtime-core/src/componentEmits.ts
+++ packages/runtime-core/src/componentEmits.ts
@@ -25,13 +25,15 @@ export type EmitFn<
   Event extends keyof Options = keyof Options
 > = Options extends any[]
   ? (event: Options[0], ...args: any[]) => void
-  : UnionToIntersection<
-      {
-        [key in Event]: Options[key] extends ((...args: infer Args) => any)
-          ? (event: key, ...args: Args) => void
-          : (event: key, ...args: any[]) => void
-      }[Event]
-    >
+  : {} extends Options // if the emit is empty object (usually the default value for emit) should be converted to function
+    ? (event: string, ...args: any[]) => void
+    : UnionToIntersection<
+        {
+          [key in Event]: Options[key] extends ((...args: infer Args) => any)
+            ? (event: key, ...args: Args) => void
+            : (event: key, ...args: any[]) => void
+        }[Event]
+      >
 
 export function emit(
   instance: ComponentInternalInstance,
