1e53f2dba3f3e091f434bd345b708ca5e057e093
1148177968 <1148177968@qq.com>
2020-07-11 20:18:56 +0800

    test(shared): add test for looseEqual

--- packages/shared/__tests__/looseEqual.spec.ts
+++ packages/shared/__tests__/looseEqual.spec.ts
@@ -8,9 +8,9 @@ test('looseEqual', () => {
   const date3 = new Date(tmp2)
   expect(looseEqual(date1, date2)).toBe(true)
   expect(looseEqual(date1, date3)).toBe(false)
-  const arr1 = [[2, 78, 'a', ['a', 'b', 11]], 9, 4, 1]
-  const arr2 = [[2, 78, 'a', ['a', 'b', 12]], 9, 4, 1]
-  const arr3 = [[2, 78, 'a', ['a', 'b', 12]], 9, 4, 1]
+  const arr1 = [[2, 78, 'a', ['a', 'b', 11]], 9, 4]
+  const arr2 = [[2, 78, 'a', ['a', 'b', 12]], 9, 4]
+  const arr3 = [[2, 78, 'a', ['a', 'b', 12]], 9, 4]
   expect(looseEqual(arr1, arr2)).toBe(false)
   expect(looseEqual(arr2, arr3)).toBe(true)
 })
