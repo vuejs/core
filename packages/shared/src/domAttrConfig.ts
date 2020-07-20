c63a815df1fdbd07443240ed596097bc0d9705c8
彭小龙 <q213546879@hotmail.com>
2020-07-20 12:13:51 +0800

    chore(shared):"bgcolor,border,height,width",this attributes is legacy,should use the css property instead

--- packages/shared/src/domAttrConfig.ts
+++ packages/shared/src/domAttrConfig.ts
@@ -68,18 +68,18 @@ export const isNoUnitNumericStyleProp = /*#__PURE__*/ makeMap(
  */
 export const isKnownAttr = /*#__PURE__*/ makeMap(
   `accept,accept-charset,accesskey,action,align,allow,alt,async,` +
-    `autocapitalize,autocomplete,autofocus,autoplay,background,bgcolor,` +
-    `border,buffered,capture,challenge,charset,checked,cite,class,code,` +
+    `autocapitalize,autocomplete,autofocus,autoplay,background,` +
+    `buffered,capture,challenge,charset,checked,cite,class,code,` +
     `codebase,color,cols,colspan,content,contenteditable,contextmenu,controls,` +
     `coords,crossorigin,csp,data,datetime,decoding,default,defer,dir,dirname,` +
     `disabled,download,draggable,dropzone,enctype,enterkeyhint,for,form,` +
     `formaction,formenctype,formmethod,formnovalidate,formtarget,headers,` +
-    `height,hidden,high,href,hreflang,http-equiv,icon,id,importance,integrity,` +
+    `hidden,high,href,hreflang,http-equiv,icon,id,importance,integrity,` +
     `ismap,itemprop,keytype,kind,label,lang,language,loading,list,loop,low,` +
     `manifest,max,maxlength,minlength,media,min,multiple,muted,name,novalidate,` +
     `open,optimum,pattern,ping,placeholder,poster,preload,radiogroup,readonly,` +
     `referrerpolicy,rel,required,reversed,rows,rowspan,sandbox,scope,scoped,` +
     `selected,shape,size,sizes,slot,span,spellcheck,src,srcdoc,srclang,srcset,` +
     `start,step,style,summary,tabindex,target,title,translate,type,usemap,` +
-    `value,width,wrap`
+    `value,wrap`
 )
