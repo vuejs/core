f6afe7000efb964355c439b7963087ab8e42d6b1
Evan You <yyx990803@gmail.com>
2020-08-03 17:04:45 -0400

    fix(runtome-core): do not cache property access in beforeCreate hook
    fix #1756

--- packages/runtime-core/src/componentOptions.ts
+++ packages/runtime-core/src/componentOptions.ts
@@ -364,6 +364,8 @@ function createDuplicateChecker() {
 
 type DataFn = (vm: ComponentPublicInstance) => any
 
+export let isInBeforeCreate = false
+
 export function applyOptions(
   instance: ComponentInternalInstance,
   options: ComponentOptions,
@@ -407,7 +409,9 @@ export function applyOptions(
 
   // applyOptions is called non-as-mixin once per instance
   if (!asMixin) {
+    isInBeforeCreate = true
     callSyncHook('beforeCreate', options, publicThis, globalMixins)
+    isInBeforeCreate = false
     // global mixins are applied first
     applyMixins(instance, globalMixins, deferredData, deferredWatch)
   }
