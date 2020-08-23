c49a6ebf924ecb63dea533c1f0ee2e02ab12ea93
Yang Mingshan <y.mingshan3@gmail.com>
2020-08-23 16:50:59 +0800

    fix(watch): deep watch

--- packages/runtime-core/__tests__/apiWatch.spec.ts
+++ packages/runtime-core/__tests__/apiWatch.spec.ts
@@ -574,6 +574,25 @@ describe('api: watch', () => {
     expect(dummy).toEqual([1, 2, 2, false])
   })
 
+  it('watching deep ref', async () => {
+    const count = ref(0)
+    const double = computed(() => count.value * 2)
+    const state = reactive([count, double])
+
+    let dummy
+    watch(
+      () => state,
+      state => {
+        dummy = [state[0].value, state[1].value]
+      },
+      { deep: true }
+    )
+
+    count.value++
+    await nextTick()
+    expect(dummy).toEqual([1, 2])
+  })
+
   it('immediate', async () => {
     const count = ref(0)
     const cb = jest.fn()
