4b2d199a04e49d426c9842ad2315a8b614ec3973
underfin <likui6666666@gmail.com>
2020-09-16 18:28:36 +0800

    workflow: use ts `noUnusedParameters` check unused arguments

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,6 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
-    'no-unused-vars': [
-      'error',
-      // we are only using this rule to check for unused arguments since TS
-      // catches unused variables but not args.
-      { varsIgnorePattern: '.*', args: 'after-used', argsIgnorePattern: '^_' }
-    ],
     // most of the codebase are expected to be env agnostic
     'no-restricted-globals': ['error', ...DOMGlobals, ...NodeGlobals],
     // since we target ES2015 for baseline support, we need to forbid object
