bac4d226148d6c21337ea4123262fc4936208fca
izayl <me@izayl.com>
2020-10-14 03:33:16 +0800

    chore: add magic number comment (#2198) [ci skip]

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -80,6 +80,7 @@ export const toTypeString = (value: unknown): string =>
   objectToString.call(value)
 
 export const toRawType = (value: unknown): string => {
+  // extract "RawType" from strings like "[object RawType]"
   return toTypeString(value).slice(8, -1)
 }
 
