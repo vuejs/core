edd49dcab40eb3faa44248772b176d5eebfd30fe
被雨水过滤的空气 <958414905@qq.com>
2020-10-20 06:08:54 +0800

    fix(runtime-core): avoid mutating EMPTY_ARR when setting dev root (#2419)
    also freeze EMPTY_ARR in dev
    
    fix #2413

--- packages/runtime-core/src/componentProps.ts
+++ packages/runtime-core/src/componentProps.ts
@@ -378,7 +378,7 @@ export function normalizePropsOptions(
   }
 
   if (!raw && !hasExtends) {
-    return (comp.__props = EMPTY_ARR)
+    return (comp.__props = EMPTY_ARR as any)
   }
 
   if (isArray(raw)) {
--- packages/vue/__tests__/index.spec.ts
+++ packages/vue/__tests__/index.spec.ts
@@ -1,3 +1,4 @@
+import { EMPTY_ARR } from '@vue/shared'
 import { createApp, ref, nextTick, reactive } from '../src'
 
 describe('compiler + runtime integration', () => {
@@ -280,5 +281,15 @@ describe('compiler + runtime integration', () => {
     list.push(2)
     await nextTick()
     expect(container.innerHTML).toBe(`<div>2<div>1</div></div>`)
+  })
+
+  // #2413
+  it('EMPTY_ARR should not change', () => {
+    const App = {
+      template: `<div v-for="v of ['a']">{{ v }}</div>`
+    }
+    const container = document.createElement('div')
+    createApp(App).mount(container)
+    expect(EMPTY_ARR.length).toBe(0)
   })
 })
