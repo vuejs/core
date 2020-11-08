76fb69d9fb4f9f0975c414df24757d4663139caf
Andrew Powers <andrew@fiction.com>
2020-11-08 14:24:03 -0800

    feat(ssr): expose SSR render context. Fix #2574

--- packages/server-renderer/src/renderToString.ts
+++ packages/server-renderer/src/renderToString.ts
@@ -47,6 +47,27 @@ function unrollBufferSync(buffer: SSRBuffer): string {
   return ret
 }
 
+/**
+ * Get the rendering context for an app including teleports
+ * https://github.com/vuejs/vue-next/issues/2574
+ */
+export async function getRenderContext(
+  input: App,
+  context: SSRContext = {}
+): Promise<{ context: SSRContext; buffer: SSRBuffer }> {
+  // rendering an app
+  const vnode = createVNode(input._component, input._props)
+  vnode.appContext = input._context
+
+  // provide the ssr context to the tree
+  input.provide(ssrContextKey, context)
+  const buffer = await renderComponentVNode(vnode)
+
+  await resolveTeleports(context)
+
+  return { buffer, context }
+}
+
 export async function renderToString(
   input: App | VNode,
   context: SSRContext = {}
@@ -56,16 +77,9 @@ export async function renderToString(
     return renderToString(createApp({ render: () => input }), context)
   }
 
-  // rendering an app
-  const vnode = createVNode(input._component, input._props)
-  vnode.appContext = input._context
-  // provide the ssr context to the tree
-  input.provide(ssrContextKey, context)
-  const buffer = await renderComponentVNode(vnode)
+  const { buffer } = await getRenderContext(input, context)
 
-  await resolveTeleports(context)
-
-  return unrollBuffer(buffer as SSRBuffer)
+  return unrollBuffer(buffer)
 }
 
 async function resolveTeleports(context: SSRContext) {
