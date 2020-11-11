7df00db11811caeb00a836e543550256f9033ab4
daiwei <daiwei521@126.com>
2020-11-11 18:53:01 +0800

    chore(shared): use type `NormalizedStyle`

--- packages/shared/src/normalizeProp.ts
+++ packages/shared/src/normalizeProp.ts
@@ -5,7 +5,7 @@ export type NormalizedStyle = Record<string, string | number>
 
 export function normalizeStyle(value: unknown): NormalizedStyle | undefined {
   if (isArray(value)) {
-    const res: Record<string, string | number> = {}
+    const res: NormalizedStyle = {}
     for (let i = 0; i < value.length; i++) {
       const item = value[i]
       const normalized = normalizeStyle(
