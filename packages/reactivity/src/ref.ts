b7ba2d30365b4b75d0a029cff6f3c09f5cd74ef6
zcating <zcating@qq.com>
2020-12-08 09:30:39 +0800

    fix(toRef): return type gets a union of 'Ref<*>'

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -174,7 +174,7 @@ class ObjectRefImpl<T extends object, K extends keyof T> {
 export function toRef<T extends object, K extends keyof T>(
   object: T,
   key: K
-): Ref<T[K]> {
+): Ref<UnwrapRef<ToRef<T[K]>>> {
   return isRef(object[key])
     ? object[key]
     : (new ObjectRefImpl(object, key) as any)
