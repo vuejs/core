3a1c84737532200f1557b0955ef2dce3b7bb73d7
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-12-31 22:33:19 +1300

    Fix typo "depdendency"

--- packages/runtime-core/src/hmr.ts
+++ packages/runtime-core/src/hmr.ts
@@ -72,7 +72,7 @@ function createRecord(
     warn(
       `HMR API usage is out of date.\n` +
         `Please upgrade vue-loader/vite/rollup-plugin-vue or other relevant ` +
-        `depdendency that handles Vue SFC compilation.`
+        `dependency that handles Vue SFC compilation.`
     )
     component = {}
   }
