48bb580becf70abcf6c9f4c4b2df891e9468c299
daiwei <daiwei521@126.com>
2021-01-14 10:43:45 +0800

    fix(compiler-dom): add test case

--- packages/compiler-dom/__tests__/index.spec.ts
+++ packages/compiler-dom/__tests__/index.spec.ts
@@ -10,4 +10,20 @@ describe('compile', () => {
 
     expect(code).toMatchSnapshot()
   })
+
+  // #3001
+  it('should work with v-bind + HTML tag', () => {
+    const { code } = compile(`<list :model="{
+      widgets: [
+        {
+          a: 'Test',
+          b: &quot;Test&quot;,
+          c: &quot;<strong>Bold Text</strong>&quot;,
+          d: '<strong>Bold Text</strong>',
+        }
+      ]
+    }"></list>`)
+
+    expect(code).toMatchSnapshot()
+  })
 })
