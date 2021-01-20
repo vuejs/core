43168ff23275ac70e5947a2ca16541b6af29bab6
Yang Mingshan <y.mingshan3@gmail.com>
2021-01-20 10:19:00 +0800

    chore: remove the order of overload comments

--- packages/runtime-core/src/apiWatch.ts
+++ packages/runtime-core/src/apiWatch.ts
@@ -80,7 +80,7 @@ const INITIAL_WATCHER_VALUE = {}
 
 type MultiWatchSources = (WatchSource<unknown> | object)[]
 
-// overload #1: array of multiple sources + cb
+// overload: array of multiple sources + cb
 export function watch<
   T extends MultiWatchSources,
   Immediate extends Readonly<boolean> = false
@@ -90,7 +90,7 @@ export function watch<
   options?: WatchOptions<Immediate>
 ): WatchStopHandle
 
-// overload #2 for multiple sources w/ `as const`
+// overload: multiple sources w/ `as const`
 // watch([foo, bar] as const, () => {})
 // somehow [...T] breaks when the type is readonly
 export function watch<
@@ -102,14 +102,14 @@ export function watch<
   options?: WatchOptions<Immediate>
 ): WatchStopHandle
 
-// overload #3: single source + cb
+// overload: single source + cb
 export function watch<T, Immediate extends Readonly<boolean> = false>(
   source: WatchSource<T>,
   cb: WatchCallback<T, Immediate extends true ? (T | undefined) : T>,
   options?: WatchOptions<Immediate>
 ): WatchStopHandle
 
-// overload #4: watching reactive object w/ cb
+// overload: watching reactive object w/ cb
 export function watch<
   T extends object,
   Immediate extends Readonly<boolean> = false
