c46217b3c738691c7fcbf5fed3f4c7c7b8a86563
yanglinlin <yanglinlin@genomics.cn>
2021-01-28 14:37:05 +0800

    optimize escape html

--- packages/shared/src/escapeHtml.ts
+++ packages/shared/src/escapeHtml.ts
@@ -1,47 +1,22 @@
 const escapeRE = /["'&<>]/
 
-export function escapeHtml(string: unknown) {
-  const str = '' + string
-  const match = escapeRE.exec(str)
-
-  if (!match) {
-    return str
-  }
-
-  let html = ''
-  let escaped: string
-  let index: number
-  let lastIndex = 0
-  for (index = match.index; index < str.length; index++) {
-    switch (str.charCodeAt(index)) {
-      case 34: // "
-        escaped = '&quot;'
-        break
-      case 38: // &
-        escaped = '&amp;'
-        break
-      case 39: // '
-        escaped = '&#39;'
-        break
-      case 60: // <
-        escaped = '&lt;'
-        break
-      case 62: // >
-        escaped = '&gt;'
-        break
+export function escapeHtml(unsafeStr: string): string {
+  return unsafeStr.replace(escapeRE, char => {
+    switch (char) {
+      case '"':
+        return '&quot;'
+      case "'":
+        return '&#39;'
+      case '&':
+        return '&amp;'
+      case '<':
+        return '&lt;'
+      case '>':
+        return '&gt;'
       default:
-        continue
+        return char
     }
-
-    if (lastIndex !== index) {
-      html += str.substring(lastIndex, index)
-    }
-
-    lastIndex = index + 1
-    html += escaped
-  }
-
-  return lastIndex !== index ? html + str.substring(lastIndex, index) : html
+  })
 }
 
 // https://www.w3.org/TR/html52/syntax.html#comments
