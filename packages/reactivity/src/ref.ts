4ca4666d58ee8025570dc14f1c163bdeac9c6012
07akioni <07akioni2@gmail.com>
2021-02-04 02:12:51 +0800

    fix(toRef): ref created from union typed prop can't be used in watch (#3048)

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -20,7 +20,7 @@ export interface Ref<T = any> {
   _shallow?: boolean
 }
 
-export type ToRef<T> = T extends Ref ? T : Ref<UnwrapRef<T>>
+export type ToRef<T> = [T] extends [Ref] ? T : Ref<UnwrapRef<T>>
 export type ToRefs<T = any> = {
   // #2687: somehow using ToRef<T[K]> here turns the resulting type into
   // a union of multiple Ref<*> types instead of a single Ref<* | *> type.
