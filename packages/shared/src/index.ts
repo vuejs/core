c2e331c867d236e2c7f1e1f63f88ca77ed426cb3
MaYuanhai <414199639@qq.com>
2021-02-05 01:37:51 +0800

    update isString ,Avoid typeof new String(1231)
    Avoid the problem such as:

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -64,12 +64,11 @@ export const isSet = (val: unknown): val is Set<any> =>
   toTypeString(val) === '[object Set]'
 
 export const isDate = (val: unknown): val is Date => val instanceof Date
-export const isFunction = (val: unknown): val is Function =>
-  typeof val === 'function'
-export const isString = (val: unknown): val is string => typeof val === 'string'
-export const isSymbol = (val: unknown): val is symbol => typeof val === 'symbol'
+export const isFunction = (val: unknown): val is Function => toTypeString(val) === '[object Function]'
+export const isString = (val: unknown): val is string => toTypeString(val) === '[object String]'
+export const isSymbol = (val: unknown): val is symbol => toTypeString(val) === '[object Symbol]'
 export const isObject = (val: unknown): val is Record<any, any> =>
-  val !== null && typeof val === 'object'
+  val !== null && toTypeString(val) === '[object Object]'
 
 export const isPromise = <T = any>(val: unknown): val is Promise<T> => {
   return isObject(val) && isFunction(val.then) && isFunction(val.catch)
