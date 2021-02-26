f4594f02dd0c6e6600f0175c7b003d1cc00a4eed
Homyee King <HomyeeKing@gmail.com>
2021-02-26 10:46:22 +0800

    fix(script): yarn check logic

--- scripts/checkYarn.js
+++ scripts/checkYarn.js
@@ -1,4 +1,4 @@
-if (!/yarn\.js$/.test(process.env.npm_execpath || '')) {
+if (process.env.npm_execpath.indexOf('yarn') === -1) {
   console.warn(
     '\u001b[33mThis repository requires Yarn 1.x for scripts to work properly.\u001b[39m\n'
   )
