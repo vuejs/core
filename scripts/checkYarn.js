943917e8d09235ce256bb0e2c7e40d5f77643928
Homyee King <HomyeeKing@gmail.com>
2021-02-27 09:51:29 -0600

    Update scripts/checkYarn.js
    Co-authored-by: Eduardo San Martin Morote <posva@users.noreply.github.com>

--- scripts/checkYarn.js
+++ scripts/checkYarn.js
@@ -1,4 +1,4 @@
-if (process.env.npm_execpath.indexOf('yarn') === -1) {
+if (!/yarn(-[\d.]+)?\.c?js$/.test(process.env.npm_execpath || '')) {
   console.warn(
     '\u001b[33mThis repository requires Yarn 1.x for scripts to work properly.\u001b[39m\n'
   )
