03283ccd3c3c748c43f46687d54af8d6a5ddcd92
shen-zhao <616510680@qq.com>
2021-03-11 18:36:14 +0800

    chore(runtime-core): change the position of the mountChildren parameter

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -866,8 +866,8 @@ function baseCreateRenderer(
     parentComponent,
     parentSuspense,
     isSVG,
-    optimized,
     slotScopeIds,
+    optimized,
     start = 0
   ) => {
     for (let i = start; i < children.length; i++) {
@@ -882,8 +882,8 @@ function baseCreateRenderer(
         parentComponent,
         parentSuspense,
         isSVG,
-        optimized,
-        slotScopeIds
+        slotScopeIds,
+        optimized
       )
     }
   }
