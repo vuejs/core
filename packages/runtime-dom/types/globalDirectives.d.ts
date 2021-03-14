200838d6edfb0f59f08e2bf69a463e433a05648b
pikax <carlos@hypermob.co.uk>
2021-03-14 09:23:23 +0000

    chore: correct interface with file name

--- packages/runtime-dom/types/globalDirectives.d.ts
+++ packages/runtime-dom/types/globalDirectives.d.ts
@@ -1,10 +1,8 @@
 // Note: this file is auto concatenated to the end of the bundled d.ts during
 // build.
 
-import { DefineComponent } from '@vue/runtime-core'
 declare module '@vue/runtime-core' {
-  interface GlobalComponents {
-    Transition: DefineComponent<TransitionProps>
-    TransitionGroup: DefineComponent<TransitionGroupProps>
+  interface GlobalDirectives {
+    vShow: typeof vShow
   }
 }
