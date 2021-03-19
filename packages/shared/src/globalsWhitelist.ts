cab0704addce0235e2f454e1472e39db6db5e985
zhangenming <282126346@qq.com>
2021-03-19 17:25:20 +0800

    add console to Whitelist, for ease of debugging

--- packages/shared/src/globalsWhitelist.ts
+++ packages/shared/src/globalsWhitelist.ts
@@ -3,6 +3,6 @@ import { makeMap } from './makeMap'
 const GLOBALS_WHITE_LISTED =
   'Infinity,undefined,NaN,isFinite,isNaN,parseFloat,parseInt,decodeURI,' +
   'decodeURIComponent,encodeURI,encodeURIComponent,Math,Number,Date,Array,' +
-  'Object,Boolean,String,RegExp,Map,Set,JSON,Intl,BigInt'
+  'Object,Boolean,String,RegExp,Map,Set,JSON,Intl,BigInt,console'
 
 export const isGloballyWhitelisted = /*#__PURE__*/ makeMap(GLOBALS_WHITE_LISTED)
