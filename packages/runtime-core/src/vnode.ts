a641eb201fe51620d50884b988f6fefc3e21a20b
HcySunYang <HcySunYang@outlook.com>
2021-05-28 05:06:55 +0800

    fix(runtime-core): fix cases of reused children arrays in render functions (#3670)
    fix #3666

--- packages/runtime-core/src/vnode.ts
+++ packages/runtime-core/src/vnode.ts
@@ -603,11 +603,16 @@ export function normalizeVNode(child: VNodeChild): VNode {
     return createVNode(Comment)
   } else if (isArray(child)) {
     // fragment
-    return createVNode(Fragment, null, child)
+    return createVNode(
+      Fragment,
+      null,
+      // #3666, avoid reference pollution when reusing vnode
+      child.slice()
+    )
   } else if (typeof child === 'object') {
     // already vnode, this should be the most common since compiled templates
     // always produce all-vnode children arrays
-    return child.el === null ? child : cloneVNode(child)
+    return cloneIfMounted(child)
   } else {
     // strings and numbers
     return createVNode(Text, null, String(child))
