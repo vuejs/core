a262e776a8759ff235f8fdf625ee1aa3eb8d0358
daiwei <daiwei521@126.com>
2021-05-31 09:29:29 +0800

    fix(runtime-core): deepMergeData corrently

--- packages/runtime-core/src/compat/data.ts
+++ packages/runtime-core/src/compat/data.ts
@@ -7,14 +7,14 @@ export function deepMergeData(
   to: any,
   from: any,
   instance: ComponentInternalInstance
-) {
+): any {
   for (const key in from) {
     const toVal = to[key]
     const fromVal = from[key]
     if (key in to && isPlainObject(toVal) && isPlainObject(fromVal)) {
       __DEV__ &&
         warnDeprecation(DeprecationTypes.OPTIONS_DATA_MERGE, instance, key)
-      deepMergeData(toVal, fromVal, instance)
+      return deepMergeData(toVal, fromVal, instance)
     } else {
       to[key] = fromVal
     }
