4db4101aebfd7f0b72825b3d8f8105f70907d116
webfansplz <>
2021-07-14 23:06:04 +0800

    fix(types): improve type of provide()

--- packages/runtime-core/src/apiInject.ts
+++ packages/runtime-core/src/apiInject.ts
@@ -5,7 +5,7 @@ import { warn } from './warning'
 
 export interface InjectionKey<T> extends Symbol {}
 
-export function provide<T>(key: InjectionKey<T> | string | number, value: T) {
+export function provide<T>(key: InjectionKey<T> | string, value: T) {
   if (!currentInstance) {
     if (__DEV__) {
       warn(`provide() can only be used inside setup().`)
