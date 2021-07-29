5b135726670d6e25dadb3e57c57f1d65eb80d591
bas <bas@planning.nl>
2021-07-29 12:51:47 +0200

    feat(effectScope): fix test

--- packages/reactivity/__tests__/effectScope.spec.ts
+++ packages/reactivity/__tests__/effectScope.spec.ts
@@ -77,8 +77,9 @@ describe('reactivity/effect/scope', () => {
       })
     })
 
-    expect(scope.effects.length).toBe(2)
-    expect(scope.effects[1]).toBeInstanceOf(EffectScope)
+    expect(scope.effects.length).toBe(1)
+    expect(scope.scopes.length).toBe(1)
+    expect(scope.scopes[0]).toBeInstanceOf(EffectScope)
 
     expect(dummy).toBe(0)
     counter.num = 7
