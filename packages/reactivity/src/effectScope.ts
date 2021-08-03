cdc7ba6a13da76eea38af683a332d03c440f436a
webfansplz <>
2021-08-03 17:31:50 +0800

    fix(effectScope): warn format

--- packages/reactivity/src/effectScope.ts
+++ packages/reactivity/src/effectScope.ts
@@ -98,8 +98,7 @@ export function onScopeDispose(fn: () => void) {
     activeEffectScope.cleanups.push(fn)
   } else if (__DEV__) {
     warn(
-      `onDispose() is called when there is no active effect scope ` +
-        ` to be associated with.`
+      `onDispose() is called when there is no active effect scope to be associated with.`
     )
   }
 }
