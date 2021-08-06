5b44e50423005b95d7d2068e6abccadb9084675c
caimengyong <caimengyong@baidu.com>
2021-08-06 12:00:10 +0800

    fix: fixed is Ref not judge instance

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -72,7 +72,10 @@ const convert = <T extends unknown>(val: T): T =>
 
 export function isRef<T>(r: Ref<T> | unknown): r is Ref<T>
 export function isRef(r: any): r is Ref {
-  return Boolean(r && r.__v_isRef === true)
+  return (
+    Boolean(r && r.__v_isRef === true) &&
+    (r instanceof RefImpl || r instanceof CustomRefImpl)
+  )
 }
 
 export function ref<T extends object>(value: T): ToRef<T>
