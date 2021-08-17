d8da4214fc280e2f56b314f8ba2d31ac3c330301
zhangenming <282126346@qq.com>
2021-08-18 00:11:52 +0800

    refactor(reactivity): reduce variable access (#4366)

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -221,9 +221,8 @@ export function toRef<T extends object, K extends keyof T>(
   object: T,
   key: K
 ): ToRef<T[K]> {
-  return isRef(object[key])
-    ? object[key]
-    : (new ObjectRefImpl(object, key) as any)
+  const val = object[key]
+  return isRef(val) ? val : (new ObjectRefImpl(object, key) as any)
 }
 
 // corner case when use narrows type
