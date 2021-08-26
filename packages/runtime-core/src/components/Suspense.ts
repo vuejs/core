162ab918e8f86ead49eb45856764a0e6c11ebd83
liximomo <liximomo@gmail.com>
2021-08-26 17:20:30 +0800

    fix: misuse Dom's Comment element constructor as a vnode type

--- packages/runtime-core/src/components/Suspense.ts
+++ packages/runtime-core/src/components/Suspense.ts
@@ -6,6 +6,7 @@ import {
   openBlock,
   closeBlock,
   currentBlock,
+  Comment,
   createVNode
 } from '../vnode'
 import { isFunction, isArray, ShapeFlags, toNumber } from '@vue/shared'
@@ -87,7 +88,9 @@ export const SuspenseImpl = {
 }
 
 // Force-casted public typing for h and TSX props inference
-export const Suspense = (__FEATURE_SUSPENSE__ ? SuspenseImpl : null) as any as {
+export const Suspense = ((__FEATURE_SUSPENSE__
+  ? SuspenseImpl
+  : null) as any) as {
   __isSuspense: true
   new (): { $props: VNodeProps & SuspenseProps }
 }
@@ -518,8 +521,13 @@ function createSuspenseBoundary(
         return
       }
 
-      const { vnode, activeBranch, parentComponent, container, isSVG } =
-        suspense
+      const {
+        vnode,
+        activeBranch,
+        parentComponent,
+        container,
+        isSVG
+      } = suspense
 
       // invoke @fallback event
       triggerEvent(vnode, 'onFallback')
