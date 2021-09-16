8370118c1394250a6d4ca8f4752ba4ec975f94ac
Wouter Kettlitz <W.Kettlitz@pm.me>
2021-09-16 17:46:41 +0200

    types: extend CSSProperties with PropertiesHyphen (#4553)
    fix #4552

--- packages/runtime-dom/types/jsx.d.ts
+++ packages/runtime-dom/types/jsx.d.ts
@@ -29,7 +29,7 @@
 import { VNode } from '@vue/runtime-core'
 import * as CSS from 'csstype'
 
-export interface CSSProperties extends CSS.Properties<string | number> {
+export interface CSSProperties extends CSS.Properties<string | number>, CSS.PropertiesHyphen<string | number> {
   /**
    * The index signature was removed to enable closed typing for style
    * using CSSType. You're able to use type assertion or module augmentation
