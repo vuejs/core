a0cacc0d83a658e291a2962f9a3b31f227aa870f
Che Guevara <836934184@qq.com>
2021-09-21 12:12:20 -0500

    chore: fix warning in ref-transform (#4643) [ci skip]

--- packages/ref-transform/src/refTransform.ts
+++ packages/ref-transform/src/refTransform.ts
@@ -466,6 +466,6 @@ function warnOnce(msg: string) {
 
 function warn(msg: string) {
   console.warn(
-    `\x1b[1m\x1b[33m[@vue/compiler-sfc]\x1b[0m\x1b[33m ${msg}\x1b[0m\n`
+    `\x1b[1m\x1b[33m[@vue/ref-transform]\x1b[0m\x1b[33m ${msg}\x1b[0m\n`
   )
 }
