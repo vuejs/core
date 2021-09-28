vuejs/vue-next

feat(props): configured prop argument to readonly

packages/runtime-core/src/componentProps.ts
@@ -2,7 +2,8 @@ import {
   toRaw,
   shallowReactive,
   trigger,
-  TriggerOpTypes
+  TriggerOpTypes,
+  shallowReadonly
 } from '@vue/reactivity'
 import {
   EMPTY_OBJ,
@@ -466,7 +467,13 @@ function validateProps(props: Data, instance: ComponentInternalInstance) {
   for (const key in options) {
     let opt = options[key]
     if (opt == null) continue
-    validateProp(key, rawValues[key], opt, rawValues, !hasOwn(rawValues, key))
+    validateProp(
+      key,
+      rawValues[key],
+      opt,
+      shallowReadonly(rawValues),
+      !hasOwn(rawValues, key)
+    )
   }
 }
 

vuejs/core

packages/runtime-core/src/componentProps.ts
@@ -2,7 +2,8 @@ import {
   toRaw,
   shallowReactive,
   trigger,
-  TriggerOpTypes
+  TriggerOpTypes,
+  shallowReadonly
 } from '@vue/reactivity'
 import {
   EMPTY_OBJ,
@@ -575,7 +576,7 @@ function validateProps(
       key,
       resolvedValues[key],
       opt,
-      resolvedValues
+      shallowReadonly(resolvedValues),
       !hasOwn(rawProps, key) && !hasOwn(rawProps, hyphenate(key))
     )
   }