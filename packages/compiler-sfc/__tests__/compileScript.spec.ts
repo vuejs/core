072e27929c567e0dc00af94be6c5c55daaa70e98
daiwei <daiwei521@126.com>
2021-09-28 22:44:56 +0800

    feat(compiler-sfc): support runtime Enum with normal script

--- packages/compiler-sfc/__tests__/compileScript.spec.ts
+++ packages/compiler-sfc/__tests__/compileScript.spec.ts
@@ -1042,6 +1042,26 @@ const emit = defineEmits(['a', 'b'])
       })
     })
 
+    test('runtime Enum w/ normal script', () => {
+      const { content, bindings } = compile(
+        `<script lang="ts">
+          export enum D { D = "D" }
+          const enum C { C = "C" }
+          enum B { B = "B" }
+        </script>
+        <script setup lang="ts">
+        enum Foo { A = 123 }
+        </script>`
+      )
+      assertCode(content)
+      expect(bindings).toStrictEqual({
+        D: BindingTypes.SETUP_CONST,
+        C: BindingTypes.SETUP_CONST,
+        B: BindingTypes.SETUP_CONST,
+        Foo: BindingTypes.SETUP_CONST
+      })
+    })
+
     test('const Enum', () => {
       const { content, bindings } = compile(
         `<script setup lang="ts">
--- packages/compiler-sfc/src/compileScript.ts
+++ packages/compiler-sfc/src/compileScript.ts
@@ -840,7 +840,8 @@ export function compileScript(
       } else if (
         (node.type === 'VariableDeclaration' ||
           node.type === 'FunctionDeclaration' ||
-          node.type === 'ClassDeclaration') &&
+          node.type === 'ClassDeclaration' ||
+          node.type === 'TSEnumDeclaration') &&
         !node.declare
       ) {
         walkDeclaration(node, scriptBindings, userImportAlias)
@@ -1510,6 +1511,7 @@ function walkDeclaration(
       }
     }
   } else if (
+    node.type === 'TSEnumDeclaration' ||
     node.type === 'FunctionDeclaration' ||
     node.type === 'ClassDeclaration'
   ) {
