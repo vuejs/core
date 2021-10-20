029d92fdf20b99ae22745609b1486eb97668f942
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2021-10-20 11:51:44 +0200

    fix(runtime-core): fix warning for missing handler
    event handler props can be defined in camel and kebab cases
    
    Signed-off-by: Benjamin Kroeger <benjamin.kroeger@gmail.com>

--- packages/runtime-core/src/componentEmits.ts
+++ packages/runtime-core/src/componentEmits.ts
@@ -89,10 +89,18 @@ export function emit(
             event.startsWith(compatModelEventPrefix))
         )
       ) {
-        if (!propsOptions || !(toHandlerKey(event) in propsOptions)) {
+        if (
+          !propsOptions ||
+          !(
+            toHandlerKey(event) in propsOptions ||
+            toHandlerKey(camelize(event)) in propsOptions
+          )
+        ) {
           warn(
             `Component emitted event "${event}" but it is neither declared in ` +
-              `the emits option nor as an "${toHandlerKey(event)}" prop.`
+              `the emits option nor as an "${toHandlerKey(
+                event
+              )}" or "${toHandlerKey(camelize(event))}" prop.`
           )
         }
       } else {
