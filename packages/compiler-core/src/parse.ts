0cfc79196de2c32e1fa18b8492a536eebc773e41
zhangenming <282126346@qq.com>
2021-12-16 16:54:59 +0800

    .

--- packages/compiler-core/src/parse.ts
+++ packages/compiler-core/src/parse.ts
@@ -148,11 +148,11 @@ function parseChildren(
   const parent = last(ancestors)
   const ns = parent ? parent.ns : Namespaces.HTML
   const nodes: TemplateChildNode[] = []
+  const s = context.source
+  let node: TemplateChildNode | TemplateChildNode[] | undefined = undefined
 
   while (!isEnd(context, mode, ancestors)) {
     __TEST__ && assert(context.source.length > 0)
-    const s = context.source
-    let node: TemplateChildNode | TemplateChildNode[] | undefined = undefined
 
     if (mode === TextModes.DATA || mode === TextModes.RCDATA) {
       if (!context.inVPre && startsWith(s, context.options.delimiters[0])) {
