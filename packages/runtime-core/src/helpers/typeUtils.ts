e5c9c8f2d1e2780abd17474f183f7ebdc6f8c8af
johnsoncodehk <johnsoncodehk@gmail.com>
2021-12-25 00:33:22 +0800

    feat(types): fix `LooseRequired` not working

--- packages/runtime-core/src/helpers/typeUtils.ts
+++ packages/runtime-core/src/helpers/typeUtils.ts
@@ -5,7 +5,6 @@ export type UnionToIntersection<U> = (
   : never
 
 // make keys required but keep undefined values
-// use `keyof Pick<T, string & keyof T>` instead of `string & keyof T` to support IDE features
-export type LooseRequired<T> = { [P in keyof Pick<T, string & keyof T>]: T[P] }
+export type LooseRequired<T> = { [P in string & keyof T]: T[P] }
 
 export type IfAny<T, Y, N> = 0 extends (1 & T) ? Y : N
