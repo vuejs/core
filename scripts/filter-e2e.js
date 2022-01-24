c16421727232642c02843e757a4b1296dab99b69
ygj6 <yuangongji@foxmail.com>
2022-01-24 16:49:10 +0800

    chore: Format file paths in filter-e2e.

--- scripts/filter-e2e.js
+++ scripts/filter-e2e.js
@@ -1,9 +1,10 @@
 const e2eTests = ['/Transition', '/TransitionGroup', '/examples/']
+const path = require('path')
 
 module.exports = list => {
   return {
     filtered: list
-      .filter(t => e2eTests.some(tt => t.includes(tt)))
+      .filter(t => e2eTests.some(tt => t.includes(path.normalize(tt))))
       .map(test => ({ test }))
   }
 }
