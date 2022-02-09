ac58c7377da929f0c5f7dc2952298d8f832fa3c4
daiwei <daiwei521@126.com>
2022-02-09 09:24:22 +0800

    test: add test case

--- packages/runtime-core/src/apiWatch.ts
+++ packages/runtime-core/src/apiWatch.ts
@@ -212,7 +212,7 @@ function doWatch(
     deep = true
   } else if (isArray(source)) {
     isMultiSource = true
-    forceTrigger = source.some(s=>isReactive(s) || isShallow(s))
+    forceTrigger = source.some(s => isReactive(s) || isShallow(s))
     getter = () =>
       source.map(s => {
         if (isRef(s)) {
