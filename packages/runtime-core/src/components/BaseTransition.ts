e8dee0bcd9a080445fefd19407f98ba578516286
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2022-04-22 14:43:33 +1000

    fix(runtime-core): avoid transition child key of number type duplication

--- packages/runtime-core/src/components/BaseTransition.ts
+++ packages/runtime-core/src/components/BaseTransition.ts
@@ -494,10 +494,17 @@ export function getTransitionRawChildren(
   for (let i = 0; i < children.length; i++) {
     let child = children[i]
     // #5360 inherit parent key in case of <template v-for>
+    // #5761: when child.key is a number, it would be potentially
+    // duplicated with index i. In this case, ignore whatever existing
+    // number type child key (e.g. v-if node could have dedicated key based on branchs).
+    // Make it completely based on index i in children list to avoid duplication
     const key =
       parentKey == null
         ? child.key
-        : String(parentKey) + String(child.key != null ? child.key : i)
+        : String(parentKey) +
+          String(
+            child.key != null && typeof child.key !== 'number' ? child.key : i
+          )
     // handle fragment children case, e.g. v-for
     if (child.type === Fragment) {
       if (child.patchFlag & PatchFlags.KEYED_FRAGMENT) keyedFragmentCount++
