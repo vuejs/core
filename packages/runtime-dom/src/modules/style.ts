8c1e3facf22d90c5a10d3c8859c26e8690914edf
czh740132583 <740132583@qq.com>
2022-05-10 22:38:32 +0800

    Revert "fix(runtime-dom): Fixed a runtime error when the element node's style binding array was not compiled as 'createStaticVNode'. (fix #5106)"

--- packages/runtime-dom/src/modules/style.ts
+++ packages/runtime-dom/src/modules/style.ts
@@ -7,8 +7,15 @@ export function patchStyle(el: Element, prev: Style, next: Style) {
   const style = (el as HTMLElement).style
   const isCssString = isString(next)
   if (next && !isCssString) {
-    for (const key in next) {
-      setStyle(style, key, next[key])
+    // #5106
+    if(Array.isArray(next)){
+      next.forEach(val=>{
+        patchStyle(el,prev,val)
+      })
+    }else{
+      for (const key in next) {
+        setStyle(style, key, next[key])
+      }
     }
     if (prev && !isString(prev)) {
       for (const key in prev) {
