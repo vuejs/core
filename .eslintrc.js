83aaa9b5469072f43314b11388e5985b77597acd
Evan You <yyx990803@gmail.com>
2022-05-10 10:43:07 +0800

    workflow: update eslint config to allow spread usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,10 +16,11 @@ module.exports = {
     // most of the codebase are expected to be env agnostic
     'no-restricted-globals': ['error', ...DOMGlobals, ...NodeGlobals],
     // since we target ES2015 for baseline support, we need to forbid object
-    // rest spread usage (both assign and destructure)
+    // rest spread usage in destructure as it compiles into a verbose helper.
+    // TS now compiles assignment spread into Object.assign() calls so that
+    // is allowed.
     'no-restricted-syntax': [
       'error',
-      'ObjectExpression > SpreadElement',
       'ObjectPattern > RestElement',
       'AwaitExpression'
     ]
