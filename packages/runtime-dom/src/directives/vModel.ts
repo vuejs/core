a9c0c5d365bef2686237f233b45f7c88fb87671a
liulinboyi <814921718@qq.com>
2022-05-10 16:46:50 +0800

    fix(runtime-dom): v-model.lazy use with range does not update DOM element when after Vue model change

--- packages/runtime-dom/src/directives/vModel.ts
+++ packages/runtime-dom/src/directives/vModel.ts
@@ -76,10 +76,11 @@ export const vModelText: ModelDirective<
   },
   beforeUpdate(el, { value, modifiers: { lazy, trim, number } }, vnode) {
     el._assign = getModelAssigner(vnode)
+    const type = vnode.props?.type
     // avoid clearing unresolved text. #2302
     if ((el as any).composing) return
     if (document.activeElement === el) {
-      if (lazy) {
+      if (type !== 'range' && lazy) {
         return
       }
       if (trim && el.value.trim() === value) {
