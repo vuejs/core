2bab63968355995a4026cf5cd1ccad7c79e8d89c
Evan You <yyx990803@gmail.com>
2022-05-11 18:02:31 +0800

    fix(runtime-core): fix directive inheritance on dev root fragment
    fix #5523

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -1098,6 +1098,8 @@ function baseCreateRenderer(
       if (
         patchFlag > 0 &&
         patchFlag & PatchFlags.STABLE_FRAGMENT &&
+        // #5523 dev root fragment may inherit directives so always force update
+        !(__DEV__ && patchFlag & PatchFlags.DEV_ROOT_FRAGMENT) &&
         dynamicChildren &&
         // #2715 the previous fragment could've been a BAILed one as a result
         // of renderSlot() with no valid children
