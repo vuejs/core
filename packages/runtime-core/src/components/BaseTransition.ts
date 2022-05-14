ffcc524ab90a9128ff731a3923a1f497394c3a98
liulinboyi <814921718@qq.com>
2022-05-15 00:32:49 +0800

    feat(runtime-core): Error on showing element before delayed hidding transition

--- packages/runtime-core/src/components/BaseTransition.ts
+++ packages/runtime-core/src/components/BaseTransition.ts
@@ -433,6 +433,10 @@ export function resolveTransitionHooks(
           delete leavingVNodesCache[key]
         }
       })
+      if (!vnode.el) {
+        vnode = cloneVNode(vnode)
+        vnode.el = el
+      }
       leavingVNodesCache[key] = vnode
       if (onLeave) {
         callAsyncHook(onLeave, [el, done])
