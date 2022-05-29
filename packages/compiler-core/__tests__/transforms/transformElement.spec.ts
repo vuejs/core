52dc796a7ab6f56f2609bd1513e7b192182060c1
amirkian007 <68250433+amirkian007@users.noreply.github.com>
2022-05-29 22:42:00 +0430

    chore: add tests to check KeepAlive multi children

--- packages/compiler-core/__tests__/transforms/transformElement.spec.ts
+++ packages/compiler-core/__tests__/transforms/transformElement.spec.ts
@@ -1224,4 +1224,55 @@ describe('compiler: element transform', () => {
       isBlock: false
     })
   })
+  
+  describe('<KeepAlive> multiple children', () => {
+
+    function checkWarning(
+      template: string,
+      shouldWarn: boolean,
+      message = '<KeepAlive> expects exactly one child component.'
+    ) {
+      const spy = jest.fn()
+
+      parseWithBind(template.trim(), {
+        onError: err => {
+          spy(err.message)
+        }
+      })
+
+      if (shouldWarn) expect(spy).toHaveBeenCalledWith(message)
+      else expect(spy).not.toHaveBeenCalled()
+    }
+
+    test('does not warn if has one children', () => {
+      checkWarning(
+        `<KeepAlive>
+           <component :is="activeComponent"/>
+         </KeepAlive>`,
+        false
+      )
+    })
+
+    test('warn if has multiple children', () => {
+      checkWarning(
+        `<KeepAlive>
+          <component :is="activeComponent"/>
+          <component :is="activeComponent2"/>
+         </KeepAlive>`,
+        true
+      )
+    })
+
+    test('should ignore comments', () => {
+      expect(baseCompile(
+        `<KeepAlive>
+          <!--this should be ignored -->
+          <component :is="activeComponent"/>
+          <!--this should be ignored -->
+          </KeepAlive>`,
+          {prefixIdentifiers: true}
+      ).code).toMatchSnapshot()
+    })
+  })
+  
 })
