28c0a97cca65b70a0e33777b43e2f51f8075572b
qmhc <40221744+qmhc@users.noreply.github.com>
2022-07-07 14:55:37 +0800

    ci: correct filter unit tests in windows system

--- scripts/filter-unit.js
+++ scripts/filter-unit.js
@@ -1,9 +1,10 @@
+const path = require('path')
 const { e2eTests } = require('./filter-e2e')
 
 module.exports = list => {
   return {
     filtered: list
-      .filter(t => !e2eTests.some(tt => t.includes(tt)))
+      .filter(t => !e2eTests.some(tt => t.includes(path.normalize(tt))))
       .map(test => ({ test }))
   }
 }
