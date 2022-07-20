8ae7825da54703150f1f9fc03a717e5637f3b272
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2022-07-20 14:33:13 +0300

    chore: add `hgroup` to supported `HTML_TAGS`

--- packages/shared/src/domTagConfig.ts
+++ packages/shared/src/domTagConfig.ts
@@ -5,7 +5,7 @@ import { makeMap } from './makeMap'
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element
 const HTML_TAGS =
   'html,body,base,head,link,meta,style,title,address,article,aside,footer,' +
-  'header,h1,h2,h3,h4,h5,h6,nav,section,div,dd,dl,dt,figcaption,' +
+  'header,hgroup,h1,h2,h3,h4,h5,h6,nav,section,div,dd,dl,dt,figcaption,' +
   'figure,picture,hr,img,li,main,ol,p,pre,ul,a,b,abbr,bdi,bdo,br,cite,code,' +
   'data,dfn,em,i,kbd,mark,q,rp,rt,ruby,s,samp,small,span,strong,sub,sup,' +
   'time,u,var,wbr,area,audio,map,track,video,embed,object,param,source,' +
