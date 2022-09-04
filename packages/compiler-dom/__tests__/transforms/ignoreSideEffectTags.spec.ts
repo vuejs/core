02240cca1449bc34b1aa23fe03338bf931351480
azaleta <24407500@qq.com>
2022-09-04 11:36:25 +0800

    feat(ignoreSideEffectTags): support style  in svg

--- packages/compiler-dom/__tests__/transforms/ignoreSideEffectTags.spec.ts
+++ packages/compiler-dom/__tests__/transforms/ignoreSideEffectTags.spec.ts
@@ -24,4 +24,30 @@ describe('compiler: ignore side effect tags', () => {
     expect(err).toBeDefined()
     expect(err!.message).toMatch(`Tags with side effect`)
   })
+
+  it('should not ignore style in svg', () => {
+    let err: CompilerError | undefined
+    const { code } = compile(
+      `<svg><g/><style>h1 { color: red }</style></svg>`,
+      {
+        onError(e) {
+          err = e
+        }
+      }
+    )
+    expect(code).toMatch('style')
+    expect(err).toBeUndefined()
+  })
+
+  it('should ignore style not in svg', () => {
+    let err: CompilerError | undefined
+    const { code } = compile(`<div><style>h1 { color: red }</style></div>`, {
+      onError(e) {
+        err = e
+      }
+    })
+    expect(code).not.toMatch('style')
+    expect(err).toBeDefined()
+    expect(err!.message).toMatch(`Tags with side effect`)
+  })
 })
--- packages/compiler-dom/src/transforms/ignoreSideEffectTags.ts
+++ packages/compiler-dom/src/transforms/ignoreSideEffectTags.ts
@@ -2,10 +2,13 @@ import { NodeTransform, NodeTypes, ElementTypes } from '@vue/compiler-core'
 import { DOMErrorCodes, createDOMCompilerError } from '../errors'
 
 export const ignoreSideEffectTags: NodeTransform = (node, context) => {
+  const inSvg = context.parent
+    ? context.parent.type === NodeTypes.ELEMENT && context.parent.tag === 'svg'
+    : false
   if (
     node.type === NodeTypes.ELEMENT &&
     node.tagType === ElementTypes.ELEMENT &&
-    (node.tag === 'script' || node.tag === 'style')
+    (node.tag === 'script' || (node.tag === 'style' && !inSvg))
   ) {
     context.onError(
       createDOMCompilerError(DOMErrorCodes.X_IGNORED_SIDE_EFFECT_TAG, node.loc)
