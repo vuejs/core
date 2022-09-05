4d1a818ad76bad6be50f1d92fcdf3a682a427268
Tom Nijmeijer <tom.nijmeijer@nedap.com>
2022-09-05 15:06:08 +0200

    wip: Add imported components to runtimeOptions while compiling sfc's

--- packages/compiler-sfc/src/compileScript.ts
+++ packages/compiler-sfc/src/compileScript.ts
@@ -270,6 +270,7 @@ export function compileScript(
   // metadata that needs to be returned
   const bindingMetadata: BindingMetadata = {}
   const helperImports: Set<string> = new Set()
+  const componentImports: Set<string> = new Set()
   const userImports: Record<string, ImportBinding> = Object.create(null)
   const userImportAlias: Record<string, string> = Object.create(null)
   const scriptBindings: Record<string, BindingTypes> = Object.create(null)
@@ -363,6 +364,9 @@ export function compileScript(
   ) {
     if (source === 'vue' && imported) {
       userImportAlias[imported] = local
+    }
+    if (source.endsWith('.vue')) {
+      componentImports.add(local)
     }
 
     // template usage check is only needed in non-inline mode, so we can skip
@@ -1494,6 +1498,8 @@ export function compileScript(
   } else if (propsTypeDecl) {
     runtimeOptions += genRuntimeProps(typeDeclaredProps)
   }
+  const components = Array.from(componentImports).join(", ")
+  runtimeOptions += `\n  components: { ${components} },`
   if (emitsRuntimeDecl) {
     runtimeOptions += `\n  emits: ${scriptSetup.content
       .slice(emitsRuntimeDecl.start!, emitsRuntimeDecl.end!)
