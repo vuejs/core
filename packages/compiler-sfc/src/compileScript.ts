19a84f89d6ca5d1529b7314c1330c63fd2d1a587
Travis <godxiaoji@163.com>
2022-09-12 12:25:23 +0800

    fix(compiler-sfc): Resolve object expression parsing errors in `v-on`.

--- packages/compiler-sfc/src/compileScript.ts
+++ packages/compiler-sfc/src/compileScript.ts
@@ -2135,7 +2135,7 @@ function processExp(exp: string, dir?: string): string {
     if (dir === 'slot') {
       exp = `(${exp})=>{}`
     } else if (dir === 'on') {
-      exp = `()=>{${exp}}`
+      exp = `()=>{return ${exp}}`
     } else if (dir === 'for') {
       const inMatch = exp.match(forAliasRE)
       if (inMatch) {
