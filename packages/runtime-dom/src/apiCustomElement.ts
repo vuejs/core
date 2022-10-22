b0b74a160c08941fa9a7a5460f36a1f2fccbf423
Thorsten Lünborg <t.luenborg@googlemail.com>
2022-10-22 11:20:46 +0200

    fix(runtime-core): custom-element: ensure number casting of camelCase props. (fix: #5374) (#5377)

--- packages/runtime-dom/src/apiCustomElement.ts
+++ packages/runtime-dom/src/apiCustomElement.ts
@@ -268,10 +268,11 @@ export class VueElement extends BaseClass {
 
   protected _setAttr(key: string) {
     let value = this.getAttribute(key)
-    if (this._numberProps && this._numberProps[key]) {
+    const camelKey = camelize(key)
+    if (this._numberProps && this._numberProps[camelKey]) {
       value = toNumber(value)
     }
-    this._setProp(camelize(key), value, false)
+    this._setProp(camelKey, value, false)
   }
 
   /**
