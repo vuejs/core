00bbd29285f508ac03f7d45e52d6d5a2f6dca4d3
Evan You <yyx990803@gmail.com>
2022-11-07 22:36:46 -0500

    Update normalizeProp.ts

--- packages/shared/src/normalizeProp.ts
+++ packages/shared/src/normalizeProp.ts
@@ -32,12 +32,15 @@ const styleCommentRE = /\/\*.*?\*\//gs
 
 export function parseStringStyle(cssText: string): NormalizedStyle {
   const ret: NormalizedStyle = {}
-  cssText.replace(styleCommentRE, '').split(listDelimiterRE).forEach(item => {
-    if (item) {
-      const tmp = item.split(propertyDelimiterRE)
-      tmp.length > 1 && (ret[tmp[0].trim()] = tmp[1].trim())
-    }
-  })
+  cssText
+    .replace(styleCommentRE, '')
+    .split(listDelimiterRE)
+    .forEach(item => {
+      if (item) {
+        const tmp = item.split(propertyDelimiterRE)
+        tmp.length > 1 && (ret[tmp[0].trim()] = tmp[1].trim())
+      }
+    })
   return ret
 }
 
