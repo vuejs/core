eed1f97a6eef448f329202fc207c90ebf2ef1748
Jason Yu <me@ycmjason.com>
2022-11-08 17:08:56 +0000

    fix(types): readonly is not respected in reactive / toRef(s)  (#5159)
    - emit `Readonly<Ref<T>>` accordingly in `toRef(s)`
    - emit `readonly [key]: value` accordingly in `reactive`
    - add test helper `Equals` which assert equality of `Readonly<T>` vs
      `T`
    
    fixes #5159

--- packages/shared/src/typeUtils.ts
+++ packages/shared/src/typeUtils.ts
@@ -10,3 +10,21 @@ export type LooseRequired<T> = { [P in keyof (T & Required<T>)]: T[P] }
 // If the type T accepts type "any", output type Y, otherwise output type N.
 // https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
 export type IfAny<T, Y, N> = 0 extends 1 & T ? Y : N
+
+// https://stackoverflow.com/a/52473108/3570903
+export type IsEqual<A1 extends any, A2 extends any> = (<A>() => A extends A2
+  ? true
+  : false) extends <A>() => A extends A1 ? true : false
+  ? true
+  : false
+
+export type IsReadonlyKey<O, K extends keyof O> = IsEqual<
+  { [L in K]: O[L] },
+  { readonly [L in K]: O[L] }
+>
+
+export type MarkReadonly<O, K extends keyof O> = {
+  [L in keyof ({ [M in Exclude<keyof O, K>]: unknown } & {
+    readonly [M in K]: unknown
+  })]: O[L]
+}
