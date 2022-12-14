31a1bdcf124be3647337530217ac9d5e8e74eb69
Thorsten Lünborg <t.luenborg@googlemail.com>
2022-12-14 13:55:41 +0100

    Update packages/runtime-core/src/apiAsyncComponent.ts
    Co-authored-by: 三咲智子 Kevin Deng <sxzz@sxzz.moe>

--- packages/runtime-core/src/apiAsyncComponent.ts
+++ packages/runtime-core/src/apiAsyncComponent.ts
@@ -38,7 +38,7 @@ export interface AsyncComponentOptions<T = any> {
 }
 
 export const isAsyncWrapper = (i: ComponentInternalInstance | VNode): boolean =>
-  i && i.type ? !!(i.type as ComponentOptions).__asyncLoader : false
+  !!(i && i.type && (i.type as ComponentOptions).__asyncLoader)
 
 export function defineAsyncComponent<
   T extends Component = { new (): ComponentPublicInstance }
