b7cfa6f53952daced312862fbb3a88c86e42a77e
yurj26 <54402889+yurj26@users.noreply.github.com>
2023-01-09 22:16:08 +0800

    fix(runtime-dom): style update error when component use shorthand properties (#7425)
    * fix(runtime-dom): style update error when component use shorthand properties
    
    * test(runtime-dom): style update with shorthand properties

--- packages/runtime-dom/src/modules/style.ts
+++ packages/runtime-dom/src/modules/style.ts
@@ -7,15 +7,15 @@ export function patchStyle(el: Element, prev: Style, next: Style) {
   const style = (el as HTMLElement).style
   const isCssString = isString(next)
   if (next && !isCssString) {
-    for (const key in next) {
-      setStyle(style, key, next[key])
-    }
     if (prev && !isString(prev)) {
       for (const key in prev) {
         if (next[key] == null) {
           setStyle(style, key, '')
         }
       }
+    }
+    for (const key in next) {
+      setStyle(style, key, next[key])
     }
   } else {
     const currentDisplay = style.display
