vuejs/core

commit ab45f6f8a27fd4ce8af703c42914a94a10a4017a

build: update build scripts to esm

scripts/build.mjs
@@ -14,15 +14,23 @@ nr build core --formats cjs
 ```
 */
 
-const fs = require('fs-extra')
-const path = require('path')
-const chalk = require('chalk')
-const execa = require('execa')
-const { gzipSync } = require('zlib')
-const { compress } = require('brotli')
-const { targets: allTargets, fuzzyMatchTarget } = require('./utils')
+// @ts-check
+import fs from 'node:fs/promises'
+import { existsSync, readFileSync } from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import minimist from 'minimist'
+import { gzipSync } from 'node:zlib'
+import { compress } from 'brotli'
+import chalk from 'chalk'
+import execa from 'execa'
+import { cpus } from 'node:os'
+import { createRequire } from 'node:module'
+import { targets as allTargets, fuzzyMatchTarget } from './utils.mjs'
 
-const args = require('minimist')(process.argv.slice(2))
+const require = createRequire(import.meta.url)
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const args = minimist(process.argv.slice(2))
 const targets = args._
 const formats = args.formats || args.f
 const devOnly = args.devOnly || args.d
@@ -38,7 +46,9 @@ run()
 async function run() {
   if (isRelease) {
     // remove build cache for release builds to avoid outdated enum values
-    await fs.remove(path.resolve(__dirname, '../node_modules/.rts2_cache'))
+    await fs.rm(path.resolve(__dirname, '../node_modules/.rts2_cache'), {
+      recursive: true
+    })
   }
   if (!targets.length) {
     await buildAll(allTargets)
@@ -50,7 +60,7 @@ async function run() {
 }
 
 async function buildAll(targets) {
-  await runParallel(require('os').cpus().length, targets, build)
+  await runParallel(cpus().length, targets, build)
 }
 
 async function runParallel(maxConcurrency, source, iteratorFn) {
@@ -82,7 +92,7 @@ async function build(target) {
 
   // if building a specific format, do not remove dist.
   if (!formats) {
-    await fs.remove(`${pkgDir}/dist`)
+    await fs.rm(`${pkgDir}/dist`, { recursive: true })
   }
 
   const env =
@@ -128,7 +138,7 @@ async function build(target) {
     if (extractorResult.succeeded) {
       // concat additional d.ts to rolled-up dts
       const typesDir = path.resolve(pkgDir, 'types')
-      if (await fs.exists(typesDir)) {
+      if (existsSync(typesDir)) {
         const dtsPath = path.resolve(pkgDir, pkg.types)
         const existing = await fs.readFile(dtsPath, 'utf-8')
         const typeFiles = await fs.readdir(typesDir)
@@ -150,7 +160,7 @@ async function build(target) {
       process.exitCode = 1
     }
 
-    await fs.remove(`${pkgDir}/dist/packages`)
+    await fs.rm(`${pkgDir}/dist/packages`, { recursive: true })
   }
 }
 
@@ -174,10 +184,10 @@ function checkSize(target) {
 }
 
 function checkFileSize(filePath) {
-  if (!fs.existsSync(filePath)) {
+  if (!existsSync(filePath)) {
     return
   }
-  const file = fs.readFileSync(filePath)
+  const file = readFileSync(filePath)
   const minSize = (file.length / 1024).toFixed(2) + 'kb'
   const gzipped = gzipSync(file)
   const gzippedSize = (gzipped.length / 1024).toFixed(2) + 'kb'

pnpm-lock.yaml
@@ -29,7 +29,6 @@ importers:
       eslint: ^7.7.0
       eslint-plugin-jest: 26.1.5
       execa: ^4.0.2
-      fs-extra: ^9.0.1
       jest: ^29.3.1
       jest-environment-jsdom: ^29.3.1
       lint-staged: ^10.2.10
@@ -81,7 +80,6 @@ importers:
       eslint: 7.32.0
       eslint-plugin-jest: 26.1.5_h6y6ce27mgasm6fmt6a3uqj6ne
       execa: 4.1.0
-      fs-extra: 9.1.0
       jest: 29.3.1_@types+node@16.11.12
       jest-environment-jsdom: 29.3.1
       lint-staged: 10.5.4
@@ -2128,11 +2126,6 @@ packages:
     resolution: {integrity: sha512-Oei9OH4tRh0YqU3GxhX79dM/mwVgvbZJaSNaRk+bshkj0S5cfHcgYakreBjrHwatXKbz+IoIdYLxrKim2MjW0Q==}
     dev: true
 
-  /at-least-node/1.0.0:
-    resolution: {integrity: sha512-+q/t7Ekv1EDY2l6Gda6LLiX14rU9TV20Wa3ofeQmwPFZbOMo9DXrLbOjFaaclkXKWidIaopwAObQDqwWtGUjqg==}
-    engines: {node: '>= 4.0.0'}
-    dev: true
-
   /babel-jest/29.3.1_@babel+core@7.16.0:
     resolution: {integrity: sha512-aard+xnMoxgjwV70t0L6wkW/3HQQtV+O0PEimxKgzNqCJnbYmroPojdP2tqKSOAt8QAKV/uSZU8851M7B5+fcA==}
     engines: {node: ^14.15.0 || ^16.10.0 || >=18.0.0}
@@ -3730,16 +3723,6 @@ packages:
       universalify: 0.1.2
     dev: true
 
-  /fs-extra/9.1.0:
-    resolution: {integrity: sha512-hcg3ZmepS30/7BSFqRvoo3DOMQu7IjqxO5nCDt+zM9XWjb33Wg7ziNT+Qvqbuc3+gWpzO02JubVyk2G4Zvo1OQ==}
-    engines: {node: '>=10'}
-    dependencies:
-      at-least-node: 1.0.0
-      graceful-fs: 4.2.8
-      jsonfile: 6.1.0
-      universalify: 2.0.0
-    dev: true
-
   /fs.realpath/1.0.0:
     resolution: {integrity: sha1-FQStJSMVjKpA20onh8sBQRmU6k8=}
     dev: true
@@ -3932,10 +3915,6 @@ packages:
 
   /graceful-fs/4.2.10:
     resolution: {integrity: sha512-9ByhssR2fPVsNZj478qUUbKfmL0+t5BDVyjShtyZZLiK7ZDAArFFfopyOTj0M05wE2tJPisA4iTnnXl2YoPvOA==}
-    dev: true
-
-  /graceful-fs/4.2.8:
-    resolution: {integrity: sha512-qkIilPUYcNhJpd33n0GBXTB1MMPp14TxEsEs0pTrsSVucApsYzW5V+Q8Qxhik6KU3evy+qkAAowTByymK0avdg==}
     dev: true
 
   /handlebars/4.7.7:

scripts/bootstrap.mjs
@@ -1,11 +1,19 @@
 // create package.json, README, etc. for packages that don't have them yet
+// @ts-check
+import minimist from 'minimist'
+import fs from 'node:fs'
+import path from 'node:path'
+import { createRequire } from 'node:module'
+import { fileURLToPath } from 'url'
 
-const args = require('minimist')(process.argv.slice(2))
-const fs = require('fs')
-const path = require('path')
+const args = minimist(process.argv.slice(2))
+const require = createRequire(import.meta.url)
 const version = require('../package.json').version
 
-const packagesDir = path.resolve(__dirname, '../packages')
+const packagesDir = path.resolve(
+  fileURLToPath(import.meta.url),
+  '../../packages'
+)
 const files = fs.readdirSync(packagesDir)
 
 files.forEach(shortName => {

scripts/dev.js
@@ -1,114 +1,0 @@
-// Using esbuild for faster dev builds.
-// We are still using Rollup for production builds because it generates
-// smaller files w/ better tree-shaking.
-
-// @ts-check
-const { context } = require('esbuild')
-const nodePolyfills = require('@esbuild-plugins/node-modules-polyfill')
-const { resolve, relative } = require('path')
-const args = require('minimist')(process.argv.slice(2))
-
-const target = args._[0] || 'vue'
-const format = args.f || 'global'
-const inlineDeps = args.i || args.inline
-const pkg = require(resolve(__dirname, `../packages/${target}/package.json`))
-
-// resolve output
-const outputFormat = format.startsWith('global')
-  ? 'iife'
-  : format === 'cjs'
-  ? 'cjs'
-  : 'esm'
-
-const postfix = format.endsWith('-runtime')
-  ? `runtime.${format.replace(/-runtime$/, '')}`
-  : format
-
-const outfile = resolve(
-  __dirname,
-  `../packages/${target}/dist/${
-    target === 'vue-compat' ? `vue` : target
-  }.${postfix}.js`
-)
-const relativeOutfile = relative(process.cwd(), outfile)
-
-// resolve externals
-// TODO this logic is largely duplicated from rollup.config.js
-let external = []
-if (!inlineDeps) {
-  // cjs & esm-bundler: external all deps
-  if (format === 'cjs' || format.includes('esm-bundler')) {
-    external = [
-      ...external,
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-      // for @vue/compiler-sfc / server-renderer
-      'path',
-      'url',
-      'stream'
-    ]
-  }
-
-  if (target === 'compiler-sfc') {
-    const consolidateDeps = require.resolve('@vue/consolidate/package.json', {
-      paths: [resolve(__dirname, `../packages/${target}/`)]
-    })
-    external = [
-      ...external,
-      ...Object.keys(require(consolidateDeps).devDependencies),
-      'fs',
-      'vm',
-      'crypto',
-      'react-dom/server',
-      'teacup/lib/express',
-      'arc-templates/dist/es5',
-      'then-pug',
-      'then-jade'
-    ]
-  }
-}
-
-const plugins = [
-  {
-    name: 'log-rebuild',
-    setup(build) {
-      build.onEnd(() => {
-        console.log(`built: ${relativeOutfile}`)
-      })
-    }
-  }
-]
-
-if (format === 'cjs' || pkg.buildOptions?.enableNonBrowserBranches) {
-  plugins.push(nodePolyfills.default())
-}
-
-context({
-  entryPoints: [resolve(__dirname, `../packages/${target}/src/index.ts`)],
-  outfile,
-  bundle: true,
-  external,
-  sourcemap: true,
-  format: outputFormat,
-  globalName: pkg.buildOptions?.name,
-  platform: format === 'cjs' ? 'node' : 'browser',
-  plugins,
-  define: {
-    __COMMIT__: `"dev"`,
-    __VERSION__: `"${pkg.version}"`,
-    __DEV__: `true`,
-    __TEST__: `false`,
-    __BROWSER__: String(
-      format !== 'cjs' && !pkg.buildOptions?.enableNonBrowserBranches
-    ),
-    __GLOBAL__: String(format === 'global'),
-    __ESM_BUNDLER__: String(format.includes('esm-bundler')),
-    __ESM_BROWSER__: String(format.includes('esm-browser')),
-    __NODE_JS__: String(format === 'cjs'),
-    __SSR__: String(format === 'cjs' || format.includes('esm-bundler')),
-    __COMPAT__: String(target === 'vue-compat'),
-    __FEATURE_SUSPENSE__: `true`,
-    __FEATURE_OPTIONS_API__: `true`,
-    __FEATURE_PROD_DEVTOOLS__: `false`
-  }
-}).then(ctx => ctx.watch())

scripts/dev.mjs
@@ -1,0 +1,128 @@
+// Using esbuild for faster dev builds.
+// We are still using Rollup for production builds because it generates
+// smaller files w/ better tree-shaking.
+
+// @ts-check
+import esbuild from 'esbuild'
+import { resolve, relative, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { createRequire } from 'node:module'
+import minimist from 'minimist'
+import { NodeModulesPolyfillPlugin as nodePolyfills } from '@esbuild-plugins/node-modules-polyfill'
+
+const require = createRequire(import.meta.url)
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const args = minimist(process.argv.slice(2))
+const target = args._[0] || 'vue'
+const format = args.f || 'global'
+const inlineDeps = args.i || args.inline
+const pkg = require(`../packages/${target}/package.json`)
+
+// resolve output
+const outputFormat = format.startsWith('global')
+  ? 'iife'
+  : format === 'cjs'
+  ? 'cjs'
+  : 'esm'
+
+const postfix = format.endsWith('-runtime')
+  ? `runtime.${format.replace(/-runtime$/, '')}`
+  : format
+
+const outfile = resolve(
+  __dirname,
+  `../packages/${target}/dist/${
+    target === 'vue-compat' ? `vue` : target
+  }.${postfix}.js`
+)
+const relativeOutfile = relative(process.cwd(), outfile)
+
+// resolve externals
+// TODO this logic is largely duplicated from rollup.config.js
+let external = []
+if (!inlineDeps) {
+  // cjs & esm-bundler: external all deps
+  if (format === 'cjs' || format.includes('esm-bundler')) {
+    external = [
+      ...external,
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+      // for @vue/compiler-sfc / server-renderer
+      'path',
+      'url',
+      'stream'
+    ]
+  }
+
+  if (target === 'compiler-sfc') {
+    const consolidatePkgPath = require.resolve(
+      '@vue/consolidate/package.json',
+      {
+        paths: [resolve(__dirname, `../packages/${target}/`)]
+      }
+    )
+    const consolidateDeps = Object.keys(
+      require(consolidatePkgPath).devDependencies
+    )
+    external = [
+      ...external,
+      ...consolidateDeps,
+      'fs',
+      'vm',
+      'crypto',
+      'react-dom/server',
+      'teacup/lib/express',
+      'arc-templates/dist/es5',
+      'then-pug',
+      'then-jade'
+    ]
+  }
+}
+
+const plugins = [
+  {
+    name: 'log-rebuild',
+    setup(build) {
+      build.onEnd(() => {
+        console.log(`built: ${relativeOutfile}`)
+      })
+    }
+  }
+]
+
+if (format === 'cjs' || pkg.buildOptions?.enableNonBrowserBranches) {
+  console.log(nodePolyfills)
+  plugins.push(nodePolyfills())
+}
+
+esbuild
+  .context({
+    entryPoints: [resolve(__dirname, `../packages/${target}/src/index.ts`)],
+    outfile,
+    bundle: true,
+    external,
+    sourcemap: true,
+    format: outputFormat,
+    globalName: pkg.buildOptions?.name,
+    platform: format === 'cjs' ? 'node' : 'browser',
+    plugins,
+    define: {
+      __COMMIT__: `"dev"`,
+      __VERSION__: `"${pkg.version}"`,
+      __DEV__: `true`,
+      __TEST__: `false`,
+      __BROWSER__: String(
+        format !== 'cjs' && !pkg.buildOptions?.enableNonBrowserBranches
+      ),
+      __GLOBAL__: String(format === 'global'),
+      __ESM_BUNDLER__: String(format.includes('esm-bundler')),
+      __ESM_BROWSER__: String(format.includes('esm-browser')),
+      __NODE_JS__: String(format === 'cjs'),
+      __SSR__: String(format === 'cjs' || format.includes('esm-bundler')),
+      __COMPAT__: String(target === 'vue-compat'),
+      __FEATURE_SUSPENSE__: `true`,
+      __FEATURE_OPTIONS_API__: `true`,
+      __FEATURE_PROD_DEVTOOLS__: `false`
+    }
+  })
+  .then(ctx => ctx.watch())

scripts/filter-e2e.js
@@ -1,3 +1,4 @@
+// jest only supports cjs filter files
 const path = require('path')
 
 const e2eTests = [

scripts/filter-unit.js
@@ -1,3 +1,4 @@
+// jest only supports cjs filter files
 const { e2eTests } = require('./filter-e2e')
 
 module.exports = list => {

scripts/pre-dev-sfc.mjs
@@ -1,5 +1,4 @@
-const fs = require('fs')
-const path = require('path')
+import fs from 'node:fs'
 
 const packagesToCheck = [
   'compiler-sfc',
@@ -15,7 +14,7 @@ let allFilesPresent = true
 for (const pkg of packagesToCheck) {
   if (
     !fs.existsSync(
-      path.resolve(__dirname, `../packages/${pkg}/dist/${pkg}.cjs.js`)
+      new URL(`../packages/${pkg}/dist/${pkg}.cjs.js`, import.meta.url)
     )
   ) {
     allFilesPresent = false

commit 5fef1b648f64531d5e6a3b0f5e8a82cdee569407

build: do not throw when running `build --release` for the 1st time
Fixes https://github.com/vuejs/ecosystem-ci/actions/runs/4022763262/jobs/6912842567#step:8:122

The bug was introduced in https://github.com/vuejs/core/commit/ab45f6f8a27fd4ce8af703c42914a94a10a4017a
which replaced `fs-extra` with native `fs`.

The `remove` function in `fs-extra` ["silently does nothing" if the path
does not exist](https://github.com/jprichardson/node-fs-extra/blob/f3a7f0beeb5858c628b10010ad819c813c7f3565/docs/remove.md).
Meanwhile `fs.rm` would throw [unless the `force` flag is set](https://nodejs.org/dist/latest-v18.x/docs/api/fs.html#fspromisesrmpath-options).

scripts/build.mjs
@@ -47,6 +47,7 @@ async function run() {
   if (isRelease) {
     // remove build cache for release builds to avoid outdated enum values
     await fs.rm(path.resolve(__dirname, '../node_modules/.rts2_cache'), {
+      force: true,
       recursive: true
     })
   }