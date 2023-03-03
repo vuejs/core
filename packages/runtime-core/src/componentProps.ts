e4e9c979f8ad0953eff2d468aa29958e5e44bb96
baiwusanyu-c <740132583@qq.com>
2023-03-03 15:05:29 +0800

    fix(runtime-core): update code

--- packages/runtime-core/src/componentProps.ts
+++ packages/runtime-core/src/componentProps.ts
@@ -345,7 +345,7 @@ function setFullProps(
   if (rawProps) {
     for (let key in rawProps) {
       // key, ref are reserved and never passed down
-      if (isReservedProp(key) || key.startsWith('onUpdate:xxx')) {
+      if (isReservedProp(key) || key.startsWith('onUpdate:')) {
         continue
       }
 
