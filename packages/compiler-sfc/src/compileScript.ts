4970c39e981ba1b68932b3a946fe91f97f1265dc
daiwei <daiwei521@126.com>
2023-03-14 14:57:56 +0800

    chore: improve code

--- packages/compiler-sfc/src/compileScript.ts
+++ packages/compiler-sfc/src/compileScript.ts
@@ -1433,7 +1433,7 @@ export function compileScript(
   if (
     cssVars.length &&
     // no need to do this when targeting SSR
-    !(scriptSetup && options.templateOptions?.ssr)
+    !options.templateOptions?.ssr
   ) {
     helperImports.add(CSS_VARS_HELPER)
     helperImports.add('unref')
