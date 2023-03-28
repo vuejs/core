9577f1c67330155b498e28006438a78d04862216
Evan You <yyx990803@gmail.com>
2023-03-28 11:06:18 +0800

    Update index.mjs

--- packages/vue/jsx-runtime/index.mjs
+++ packages/vue/jsx-runtime/index.mjs
@@ -1,16 +1,12 @@
 import { h, Fragment } from 'vue'
 
-export { Fragment }
-
-export function jsx(type, { children, ...props }) {
+function jsx(type, { children, ...props }) {
   return h(type, props, children)
 }
 
-export function jsxs(type, { children, ...props }) {
-  return h(type, props, ...children)
-}
-
-export function jsxDEV(type, props, key, isStatic) {
-  const fn = isStatic ? jsxs : jsx
-  return fn(type, props)
+export {
+  Fragment,
+  jsx,
+  jsx as jsxs,
+  jsx as jsxDEV
 }
