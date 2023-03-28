543d6dbc941cfcf60931168f149490779c888841
Haoqun Jiang <haoqunjiang@gmail.com>
2023-03-28 09:42:45 +0800

    build: don't wait for changelog edits when `--skip-prompts` or `--canary` is specified (#7965)

--- scripts/release.js
+++ scripts/release.js
@@ -211,14 +211,16 @@ async function main() {
   await run(`pnpm`, ['run', 'changelog'])
 
   // @ts-ignore
-  const { yes: changelogOk } = await prompt({
-    type: 'confirm',
-    name: 'yes',
-    message: `Changelog generated. Does it look good?`
-  })
+  if (!skipPrompts) {
+    const { yes: changelogOk } = await prompt({
+      type: 'confirm',
+      name: 'yes',
+      message: `Changelog generated. Does it look good?`
+    })
 
-  if (!changelogOk) {
-    return
+    if (!changelogOk) {
+      return
+    }
   }
 
   // update pnpm-lock.yaml
