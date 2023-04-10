ded04016cf3cbbb3a1b2088508eb017dbbd1d006
丶远方 <yangpanteng@gmail.com>
2023-04-10 21:19:21 +0800

    fix(general): fix function

--- packages/shared/src/general.ts
+++ packages/shared/src/general.ts
@@ -41,8 +41,12 @@ export const isSet = (val: unknown): val is Set<any> => isType(val, 'Set')
 
 export const isDate = (val: unknown): val is Date => isType(val, 'Date')
 export const isRegExp = (val: unknown): val is RegExp => isType(val, 'RegExp')
+
+export const isPlainObject = (val: unknown): val is object =>
+  isType(val, 'Object')
+
 export const isFunction = (val: unknown): val is Function =>
-  isType(val, 'Function')
+  typeof val === 'function'
 
 export const isString = (val: unknown): val is string => typeof val === 'string'
 export const isSymbol = (val: unknown): val is symbol => typeof val === 'symbol'
@@ -65,9 +69,6 @@ export const toRawType = (value: unknown): string => {
   // extract "RawType" from strings like "[object RawType]"
   return toTypeString(value).slice(8, -1)
 }
-
-export const isPlainObject = (val: unknown): val is object =>
-  toTypeString(val) === '[object Object]'
 
 export const isIntegerKey = (key: unknown) =>
   isString(key) &&
