9f2ca5155e5d44c94d95aa8b569283afdf352c1c
Evan You <yyx990803@gmail.com>
2023-04-11 14:17:12 +0800

    refactor(compiler-sfc): extract processDefineModel, move main magic string instance to context

--- packages/compiler-sfc/src/script/defineModel.ts
+++ packages/compiler-sfc/src/script/defineModel.ts
@@ -1,12 +1,91 @@
-import { TSType } from '@babel/types'
+import { LVal, Node, ObjectProperty, TSType } from '@babel/types'
 import { ScriptCompileContext } from './context'
 import { inferRuntimeType } from './resolveType'
-import { UNKNOWN_TYPE, concatStrings, toRuntimeTypeString } from './utils'
+import {
+  UNKNOWN_TYPE,
+  concatStrings,
+  isCallOf,
+  toRuntimeTypeString,
+  unwrapTSNode
+} from './utils'
+
+export const DEFINE_MODEL = 'defineModel'
 
 export interface ModelDecl {
   type: TSType | undefined
   options: string | undefined
   identifier: string | undefined
+}
+
+export function processDefineModel(
+  ctx: ScriptCompileContext,
+  node: Node,
+  declId?: LVal
+): boolean {
+  if (!ctx.options.defineModel || !isCallOf(node, DEFINE_MODEL)) {
+    return false
+  }
+  ctx.hasDefineModelCall = true
+
+  const type =
+    (node.typeParameters && node.typeParameters.params[0]) || undefined
+  let modelName: string
+  let options: Node | undefined
+  const arg0 = node.arguments[0] && unwrapTSNode(node.arguments[0])
+  if (arg0 && arg0.type === 'StringLiteral') {
+    modelName = arg0.value
+    options = node.arguments[1]
+  } else {
+    modelName = 'modelValue'
+    options = arg0
+  }
+
+  if (ctx.modelDecls[modelName]) {
+    ctx.error(`duplicate model name ${JSON.stringify(modelName)}`, node)
+  }
+
+  const optionsString = options && ctx.getString(options)
+
+  ctx.modelDecls[modelName] = {
+    type,
+    options: optionsString,
+    identifier: declId && declId.type === 'Identifier' ? declId.name : undefined
+  }
+
+  let runtimeOptions = ''
+  if (options) {
+    if (options.type === 'ObjectExpression') {
+      const local = options.properties.find(
+        p =>
+          p.type === 'ObjectProperty' &&
+          ((p.key.type === 'Identifier' && p.key.name === 'local') ||
+            (p.key.type === 'StringLiteral' && p.key.value === 'local'))
+      ) as ObjectProperty
+
+      if (local) {
+        runtimeOptions = `{ ${ctx.getString(local)} }`
+      } else {
+        for (const p of options.properties) {
+          if (p.type === 'SpreadElement' || p.computed) {
+            runtimeOptions = optionsString!
+            break
+          }
+        }
+      }
+    } else {
+      runtimeOptions = optionsString!
+    }
+  }
+
+  ctx.s.overwrite(
+    ctx.startOffset! + node.start!,
+    ctx.startOffset! + node.end!,
+    `${ctx.helper('useModel')}(__props, ${JSON.stringify(modelName)}${
+      runtimeOptions ? `, ${runtimeOptions}` : ``
+    })`
+  )
+
+  return true
 }
 
 export function genModelProps(ctx: ScriptCompileContext) {
