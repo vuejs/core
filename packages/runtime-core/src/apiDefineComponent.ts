c2172f3a0ebbd7153e209dd8df6d9724bc524d9a
Evan You <yyx990803@gmail.com>
2023-05-08 12:12:15 +0800

    fix(build): retain defineComponent() treeshakability in Rollup
    close #8236

--- packages/runtime-core/src/apiDefineComponent.ts
+++ packages/runtime-core/src/apiDefineComponent.ts
@@ -279,6 +279,9 @@ export function defineComponent(
   extraOptions?: ComponentOptions
 ) {
   return isFunction(options)
-    ? extend({ name: options.name }, extraOptions, { setup: options })
+    ? // #8326: extend call and options.name access are considered side-effects
+      // by Rollup, so we have to wrap it in a pure-annotated IIFE.
+      /*#__PURE__*/ (() =>
+        extend({ name: options.name }, extraOptions, { setup: options }))()
     : options
 }
