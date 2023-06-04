3ef23c5f66a746034cdaa03fdf007f7c14b41beb
Miachel Brevard <yonshi29@gmail.com>
2023-06-04 16:58:45 +0300

    Make sure dir is defined

--- packages/server-renderer/src/helpers/ssrGetDirectiveProps.ts
+++ packages/server-renderer/src/helpers/ssrGetDirectiveProps.ts
@@ -7,7 +7,7 @@ export function ssrGetDirectiveProps(
   arg?: string,
   modifiers: Record<string, boolean> = {}
 ): Record<string, any> {
-  if (typeof dir !== 'function' && dir.getSSRProps) {
+  if (dir !== undefined && typeof dir !== 'function' && dir.getSSRProps) {
     return (
       dir.getSSRProps(
         {
