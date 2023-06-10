c6ff53df1e919c97b08ecf9f27c4f2da8decd15f
mattersj <throwget@gmail.com>
2023-06-10 20:00:40 +0400

    fix(runtime-core): avoid symbol coercion during props validation

--- packages/runtime-core/src/componentProps.ts
+++ packages/runtime-core/src/componentProps.ts
@@ -10,6 +10,7 @@ import {
   hyphenate,
   capitalize,
   isString,
+  isSymbol,
   isFunction,
   isArray,
   isObject,
@@ -736,7 +737,7 @@ function getInvalidTypeMessage(
   if (
     expectedTypes.length === 1 &&
     isExplicable(expectedType) &&
-    !isBoolean(expectedType, receivedType)
+    isCoercible(expectedType, receivedType)
   ) {
     message += ` with value ${expectedValue}`
   }
@@ -752,7 +753,9 @@ function getInvalidTypeMessage(
  * dev only
  */
 function styleValue(value: unknown, type: string): string {
-  if (type === 'String') {
+  if (isSymbol(value)) {
+    return value.toString()
+  } else if (type === 'String') {
     return `"${value}"`
   } else if (type === 'Number') {
     return `${Number(value)}`
@@ -772,6 +775,9 @@ function isExplicable(type: string): boolean {
 /**
  * dev only
  */
-function isBoolean(...args: string[]): boolean {
-  return args.some(elem => elem.toLowerCase() === 'boolean')
+function isCoercible(...args: string[]): boolean {
+  return args.every(elem => {
+    const value = elem.toLowerCase()
+    return value !== 'boolean' && value !== 'symbol'
+  })
 }
