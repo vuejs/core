2a2810c716e7882a2dbd5ce2007973d5c14e81f5
Evan You <yyx990803@gmail.com>
2023-07-11 18:44:00 +0800

    chore: upgrade to typescript 5.1

--- packages/shared/src/general.ts
+++ packages/shared/src/general.ts
@@ -52,8 +52,8 @@ export const isObject = (val: unknown): val is Record<any, any> =>
 export const isPromise = <T = any>(val: unknown): val is Promise<T> => {
   return (
     (isObject(val) || isFunction(val)) &&
-    isFunction(val.then) &&
-    isFunction(val.catch)
+    isFunction((val as any).then) &&
+    isFunction((val as any).catch)
   )
 }
 
