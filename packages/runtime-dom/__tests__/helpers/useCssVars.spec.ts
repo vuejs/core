2f9cf3e87d1c0fe253c2df974d6d0e3134c731e3
baiwusanyu-c <740132583@qq.com>
2023-07-25 09:15:16 +0800

    feat(custom-element): added unit test

--- packages/runtime-dom/__tests__/helpers/useCssVars.spec.ts
+++ packages/runtime-dom/__tests__/helpers/useCssVars.spec.ts
@@ -9,7 +9,8 @@ import {
   ComponentOptions,
   Suspense,
   Teleport,
-  FunctionalComponent
+  FunctionalComponent,
+  defineCustomElement
 } from '@vue/runtime-dom'
 
 describe('useCssVars', () => {
@@ -274,5 +275,25 @@ describe('useCssVars', () => {
     for (const c of [].slice.call(target.children as any)) {
       expect((c as HTMLElement).style.getPropertyValue(`--color`)).toBe('red')
     }
+  })
+
+  // #8826
+  test('with custom element', async () => {
+    const state = reactive({ color: 'red' })
+    const container = document.createElement('div')
+    const App = defineCustomElement({
+      styles: [`div { color: red; }`],
+      setup() {
+        useCssVars(() => state)
+        return () => {
+          return h('div', 'hello')
+        }
+      }
+    })
+    customElements.define('css-vars-ce', App)
+    container.innerHTML = `<css-vars-ce></css-vars-ce>`
+    document.body.appendChild(container)
+    await nextTick()
+    expect(container.innerHTML).toBe(`<css-vars-ce style="--color: red;"></css-vars-ce>`)
   })
 })
