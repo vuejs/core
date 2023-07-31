52467519ab8557b7905394706317097d675c1188
Tomxuetao <15555030339@163.com>
2023-07-31 09:01:25 +0800

    Merge branch 'main' of https://github.com/vuejs/core into vuejs-main
    # Conflicts:
    #	packages/compiler-sfc/src/script/importUsageCheck.ts
    #	packages/dts-test/defineComponent.test-d.tsx

--- packages/dts-test/defineComponent.test-d.tsx
+++ packages/dts-test/defineComponent.test-d.tsx
@@ -1363,13 +1363,13 @@ describe('function syntax w/ runtime props', () => {
     }
   )
 
-  // @ts-expect-error string prop names don't match
   defineComponent(
     (_props: { msg: string }) => {
       return () => {}
     },
     {
       props: {
+        // @ts-expect-error prop type mismatch
         msg: Number
       }
     }
