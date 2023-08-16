eb99101119354771f9d40823ba6c74481f4523b6
丶远方 <yangpanteng@gmail.com>
2023-08-16 22:43:04 +0800

    chore: replace `() => {}` with `NOOP`

--- packages/runtime-core/src/compat/global.ts
+++ packages/runtime-core/src/compat/global.ts
@@ -461,7 +461,7 @@ function installCompatMount(
 
     const hasNoRender =
       !isFunction(component) && !component.render && !component.template
-    const emptyRender = () => {}
+    const emptyRender = NOOP
 
     // create root instance
     const instance = createComponentInstance(vnode, null, null)
