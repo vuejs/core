e1287fc568aa8bd83ca835da6ec8183de8b19e0b
daiwei <daiwei521@126.com>
2023-08-22 09:58:12 +0800

    test: update test case

--- packages/runtime-dom/__tests__/customElement.spec.ts
+++ packages/runtime-dom/__tests__/customElement.spec.ts
@@ -134,10 +134,11 @@ describe('defineCustomElement', () => {
       expect(e.shadowRoot!.innerHTML).toBe('<div>three</div><div>two</div>')
       expect(e.getAttribute('foo')).toBe('three')
 
-      e.foo = null
+      e.foo = undefined
       await nextTick()
       expect(e.shadowRoot!.innerHTML).toBe('<div></div><div>two</div>')
       expect(e.hasAttribute('foo')).toBe(false)
+      expect(e.foo).toBe(undefined)
 
       e.bazQux = 'four'
       await nextTick()
