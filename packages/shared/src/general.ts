fc2ff6460bb4af0e2435bffdb42988fa3af1e8c8
Mustafa Uzun <mustafauzun0@gmail.com>
2023-08-23 15:34:02 +0300

    feat(shared): add isBigInt function

--- packages/shared/src/general.ts
+++ packages/shared/src/general.ts
@@ -46,6 +46,7 @@ export const isFunction = (val: unknown): val is Function =>
   typeof val === 'function'
 export const isString = (val: unknown): val is string => typeof val === 'string'
 export const isSymbol = (val: unknown): val is symbol => typeof val === 'symbol'
+export const isBigInt = (val: unknown): val is bigint => typeof val === 'bigint'
 export const isObject = (val: unknown): val is Record<any, any> =>
   val !== null && typeof val === 'object'
 
