1dc623e11bfd52b920209f41db89e8b14c6f3f19
Haoqun Jiang <haoqunjiang@gmail.com>
2023-08-29 16:58:23 +0800

    build: suppress rollup warning

--- rollup.config.js
+++ rollup.config.js
@@ -215,7 +215,12 @@ function createConfig(format, output, plugins = []) {
   }
 
   function resolveExternal() {
-    const treeShakenDeps = ['source-map-js', '@babel/parser', 'estree-walker']
+    const treeShakenDeps = [
+      'source-map-js',
+      '@babel/parser',
+      'estree-walker',
+      'html-parsed-element'
+    ]
 
     if (isGlobalBuild || isBrowserESMBuild || isCompatPackage) {
       if (!packageOptions.enableNonBrowserBranches) {
