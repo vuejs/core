fa648870d10eb4fc506a6da596546eeaa5a43cab
shixinzhu <shixinzhu@jintengtech.com>
2023-08-29 14:41:39 +0800

    feat: refactor globalsAllowList.ts to use a more comprehensive list of globally allowed values

--- packages/shared/src/globalsAllowList.ts
+++ packages/shared/src/globalsAllowList.ts
@@ -1,11 +1,40 @@
 import { makeMap } from './makeMap'
 
-const GLOBALS_ALLOWED =
-  'Infinity,undefined,NaN,isFinite,isNaN,parseFloat,parseInt,decodeURI,' +
-  'decodeURIComponent,encodeURI,encodeURIComponent,Math,Number,Date,Array,' +
-  'Object,Boolean,String,RegExp,Map,Set,JSON,Intl,BigInt,console'
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+const GLOBAL_SIMPLE_VALUE = 'globalThis,Infinity,NaN,undefined'
+const FUNCTION =
+  'eval,isFinite,isNaN,parseFloat,parseInt,decodeURI,decodeURIComponent,encodeURI,encodeURIComponent'
+const FUNDAMENTAL = 'Object,Function,Boolean,Symbol'
+const ERROR =
+  'Error,AggregateError,EvalError,RangeError,ReferenceError,SyntaxError,TypeError,URIError'
+const NUMBER_AND_DATES = 'Math,BigInt,Number,Date'
+const TEXT = 'String,RegExp'
+const INDEXED_COLLECTIONS =
+  'Array,Int8Array,Uint8Array,Uint8ClampedArray,Int16Array,Uint16Array,Int32Array,Uint32Array,BigInt64Array,BigUint64Array,Float32Array,Float64Array'
+const KEYED_COLLECTIONS = 'Map,Set,WeakMap,WeakSet'
+const STRUCTURED_DATA = 'ArrayBuffer,SharedArrayBuffer,DataView,Atomics,JSON'
+const MANAGING_MEMORY = 'WeakRef,FinalizationRegistry'
+const CONTROL_ABSTRACTION_OBJECTS =
+  'Iterator,AsyncIterator,Promise,GeneratorFunction,AsyncGeneratorFunction,Generator,AsyncGenerator,AsyncFunction'
+const Reflection = 'Reflect,Proxy'
+const INTERNATIONALIZATION = 'Intl'
+const OTHER_GLOBALS_ALLOWED = 'console'
+
+const GLOBALS_ALLOWED = [
+  GLOBAL_SIMPLE_VALUE,
+  FUNCTION,
+  FUNDAMENTAL,
+  ERROR,
+  NUMBER_AND_DATES,
+  TEXT,
+  INDEXED_COLLECTIONS,
+  KEYED_COLLECTIONS,
+  STRUCTURED_DATA,
+  MANAGING_MEMORY,
+  CONTROL_ABSTRACTION_OBJECTS,
+  Reflection,
+  INTERNATIONALIZATION,
+  OTHER_GLOBALS_ALLOWED
+].join(',')
 
 export const isGloballyAllowed = /*#__PURE__*/ makeMap(GLOBALS_ALLOWED)
-
-/** @deprecated use `isGloballyAllowed` instead */
-export const isGloballyWhitelisted = isGloballyAllowed
