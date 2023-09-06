4471c547558c8f1804d6eb85b6e36326962ea94c
shixinzhu <shixinzhu@jintengtech.com>
2023-09-06 14:42:46 +0800

    feat: refactor globalsAllowList.ts for improved readability and maintainability

--- packages/shared/src/globalsAllowList.ts
+++ packages/shared/src/globalsAllowList.ts
@@ -1,44 +1,30 @@
 import { makeMap } from './makeMap'
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
-const GLOBAL_SIMPLE_VALUE = 'Infinity,NaN,undefined'
-
-const ERROR =
-  'Error,AggregateError,EvalError,RangeError,ReferenceError,SyntaxError,TypeError,URIError'
-
-const FUNCTION =
+const GLOBALS_ALLOWED =
+  // Simple Global Values
+  'Infinity,NaN,undefined,' +
+  // Error types
+  'Error,AggregateError,EvalError,RangeError,ReferenceError,SyntaxError,TypeError,URIError,' +
+  // Fundamental functions
   'isFinite,isNaN,parseFloat,parseInt,decodeURI,' +
-  'decodeURIComponent,encodeURI,encodeURIComponent'
-
-const FUNDAMENTAL = 'Object,Boolean,Symbol'
-
-const NUMBER_AND_DATES = 'Math,Number,Date,BigInt'
-
-const TEXT = 'String,RegExp'
-
-const INDEXED_COLLECTIONS = 'Array'
-
-const KEYED_COLLECTIONS = 'Map,Set'
-
-const STRUCTURED_DATA = 'JSON'
-
-const INTERNATIONALIZATION = 'Intl'
-
-const OTHER_GLOBALS_ALLOWED = 'console'
-
-const GLOBALS_ALLOWED = [
-  GLOBAL_SIMPLE_VALUE,
-  FUNCTION,
-  FUNDAMENTAL,
-  ERROR,
-  NUMBER_AND_DATES,
-  TEXT,
-  INDEXED_COLLECTIONS,
-  KEYED_COLLECTIONS,
-  STRUCTURED_DATA,
-  INTERNATIONALIZATION,
-  OTHER_GLOBALS_ALLOWED
-].join(',')
+  'decodeURIComponent,encodeURI,encodeURIComponent,' +
+  // Fundamental objects
+  'Object,Boolean,Symbol,' +
+  // Numbers and dates
+  'Math,Number,Date,BigInt,' +
+  // Text processing
+  'String,RegExp,' +
+  // Indexed collections
+  'Array,' +
+  // Keyed collections
+  'Map,Set,' +
+  // Structured data
+  'JSON,' +
+  // Internationalization & Localization
+  'Intl,' +
+  // Other globals
+  'console'
 
 export const isGloballyAllowed = /*#__PURE__*/ makeMap(GLOBALS_ALLOWED)
 
