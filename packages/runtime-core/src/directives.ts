2ba6cc50b54de39b9e3b0ec8eb1f6df08d22ebae
nandi95 <41805560+nandi95@users.noreply.github.com>
2023-10-19 15:59:38 +0100

    Update packages/runtime-core/src/directives.ts
    Default of unknown is better than any

--- packages/runtime-core/src/directives.ts
+++ packages/runtime-core/src/directives.ts
@@ -38,7 +38,7 @@ export type DirectiveHook<T = any, Prev = VNode<any, T> | null, V = any> = (
   prevVNode: Prev
 ) => void
 
-export type SSRDirectiveHook<V = any> = (
+export type SSRDirectiveHook<V> = (
   binding: DirectiveBinding<V>,
   vnode: VNode
 ) => Data | undefined
