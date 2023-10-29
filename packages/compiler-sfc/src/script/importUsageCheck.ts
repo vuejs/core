1f581b79ecc6642c8966fa38634aa6d5c115e4bc
edison <daiwei521@126.com>
2023-10-29 08:40:53 +0800

    Update importUsageCheck.ts

--- packages/compiler-sfc/src/script/importUsageCheck.ts
+++ packages/compiler-sfc/src/script/importUsageCheck.ts
@@ -50,12 +50,15 @@ function resolveTemplateUsageCheckString(sfc: SFCDescriptor) {
               if (!isBuiltInDirective(prop.name)) {
                 code += `,v${capitalize(camelize(prop.name))}`
               }
+              
+              //  process dynamic directive arguments as bind
               if (prop.arg && !(prop.arg as SimpleExpressionNode).isStatic) {
                 code += `,${processExp(
                   (prop.arg as SimpleExpressionNode).content,
                   'bind'
                 )}`
               }
+              
               if (prop.exp) {
                 code += `,${processExp(
                   (prop.exp as SimpleExpressionNode).content,
