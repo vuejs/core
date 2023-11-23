79fc4025c3c1712ea05aaa4a18ae9ac5ed8b6ba5
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-11-23 23:55:33 +0800

    fix: ts error

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -4,5 +4,6 @@ import App from './App.vue'
 render(() => {
   // @ts-expect-error
   const returned = App.setup({}, { expose() {} })
+  // @ts-expect-error
   return App.render(returned)
 }, '#app')
