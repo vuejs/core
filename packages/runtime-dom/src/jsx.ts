fd0b6ba01660499fa07b0cf360eefaac8cca8287
Mehmet <hi@productdevbook.com>
2023-12-11 17:05:48 +0300

    feat(types): export AriaAttributes type (#8909)

--- packages/runtime-dom/src/jsx.ts
+++ packages/runtime-dom/src/jsx.ts
@@ -46,7 +46,7 @@ type Booleanish = boolean | 'true' | 'false'
 type Numberish = number | string
 
 // All the WAI-ARIA 1.1 attributes from https://www.w3.org/TR/wai-aria-1.1/
-interface AriaAttributes {
+export interface AriaAttributes {
   /** Identifies the currently active element when DOM focus is on a composite widget, textbox, group, or application. */
   'aria-activedescendant'?: string
   /** Indicates whether assistive technologies will present all, or only parts of, the changed region based on the change notifications defined by the aria-relevant attribute. */
