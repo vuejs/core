e748930a48d19037bafafbbc7a6134f0fc5ce368
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-12-20 07:07:49 +0800

    fix: compiler-sfc nodejs-modules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ const NodeGlobals = ['module', 'require']
 const banConstEnum = {
   selector: 'TSEnumDeclaration[const=true]',
   message:
-    'Please use non-const enums. This project automatically inlines enums.'
+    'Please use non-const enums. This project automatically inlines enums.',
 }
 
 /**
@@ -16,7 +16,7 @@ const banConstEnum = {
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['jest', 'import', '@typescript-eslint'],
   rules: {
@@ -33,13 +33,13 @@ module.exports = {
       // tsc compiles assignment spread into Object.assign() calls, but esbuild
       // still generates verbose helpers, so spread assignment is also prohiboted
       'ObjectExpression > SpreadElement',
-      'AwaitExpression'
+      'AwaitExpression',
     ],
     'sort-imports': ['error', { ignoreDeclarationSort: true }],
 
     'import/no-nodejs-modules': [
       'error',
-      { allow: builtinModules.map(mod => `node:${mod}`) }
+      { allow: builtinModules.map(mod => `node:${mod}`) },
     ],
     // This rule enforces the preference for using '@ts-expect-error' comments in TypeScript
     // code to indicate intentional type errors, improving code clarity and maintainability.
@@ -49,11 +49,11 @@ module.exports = {
       'error',
       {
         fixStyle: 'inline-type-imports',
-        disallowTypeAnnotations: false
+        disallowTypeAnnotations: false,
       },
     ],
     // Enforce the use of top-level import type qualifier when an import only has specifiers with inline type qualifiers
-    '@typescript-eslint/no-import-type-side-effects': 'error'
+    '@typescript-eslint/no-import-type-side-effects': 'error',
   },
   overrides: [
     // tests, no restrictions (runs in Node / jest with jsdom)
@@ -63,61 +63,61 @@ module.exports = {
         'no-restricted-globals': 'off',
         'no-restricted-syntax': 'off',
         'jest/no-disabled-tests': 'error',
-        'jest/no-focused-tests': 'error'
-      }
+        'jest/no-focused-tests': 'error',
+      },
     },
     // shared, may be used in any env
     {
       files: ['packages/shared/**'],
       rules: {
-        'no-restricted-globals': 'off'
-      }
+        'no-restricted-globals': 'off',
+      },
     },
     // Packages targeting DOM
     {
       files: ['packages/{vue,vue-compat,runtime-dom}/**'],
       rules: {
-        'no-restricted-globals': ['error', ...NodeGlobals]
-      }
+        'no-restricted-globals': ['error', ...NodeGlobals],
+      },
     },
     // Packages targeting Node
     {
       files: ['packages/{compiler-sfc,compiler-ssr,server-renderer}/**'],
       rules: {
         'no-restricted-globals': ['error', ...DOMGlobals],
-        'no-restricted-syntax': ['error', banConstEnum]
-      }
+        'no-restricted-syntax': ['error', banConstEnum],
+      },
     },
     // Private package, browser only + no syntax restrictions
     {
       files: ['packages/template-explorer/**', 'packages/sfc-playground/**'],
       rules: {
         'no-restricted-globals': ['error', ...NodeGlobals],
-        'no-restricted-syntax': ['error', banConstEnum]
-      }
+        'no-restricted-syntax': ['error', banConstEnum],
+      },
     },
     // JavaScript files
     {
       files: ['*.js', '*.cjs'],
       rules: {
         // We only do `no-unused-vars` checks for js files, TS files are checked by TypeScript itself.
-        'no-unused-vars': ['error', { vars: 'all', args: 'none' }]
-      }
+        'no-unused-vars': ['error', { vars: 'all', args: 'none' }],
+      },
     },
     // Node scripts
     {
       files: ['scripts/**', '*.{js,ts}', 'packages/**/index.js'],
       rules: {
         'no-restricted-globals': 'off',
-        'no-restricted-syntax': ['error', banConstEnum]
-      }
+        'no-restricted-syntax': ['error', banConstEnum],
+      },
     },
     // Import nodejs modules in compiler-sfc
     {
-      files: ['packages/compiler-sfc/**'],
+      files: ['packages/compiler-sfc/src/**'],
       rules: {
-        'import/no-nodejs-modules': ['error', { allow: builtinModules }]
-      }
+        'import/no-nodejs-modules': ['error', { allow: builtinModules }],
+      },
     },
   ]
 }
