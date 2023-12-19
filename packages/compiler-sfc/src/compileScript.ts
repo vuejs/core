8f339ea144956b633d975731fbb7abf43e677612
Evan You <yyx990803@gmail.com>
2023-12-19 18:02:28 +0800

    chore: reuse descriptor ast in compileScript inlineTemplate mode by default

--- packages/compiler-sfc/src/compileScript.ts
+++ packages/compiler-sfc/src/compileScript.ts
@@ -833,6 +833,7 @@ export function compileScript(
       // inline it right here
       const { code, ast, preamble, tips, errors } = compileTemplate({
         filename,
+        ast: sfc.template.ast,
         source: sfc.template.content,
         inMap: sfc.template.map,
         ...options.templateOptions,
