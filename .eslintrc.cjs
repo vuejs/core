97431d6e1772a46a6f77a9769d8dc99f1cb45ca8
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-12-20 06:41:19 +0800

    fix: disable no-unnecessary-type-assertion

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -45,8 +45,6 @@ module.exports = {
     // This rule enforces the preference for using '@ts-expect-error' comments in TypeScript
     // code to indicate intentional type errors, improving code clarity and maintainability.
     '@typescript-eslint/prefer-ts-expect-error': 'error',
-    // Avoid unnecessary type assertions
-    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
     // Enforce the use of 'import type' for importing types
     '@typescript-eslint/consistent-type-imports': [
       'error',
