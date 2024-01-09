78fd18f60cfd42444b6c68556d56c2e96236faf2
Evan You <yyx990803@gmail.com>
2024-01-09 11:21:22 +0800

    test: improve test case

--- packages/runtime-core/__tests__/apiComputed.spec.ts
+++ packages/runtime-core/__tests__/apiComputed.spec.ts
@@ -1,26 +1,44 @@
 import {
+  type ComputedRef,
   computed,
   getCurrentInstance,
   h,
   nodeOps,
+  ref,
   render,
   serializeInner,
 } from '@vue/runtime-test'
 
 describe('api: computed', () => {
   test('should ensure currentInstance in computed getter', () => {
+    let c: ComputedRef
+    const n = ref(0)
+    const spy = vi.fn()
     const Comp = {
       setup() {
-        const c = computed(() => {
-          return !!getCurrentInstance()
+        c = computed(() => {
+          spy()
+          return !!getCurrentInstance() ? n.value : n.value + 1
         })
+        // access
+        c.value
+        expect(spy).toHaveBeenCalledTimes(1)
+        // should not break currentInstance
+        expect(getCurrentInstance()).toBeTruthy()
+
         return () => {
-          return String(c.value)
+          return c.value
         }
       },
     }
     const root = nodeOps.createElement('div')
     render(h(Comp), root)
-    expect(serializeInner(root)).toBe('true')
+    expect(serializeInner(root)).toBe(String(n.value))
+    expect(spy).toHaveBeenCalledTimes(1)
+
+    // invalidate caching so the getter is evaluated without active instance
+    n.value++
+    expect(c!.value).toBe(n.value)
+    expect(spy).toHaveBeenCalledTimes(2)
   })
 })
