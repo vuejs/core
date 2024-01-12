4c40cf3490898953aa477417f2173037fa18c786
Doctorwu <doctorwu@moego.pet>
2024-01-12 18:28:57 +0800

    fix(reactivity): correct dirty assign in recursive render
    close #10082

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -295,7 +295,9 @@ export function triggerEffects(
     }
     if (
       effect._dirtyLevel < dirtyLevel &&
-      (!effect._runnings || dirtyLevel !== DirtyLevels.ComputedValueDirty)
+      (!effect._runnings ||
+        effect.allowRecurse ||
+        dirtyLevel !== DirtyLevels.ComputedValueDirty)
     ) {
       const lastDirtyLevel = effect._dirtyLevel
       effect._dirtyLevel = dirtyLevel
