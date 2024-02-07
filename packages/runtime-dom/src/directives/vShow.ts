9b19f0912104bfccb10c8cf5beab02b21a648935
Doctor Wu <44631608+Doctor-wu@users.noreply.github.com>
2024-02-07 12:54:21 +0800

    fix(runtime-dom): ensure v-show respects display value set via v-bind (#10161)
    close #10151

--- packages/runtime-dom/src/directives/vShow.ts
+++ packages/runtime-dom/src/directives/vShow.ts
@@ -22,7 +22,7 @@ export const vShow: ObjectDirective<VShowElement> & { name?: 'show' } = {
     }
   },
   updated(el, { value, oldValue }, { transition }) {
-    if (!value === !oldValue) return
+    if (!value === !oldValue && el.style.display === el[vShowOldKey]) return
     if (transition) {
       if (value) {
         transition.beforeEnter(el)
