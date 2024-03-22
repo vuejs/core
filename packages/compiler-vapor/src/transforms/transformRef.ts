ed6b1718d2f166f8de588f814ea90a69f87d666f
Jevon <1787176370@qq.com>
2024-03-22 23:33:55 +0800

    fix(compiler-vapor): SET_REF operation should be registered last (#159)
    Co-authored-by: 三咲智子 Kevin Deng <sxzz@sxzz.moe>

--- packages/compiler-vapor/src/transforms/transformRef.ts
+++ packages/compiler-vapor/src/transforms/transformRef.ts
@@ -22,10 +22,10 @@ export const transformRef: NodeTransform = (node, context) => {
       ? createSimpleExpression(dir.value.content, true, dir.value.loc)
       : EMPTY_EXPRESSION
   }
-
-  context.registerOperation({
-    type: IRNodeTypes.SET_REF,
-    element: context.reference(),
-    value,
-  })
+  return () =>
+    context.registerOperation({
+      type: IRNodeTypes.SET_REF,
+      element: context.reference(),
+      value,
+    })
 }
