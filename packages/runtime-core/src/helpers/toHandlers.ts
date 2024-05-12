4f9052358e0b8363c9e4bdc9852a08eb34e321fd
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-05-13 02:19:11 +0800

    fix: pass warn to `toHandlers` only on dev mode

--- packages/runtime-core/src/helpers/toHandlers.ts
+++ packages/runtime-core/src/helpers/toHandlers.ts
@@ -1,4 +1,5 @@
 import { toHandlers as _toHandlers } from '@vue/runtime-shared'
 import { warn } from '../warning'
+import { NOOP } from '@vue/shared'
 
-export const toHandlers = _toHandlers.bind(undefined, warn)
+export const toHandlers = _toHandlers.bind(undefined, __DEV__ ? warn : NOOP)
