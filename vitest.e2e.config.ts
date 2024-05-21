2e2f3e2b96be4d6f62eed55e619744abf7622fee
Rizumu Ayaka <rizumu@ayaka.moe>
2024-05-21 08:43:46 +0800

    test(vue-vapor): todomvc e2e test (#115)

--- vitest.e2e.config.ts
+++ vitest.e2e.config.ts
@@ -8,6 +8,9 @@ export default mergeConfig(config, {
         singleThread: !!process.env.CI,
       },
     },
-    include: ['packages/vue/__tests__/e2e/*.spec.ts'],
+    include: [
+      'packages/vue/__tests__/e2e/*.spec.ts',
+      'packages/vue-vapor/__tests__/e2e/*.spec.ts',
+    ],
   },
 })
