8ae4c293adcec28f18114cb6016230a86787e6a9
Tycho <jh.leong@outlook.com>
2024-06-22 16:42:12 +0800

    fix(runtime-dom): prevent setting state as attribute for custom elements (#11165)
    close #11163

--- packages/runtime-dom/__tests__/customElement.spec.ts
+++ packages/runtime-dom/__tests__/customElement.spec.ts
@@ -9,6 +9,7 @@ import {
   inject,
   nextTick,
   ref,
+  render,
   renderSlot,
 } from '../src'
 
@@ -137,7 +138,7 @@ describe('defineCustomElement', () => {
 
   describe('props', () => {
     const E = defineCustomElement({
-      props: ['foo', 'bar', 'bazQux'],
+      props: ['foo', 'bar', 'bazQux', 'value'],
       render() {
         return [
           h('div', null, this.foo),
@@ -146,6 +147,12 @@ describe('defineCustomElement', () => {
       },
     })
     customElements.define('my-el-props', E)
+
+    test('renders custom element w/ correct object prop value', () => {
+      render(h('my-el-props', { value: { x: 1 } }), container)
+      const el = container.children[0]
+      expect((el as any).value).toEqual({ x: 1 })
+    })
 
     test('props via attribute', async () => {
       // bazQux should map to `baz-qux` attribute
