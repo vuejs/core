520330b7073e655a5330e0df807cac42597fba17
tycho <jh.leong@outlook.com>
2024-07-28 16:33:08 +0800

    fix: correct type inference for watch API when source is a ref from MaybeRef

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -32,7 +32,7 @@ export declare const RawSymbol: unique symbol
 
 export interface Ref<T = any, S = T> {
   get value(): T
-  set value(_: T | S)
+  set value(_: S)
   /**
    * Type differentiator only.
    * We need this to be in public d.ts but don't want it to show up in IDE
@@ -109,7 +109,7 @@ export function isRef(r: any): r is Ref {
  * @param value - The object to wrap in the ref.
  * @see {@link https://vuejs.org/api/reactivity-core.html#ref}
  */
-export function ref<T>(value: T): Ref<UnwrapRef<T>, T>
+export function ref<T>(value: T): Ref<UnwrapRef<T>, UnwrapRef<T> | T>
 export function ref<T = any>(): Ref<T | undefined>
 export function ref(value?: unknown) {
   return createRef(value, false)
