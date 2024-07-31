d14a11c1cdcee88452f17ce97758743c863958f4
Evan You <evan@vuejs.org>
2024-07-31 12:14:51 +0800

    feat: lazy hydration strategies for async components (#11458)

--- packages/runtime-core/src/componentOptions.ts
+++ packages/runtime-core/src/componentOptions.ts
@@ -199,6 +199,15 @@ export interface ComponentOptionsBase<
    * @internal
    */
   __asyncResolved?: ConcreteComponent
+  /**
+   * Exposed for lazy hydration
+   * @internal
+   */
+  __asyncHydrate?: (
+    el: Element,
+    instance: ComponentInternalInstance,
+    hydrate: () => void,
+  ) => void
 
   // Type differentiators ------------------------------------------------------
 
