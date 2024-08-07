045d0dfbf6e7d735a918d1a95ec9111166eabe4d
LiuSeen <91084928+liuseen-l@users.noreply.github.com>
2024-08-07 16:58:22 +0800

    feat: useModel test

--- packages/runtime-core/__tests__/helpers/useModel.spec.ts
+++ packages/runtime-core/__tests__/helpers/useModel.spec.ts
@@ -657,4 +657,50 @@ describe('useModel', () => {
     expect(setValue).toBeCalledTimes(2)
     expect(msg.value).toBe(defaultVal)
   })
+
+    test('custom setter',() => {
+    let changeChildMsg!: (val: boolean) => void
+
+    const Comp = defineComponent({
+      props: ['msg'],
+      emits: ['update:msg'],
+      setup(props) {
+        const childMsg = useModel(props, 'msg',{
+          set: (value) => {
+            if (value === msg.value) {
+              return null;
+            } else {
+              return value;
+            }
+          }
+        })
+        changeChildMsg = (val: boolean) => (childMsg.value = val)
+        return () => {
+          return childMsg.value
+        }
+      },
+    })
+
+    const defaultVal = false
+    const msg = ref(defaultVal)
+    const Parent = defineComponent({
+      setup() {
+        return () =>
+          h(Comp, {
+            msg: msg.value,
+            'onUpdate:msg': val => {
+              msg.value = val
+            },
+          })
+      },
+    })
+
+    const root = nodeOps.createElement('div')
+    render(h(Parent), root)
+
+    changeChildMsg(true)
+    expect(msg.value).toBe(true)
+
+    changeChildMsg(true)
+    expect(msg.value).toBe(null)
 })
