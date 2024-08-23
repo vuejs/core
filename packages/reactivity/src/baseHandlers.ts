4a537ce6a1e90c396d9ba323d96fbfda85f6df64
daiwei <daiwei521@126.com>
2024-08-23 13:54:44 +0800

    test: update test case

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -53,7 +53,6 @@ class BaseReactiveHandler implements ProxyHandler<Target> {
   ) {}
 
   get(target: Target, key: string | symbol, receiver: object): any {
-    if (key === ReactiveFlags.SKIP) return target[ReactiveFlags.SKIP]
     const isReadonly = this._isReadonly,
       isShallow = this._isShallow
     if (key === ReactiveFlags.IS_REACTIVE) {
