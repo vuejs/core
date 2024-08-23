5f988c21693da8827d63bedff811d0f081b14fc8
linzhe141 <1572213544@qq.com>
2024-08-23 14:14:22 +0800

    fix: fix guardReactiveProps in v-bind case

--- packages/runtime-core/src/vnode.ts
+++ packages/runtime-core/src/vnode.ts
@@ -642,11 +642,25 @@ function _createVNode(
   )
 }
 
+function generateProps(props: Data & VNodeProps) {
+  const target: Data & VNodeProps = {}
+  for (const key in props) {
+    if (isProxy(props[key])) {
+      target[key] = extend({}, props[key])
+    } else {
+      target[key] = props[key]
+    }
+  }
+  return target
+}
+
 export function guardReactiveProps(
   props: (Data & VNodeProps) | null,
 ): (Data & VNodeProps) | null {
   if (!props) return null
-  return isProxy(props) || isInternalObject(props) ? extend({}, props) : props
+  return isProxy(props) || isInternalObject(props)
+    ? extend({}, generateProps(props))
+    : props
 }
 
 export function cloneVNode<T, U>(
