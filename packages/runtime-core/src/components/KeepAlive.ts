49a1e148a321cdd6972ea8942f5bf7c9fdfeda9a
linzhe141 <1572213544@qq.com>
2024-08-26 17:58:04 +0800

    fix(runtime-core):  When the component does not change keepalive, it should be excluded

--- packages/runtime-core/src/components/KeepAlive.ts
+++ packages/runtime-core/src/components/KeepAlive.ts
@@ -310,6 +310,7 @@ const KeepAliveImpl: ComponentOptions = {
         (include && (!name || !matches(include, name))) ||
         (exclude && name && matches(exclude, name))
       ) {
+        vnode.shapeFlag &= ~ShapeFlags.COMPONENT_SHOULD_KEEP_ALIVE
         current = vnode
         return rawVNode
       }
