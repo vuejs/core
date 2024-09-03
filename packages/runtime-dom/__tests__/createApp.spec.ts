93be5857880ab4630ebd0472ea5d35f94d09d874
linzhe141 <1572213544@qq.com>
2024-09-03 14:05:28 +0800

    chore: update test

--- packages/runtime-dom/__tests__/createApp.spec.ts
+++ packages/runtime-dom/__tests__/createApp.spec.ts
@@ -1,4 +1,13 @@
-import { createApp, h } from '../src'
+import {
+  KeepAlive,
+  Transition,
+  createApp,
+  h,
+  nextTick,
+  onDeactivated,
+  onUnmounted,
+  ref,
+} from '../src'
 
 describe('createApp for dom', () => {
   // #2926
@@ -38,5 +47,56 @@ describe('createApp for dom', () => {
 
     // ensure no mutation happened to Root object
     expect(originalObj).toMatchObject(Root)
+  })
+
+  test('Transition + KeepAlive', async () => {
+    const deactivated = vi.fn()
+    const unmounted = vi.fn()
+    const CompA = {
+      name: 'CompA',
+      setup() {
+        return () => h('h1', 'CompA')
+      },
+    }
+    const CompB = {
+      name: 'CompB',
+      setup() {
+        onDeactivated(deactivated)
+        onUnmounted(unmounted)
+        return () => h('h1', 'CompB')
+      },
+    }
+
+    const current = ref('CompA')
+    const cacheList = ref(['CompA', 'CompB'])
+    const App = createApp({
+      setup() {
+        return () =>
+          h(
+            Transition,
+            { mode: 'out-in' },
+            {
+              default: () => [
+                h(
+                  KeepAlive,
+                  {
+                    include: cacheList.value,
+                  },
+                  [current.value === 'CompA' ? h(CompA) : h(CompB)],
+                ),
+              ],
+            },
+          )
+      },
+    })
+    App.mount(document.createElement('div'))
+    current.value = 'CompB'
+    await new Promise(resolve => setTimeout(resolve, 500))
+    await nextTick()
+    cacheList.value.splice(1)
+    current.value = 'CompA'
+    await nextTick()
+    expect(deactivated).toHaveBeenCalledTimes(1)
+    expect(unmounted).toHaveBeenCalledTimes(1)
   })
 })
