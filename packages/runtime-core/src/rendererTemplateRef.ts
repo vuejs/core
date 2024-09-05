980185626ba9727cc964dcfb2c48e5a337f7a3eb
linzhe141 <1572213544@qq.com>
2024-09-05 09:20:36 +0800

    chore: update

--- packages/runtime-core/src/rendererTemplateRef.ts
+++ packages/runtime-core/src/rendererTemplateRef.ts
@@ -68,15 +68,17 @@ export function setRef(
       ? () => false
       : (key: string) => {
           if (!hasOwn(setupState, key)) return false
-          if (!__DEV__) return true
-          if (
-            setupState.__v__setupResult &&
-            // @ts-expect-error
-            setupState.__v__setupResult[ref] &&
-            // @ts-expect-error
-            setupState.__v__setupResult[ref].__v__TemplateRef
-          ) {
-            return false
+          if (__DEV__) {
+            if (
+              setupState.__v__setupResult &&
+              // @ts-expect-error
+              setupState.__v__setupResult[ref] &&
+              // @ts-expect-error
+              setupState.__v__setupResult[ref].__v__TemplateRef
+            ) {
+              return false
+            }
+            return true
           }
           return true
         }
