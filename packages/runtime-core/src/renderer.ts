222eb532fed00cd8a4f7b61960f5969f8ca81772
linzhe141 <1572213544@qq.com>
2024-10-25 13:14:29 +0800

    chore: update

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -1328,7 +1328,10 @@ function baseCreateRenderer(
               parentSuspense,
               null,
             )
-            initialVNode.el = instance.subTree.el
+            // when data-allow-mismatch is set, the component vnode's el may be incorrect, so it should be updated
+            if (initialVNode) {
+              initialVNode.el = instance.subTree.el
+            }
             if (__DEV__) {
               endMeasure(instance, `hydrate`)
             }
