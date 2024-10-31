71284faa967a3b8ab29d1f78a51d45f72a0f5905
daiwei <daiwei521@126.com>
2024-10-31 20:42:52 +0800

    chore: update

--- packages/runtime-core/src/components/Teleport.ts
+++ packages/runtime-core/src/components/Teleport.ts
@@ -480,7 +480,7 @@ function updateCssVars(vnode: VNode, isDisabled: boolean) {
       if (node.nodeType === 1) node.setAttribute('data-v-owner', ctx.uid)
       node = node.nextSibling
     }
-    ctx.ut(node.target)
+    ctx.ut(vnode.target)
   }
 }
 
