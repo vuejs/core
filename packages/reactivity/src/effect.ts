2d832a41f8bc1906fc4e6bcf404216cacb48c302
Johnson Chu <johnsoncodehk@gmail.com>
2024-11-14 08:57:17 +0800

    Fix link

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -300,7 +300,7 @@ function cleanupEffect(e: ReactiveEffect) {
   }
 }
 
-//#region Ported from https://github.com/stackblitz/alien-signals/blob/2f3656041a183956a91f805582fcd33026ed46a3/src/ts
+//#region Ported from https://github.com/stackblitz/alien-signals/blob/2f3656041a183956a91f805582fcd33026ed46a3/src/system.ts
 export interface IEffect extends Subscriber {
   nextNotify: IEffect | undefined
   notify(): void
