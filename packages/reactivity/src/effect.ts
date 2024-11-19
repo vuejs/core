c7116ca5b5a637f2810860652fcd0026ed2c0ee6
Johnson Chu <johnsoncodehk@gmail.com>
2024-11-19 08:56:38 +0800

    Revert "Sync alien-signals last commit"
    This reverts commit 66dd59c95164348bb0b625596fef794c856d4448.

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -538,27 +538,31 @@ export function checkDirty(deps: Link): boolean {
         if (dep.update()) {
           propagate(dep.subs!)
           let dirty = true
-          while (stack > 0) {
-            stack--
-            const sub = deps.sub as IComputed
-            const subSubs = sub.subs!
-            const prevLink = subSubs.prevSub!
-            subSubs.prevSub = undefined
-            if (dirty) {
-              if (sub.update()) {
-                propagate(subSubs)
-                deps = prevLink
-                dirty = true
-                continue
+          if (stack > 0) {
+            let sub = deps.sub as IComputed
+            do {
+              stack--
+              const subSubs = sub.subs!
+              const prevLink = subSubs.prevSub!
+              subSubs.prevSub = undefined
+              if (dirty) {
+                if (sub.update()) {
+                  propagate(subSubs)
+                  deps = prevLink
+                  sub = prevLink.sub as IComputed
+                  dirty = true
+                  continue
+                }
+              } else {
+                sub.dirtyLevel = DirtyLevels.None
               }
-            } else {
-              sub.dirtyLevel = DirtyLevels.None
-            }
-            deps = prevLink.nextDep!
-            if (deps !== undefined) {
-              continue top
-            }
-            dirty = false
+              deps = prevLink.nextDep!
+              if (deps !== undefined) {
+                continue top
+              }
+              dirty = false
+              sub = prevLink.sub as IComputed
+            } while (stack > 0)
           }
           return dirty
         }
@@ -568,27 +572,31 @@ export function checkDirty(deps: Link): boolean {
     const nextDep = deps.nextDep!
     if (nextDep === undefined) {
       let dirty = false
-      while (stack > 0) {
-        stack--
-        const sub = deps.sub as IComputed
-        const subSubs = sub.subs!
-        const prevLink = subSubs.prevSub!
-        subSubs.prevSub = undefined
-        if (dirty) {
-          if (sub.update()) {
-            propagate(subSubs)
-            deps = prevLink
-            dirty = true
-            continue
+      if (stack > 0) {
+        let sub = deps.sub as IComputed
+        do {
+          stack--
+          const subSubs = sub.subs!
+          const prevLink = subSubs.prevSub!
+          subSubs.prevSub = undefined
+          if (dirty) {
+            if (sub.update()) {
+              propagate(subSubs)
+              deps = prevLink
+              sub = prevLink.sub as IComputed
+              dirty = true
+              continue
+            }
+          } else {
+            sub.dirtyLevel = DirtyLevels.None
           }
-        } else {
-          sub.dirtyLevel = DirtyLevels.None
-        }
-        deps = prevLink.nextDep!
-        if (deps !== undefined) {
-          continue top
-        }
-        dirty = false
+          deps = prevLink.nextDep!
+          if (deps !== undefined) {
+            continue top
+          }
+          sub = prevLink.sub as IComputed
+          dirty = false
+        } while (stack > 0)
       }
       return dirty
     }
