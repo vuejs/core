a3edc274e46f15453241c625c7aafb706e607c7d
Evan You <evan@vuejs.org>
2024-12-01 18:21:26 +0800

    chore: remove unnecessary config in dev config [ci skip]

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -5,15 +5,6 @@ import Vue from '@vitejs/plugin-vue'
 import * as CompilerSFC from '@vue/compiler-sfc'
 
 export default defineConfig({
-  build: {
-    target: 'esnext',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        pure_getters: true,
-      },
-    },
-  },
   clearScreen: false,
   plugins: [
     Vue({
