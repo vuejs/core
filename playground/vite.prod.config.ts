d6415d84424f7a5b4ac8e799e77fef8dfab824f8
Evan You <evan@vuejs.org>
2024-12-01 17:37:17 +0800

    chore: use actual production build for playground build and preview

---
+++ playground/vite.prod.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import * as CompilerSFC from '@vue/compiler-sfc'
+
+export default defineConfig({
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        pure_getters: true,
+      },
+    },
+  },
+  clearScreen: false,
+  plugins: [
+    Vue({
+      compiler: CompilerSFC,
+    }),
+  ],
+})
