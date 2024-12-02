0986051f1242ee95b667fa965d6e5f7c46e543c3
Evan You <evan@vuejs.org>
2024-12-02 21:00:52 +0800

    chore: avoid toHandlers not treeshaken

--- packages/runtime-vapor/src/helpers/toHandlers.ts
+++ packages/runtime-vapor/src/helpers/toHandlers.ts
@@ -2,7 +2,8 @@ import { toHandlers as _toHandlers } from '@vue/runtime-shared'
 import { warn } from '../warning'
 import { NOOP } from '@vue/shared'
 
-export const toHandlers: (
+export const toHandlers = (
   obj: Record<string, any>,
   preserveCaseIfNecessary?: boolean | undefined,
-) => Record<string, any> = _toHandlers.bind(undefined, __DEV__ ? warn : NOOP)
+): Record<string, any> =>
+  _toHandlers(__DEV__ ? warn : NOOP, obj, preserveCaseIfNecessary)
