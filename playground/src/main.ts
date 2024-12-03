e88c4e2ea7bfbbba881a69cfadd9f63bea9c3c54
Evan You <evan@vuejs.org>
2024-12-03 22:55:48 +0800

    chore: playground

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,13 +1,1 @@
-import { createComponentSimple, createVaporAppSimple } from 'vue/vapor'
-import List from './list'
-import Props from './props'
-import './style.css'
-
-const s = performance.now()
-const app = createVaporAppSimple({
-  setup() {
-    return [createComponentSimple(Props), createComponentSimple(List)]
-  },
-})
-app.mount('#app')
-console.log((performance.now() - s).toFixed(2))
+import './_entry'
