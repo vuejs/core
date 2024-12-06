f6f3f14a3e1144cebe7b8c1922b6a36c71cc8ed9
Evan You <evan@vuejs.org>
2024-12-06 21:07:42 +0800

    chore: update playground vapor welcome

--- packages-private/sfc-playground/src/welcome.vue
+++ packages-private/sfc-playground/src/welcome.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
-import { ref, getCurrentInstance } from 'vue'
+import { ref } from 'vue'
 
 const msg = ref('Hello World!')
-// @ts-expect-error
-const isVapor = getCurrentInstance().vapor
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-  <p>
-    VAPOR <b>{{ isVapor ? 'ON' : 'OFF' }}</b>
-  </p>
-  <input v-model="msg" />
 </template>
