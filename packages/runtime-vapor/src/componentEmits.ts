1f911a57b1d5e95137d602fb21508959171ee273
daiwei <daiwei521@126.com>
2024-12-24 21:22:12 +0800

    fix(compiler-vapor): once modifier work with component event

--- packages/runtime-vapor/src/componentEmits.ts
+++ packages/runtime-vapor/src/componentEmits.ts
@@ -46,7 +46,7 @@ function propGetter(rawProps: Record<string, any>, key: string) {
     let i = dynamicSources.length
     while (i--) {
       const source = resolveSource(dynamicSources[i])
-      if (hasOwn(source, key)) return source[key]
+      if (hasOwn(source, key)) return resolveSource(source[key])
     }
   }
   return rawProps[key] && resolveSource(rawProps[key])
