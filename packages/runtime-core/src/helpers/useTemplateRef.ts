b9cd226d2342f78464487bc8aca0cc4aff3d27d3
daiwei <daiwei521@126.com>
2025-01-21 10:33:41 +0800

    chore: minor tweaks

--- packages/runtime-core/src/helpers/useTemplateRef.ts
+++ packages/runtime-core/src/helpers/useTemplateRef.ts
@@ -36,7 +36,7 @@ export function useTemplateRef<T = unknown, Keys extends string = string>(
 
 export function isUseTemplateRefKey(refs: Data, key: string): boolean {
   let desc: PropertyDescriptor | undefined
-  return (
+  return !!(
     (desc = Object.getOwnPropertyDescriptor(refs, key)) && !desc.configurable
   )
 }
