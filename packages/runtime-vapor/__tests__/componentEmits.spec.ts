9df9281b506451667e0d5cc5e67420028a091224
zhiyuanzmj <260480378@qq.com>
2025-03-20 10:35:30 +0800

    feat: use for vapor

--- packages/runtime-vapor/__tests__/componentEmits.spec.ts
+++ packages/runtime-vapor/__tests__/componentEmits.spec.ts
@@ -265,7 +265,7 @@ describe('component: emit', () => {
     const fn2 = vi.fn()
     render({
       modelValue: () => null,
-      modelModifiers: () => ({ number: true }),
+      modelValueModifiers: () => ({ number: true }),
       ['onUpdate:modelValue']: () => fn1,
       foo: () => null,
       fooModifiers: () => ({ number: true }),
@@ -291,7 +291,7 @@ describe('component: emit', () => {
       modelValue() {
         return null
       },
-      modelModifiers() {
+      modelValueModifiers() {
         return { trim: true }
       },
       ['onUpdate:modelValue']() {
@@ -327,7 +327,7 @@ describe('component: emit', () => {
       modelValue() {
         return null
       },
-      modelModifiers() {
+      modelValueModifiers() {
         return { trim: true, number: true }
       },
       ['onUpdate:modelValue']() {
@@ -361,7 +361,7 @@ describe('component: emit', () => {
       modelValue() {
         return null
       },
-      modelModifiers() {
+      modelValueModifiers() {
         return { trim: true }
       },
       ['onUpdate:modelValue']() {
