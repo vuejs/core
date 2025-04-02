8684696a9ff930317eaa3f05a99b6e100bd00c98
daiwei <daiwei521@126.com>
2025-04-02 10:54:28 +0800

    fix(compiler-core): adjust COMPILER_V_BIND_SYNC warning content

--- packages/compiler-core/src/parser.ts
+++ packages/compiler-core/src/parser.ts
@@ -388,7 +388,7 @@ const tokenizer = new Tokenizer(stack, {
               CompilerDeprecationTypes.COMPILER_V_BIND_SYNC,
               currentOptions,
               currentProp.loc,
-              currentProp.rawName,
+              currentProp.exp.content,
             )
           ) {
             currentProp.name = 'model'
