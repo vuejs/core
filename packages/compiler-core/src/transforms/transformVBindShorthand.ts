fd7d6cf67e9eb3e4bcc8c609690c61883774f9aa
daiwei <daiwei521@126.com>
2025-06-05 14:52:12 +0800

    refactor(compile): add independent transform for VBindShorthand

---
+++ packages/compiler-core/src/transforms/transformVBindShorthand.ts
@@ -0,0 +1,36 @@
+import { camelize } from '@vue/shared'
+import {
+  NodeTypes,
+  type SimpleExpressionNode,
+  createSimpleExpression,
+} from '../ast'
+import type { NodeTransform } from '../transform'
+import { ErrorCodes, createCompilerError } from '../errors'
+
+export const transformVBindShorthand: NodeTransform = (node, context) => {
+  if (node.type === NodeTypes.ELEMENT) {
+    for (const prop of node.props) {
+      // same-name shorthand - :arg is expanded to :arg="arg"
+      if (
+        prop.type === NodeTypes.DIRECTIVE &&
+        prop.name === 'bind' &&
+        !prop.exp
+      ) {
+        const arg = prop.arg!
+        if (arg.type !== NodeTypes.SIMPLE_EXPRESSION || !arg.isStatic) {
+          // only simple expression is allowed for same-name shorthand
+          context.onError(
+            createCompilerError(
+              ErrorCodes.X_V_BIND_INVALID_SAME_NAME_ARGUMENT,
+              arg.loc,
+            ),
+          )
+          prop.exp = createSimpleExpression('', true, arg.loc)
+        } else {
+          const propName = camelize((arg as SimpleExpressionNode).content)
+          prop.exp = createSimpleExpression(propName, false, arg.loc)
+        }
+      }
+    }
+  }
+}
