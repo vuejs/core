34d7300ac112146453ad8caad189ded7948b499d
IlyaL <ilyaliao324@gmail.com>
2025-07-05 12:30:51 +0800

    perf(shared): use loose equality in isObject for better performance

--- packages/shared/src/general.ts
+++ packages/shared/src/general.ts
@@ -51,7 +51,7 @@ export const isFunction = (val: unknown): val is Function =>
 export const isString = (val: unknown): val is string => typeof val === 'string'
 export const isSymbol = (val: unknown): val is symbol => typeof val === 'symbol'
 export const isObject = (val: unknown): val is Record<any, any> =>
-  val !== null && typeof val === 'object'
+  val != null && typeof val === 'object'
 
 export const isPromise = <T = any>(val: unknown): val is Promise<T> => {
   return (
