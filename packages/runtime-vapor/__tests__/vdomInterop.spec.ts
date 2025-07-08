0c927dfad6baf72826567521f509355884ec8948
zhiyuanzmj <260480378@qq.com>
2025-07-08 14:45:37 +0800

    chore: remove console.log

--- packages/runtime-vapor/__tests__/vdomInterop.spec.ts
+++ packages/runtime-vapor/__tests__/vdomInterop.spec.ts
@@ -42,7 +42,6 @@ describe('vdomInterop', () => {
     test('functional slot', () => {
       const VDomChild = defineComponent({
         setup(_, { slots }) {
-          console.log(slots.default)
           return () => createVNode(slots.default!)
         },
       })
