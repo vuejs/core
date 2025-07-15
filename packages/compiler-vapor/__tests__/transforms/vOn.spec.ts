1920cdd11d86f63ced9589ad879c21eb13ca5d58
zhiyuanzmj <260480378@qq.com>
2025-07-15 10:12:58 +0800

    chore: update

--- packages/compiler-vapor/__tests__/transforms/vOn.spec.ts
+++ packages/compiler-vapor/__tests__/transforms/vOn.spec.ts
@@ -696,11 +696,15 @@ describe('v-on', () => {
     expect(code).include('n0.$evtclick = e => _ctx.handleClick(e)')
   })
 
-  test('component event with argument', () => {
-    const { code } = compileWithVOn(`<Foo @update:model="() => {}"/>`)
+  test('component event with special characters', () => {
+    const { code } = compileWithVOn(
+      `<Foo @update:model="() => {}" @update-model="() => {}" />`,
+    )
 
     expect(code).matchSnapshot()
     expect(code).contains('const _on_update_model = () => {}')
-    expect(code).contains('{ "onUpdate:model": () => _on_update_model }')
+    expect(code).contains('const _on_update_model1 = () => {}')
+    expect(code).contains('"onUpdate:model": () => _on_update_model')
+    expect(code).contains('"onUpdate-model": () => _on_update_model1')
   })
 })
