9296e96354a0a498b23e09170bf61537d1fcab85
Rizumu Ayaka <rizumu@ayaka.moe>
2025-08-24 17:54:32 +0800

    feat: template + get child + createXXX functions use Draft Node

--- packages/runtime-vapor/src/dom/node.ts
+++ packages/runtime-vapor/src/dom/node.ts
@@ -1,29 +1,59 @@
 /*! #__NO_SIDE_EFFECTS__ */
-export function createTextNode(value = ''): Text {
+
+import { isHydrating } from './hydration'
+import {
+  CommentDraft,
+  NodeRef,
+  TextNodeDraft,
+  type VaporNode,
+  type VaporParentNode,
+  isUnresolvedNode,
+  toNode,
+} from './nodeDraft'
+
+export function createTextNode(value = ''): VaporNode<Text, TextNodeDraft> {
+  if (isHydrating) {
+    const node = new NodeRef<boolean, Text, TextNodeDraft>(TextNodeDraft)
+    node.ref.textContent
+    return node
+  }
   return document.createTextNode(value)
 }
 
 /*! #__NO_SIDE_EFFECTS__ */
-export function createComment(data: string): Comment {
+export function createComment(data: string): VaporNode<Comment, CommentDraft> {
+  if (isHydrating) {
+    const node = new NodeRef<boolean, Comment, CommentDraft>(CommentDraft)
+    node.ref.data
+    return node
+  }
   return document.createComment(data)
 }
 
 /*! #__NO_SIDE_EFFECTS__ */
-export function querySelector(selectors: string): Element | null {
-  return document.querySelector(selectors)
+export function child(node: VaporParentNode): VaporNode {
+  if (isUnresolvedNode(node) && !node.ref.childNodes[0]) {
+    return (node.ref.childNodes[0] = new NodeRef())
+  }
+
+  return toNode(node).firstChild!
 }
 
 /*! #__NO_SIDE_EFFECTS__ */
-export function child(node: ParentNode): Node {
-  return node.firstChild!
+export function nthChild(node: ParentNode, i: number): VaporNode {
+  if (isUnresolvedNode(node) && !node.ref.childNodes[i]) {
+    return (node.ref.childNodes[i] = new NodeRef())
+  }
+
+  return toNode(node).childNodes[i]
 }
 
 /*! #__NO_SIDE_EFFECTS__ */
-export function nthChild(node: Node, i: number): Node {
-  return node.childNodes[i]
-}
+export function next(node: VaporParentNode): VaporNode {
+  if (isUnresolvedNode(node) && !node.ref.nextSibling) {
+    const childNodes = node.ref.childNodes
+    return (childNodes[childNodes.indexOf(node) + 1] = new NodeRef())
+  }
 
-/*! #__NO_SIDE_EFFECTS__ */
-export function next(node: Node): Node {
-  return node.nextSibling!
+  return toNode(node).nextSibling!
 }
