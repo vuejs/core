a9e429c6105fce04acce8e29280e8f98c2ae4cc1
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2025-09-26 17:16:02 +0200

    test(vapor): use browser mode instead of pupeteer to run tests

---
+++ packages-private/vapor-e2e-test/__tests__/e2eUtils.ts
@@ -0,0 +1,9 @@
+import { type Locator, page, userEvent } from '@vitest/browser/context'
+
+export const css = (css: string) => page.getByCSS(css)
+export const E2E_TIMEOUT: number = 30 * 1000
+
+export async function enterValue(locator: Locator, text: string) {
+  await locator.fill(text)
+  await userEvent.type(locator, '{enter}')
+}
--- packages-private/vapor-e2e-test/__tests__/vdomInterop.spec.ts
+++ packages-private/vapor-e2e-test/__tests__/vdomInterop.spec.ts
@@ -1,84 +1,62 @@
-import path from 'node:path'
-import {
-  E2E_TIMEOUT,
-  setupPuppeteer,
-} from '../../../packages/vue/__tests__/e2e/e2eUtils'
-import connect from 'connect'
-import sirv from 'sirv'
+import { createApp, vaporInteropPlugin } from 'vue'
+import App from '../interop/App.vue'
+import { E2E_TIMEOUT, css } from './e2eUtils'
 
 describe('vdom / vapor interop', () => {
-  const { page, click, text, enterValue } = setupPuppeteer()
+  test('should work', { timeout: E2E_TIMEOUT }, async () => {
+    createApp(App).use(vaporInteropPlugin).mount('#app')
 
-  let server: any
-  const port = '8193'
-  beforeAll(() => {
-    server = connect()
-      .use(sirv(path.resolve(import.meta.dirname, '../dist')))
-      .listen(port)
-    process.on('SIGTERM', () => server && server.close())
+    await expect
+      .element(css('.vapor > h2'))
+      .toHaveTextContent('Vapor component in VDOM')
+
+    expect(css('.vapor-prop')).toHaveTextContent('hello')
+
+    const l = css('.vdom-slot-in-vapor-default')
+    expect(l).toHaveTextContent('slot prop: slot prop')
+    expect(l).toHaveTextContent('component prop: hello')
+
+    await css('.change-vdom-slot-in-vapor-prop').click()
+    expect(css('.vdom-slot-in-vapor-default')).toHaveTextContent(
+      'slot prop: changed',
+    )
+
+    expect(css('.vdom-slot-in-vapor-test')).toHaveTextContent('A test slot')
+
+    await css('.toggle-vdom-slot-in-vapor').click()
+    expect(css('.vdom-slot-in-vapor-test')).toHaveTextContent(
+      'fallback content',
+    )
+
+    await css('.toggle-vdom-slot-in-vapor').click()
+    expect(css('.vdom-slot-in-vapor-test')).toHaveTextContent('A test slot')
+
+    expect(css('.vdom > h2')).toHaveTextContent('VDOM component in Vapor')
+
+    expect(css('.vdom-prop')).toHaveTextContent('hello')
+
+    const tt = css('.vapor-slot-in-vdom-default')
+    expect(tt).toHaveTextContent('slot prop: slot prop')
+    expect(tt).toHaveTextContent('component prop: hello')
+
+    await css('.change-vapor-slot-in-vdom-prop').click()
+    expect(css('.vapor-slot-in-vdom-default')).toHaveTextContent(
+      'slot prop: changed',
+    )
+
+    expect(css('.vapor-slot-in-vdom-test')).toHaveTextContent('fallback')
+
+    await css('.toggle-vapor-slot-in-vdom-default').click()
+    expect(css('.vapor-slot-in-vdom-default')).toHaveTextContent(
+      'default slot fallback',
+    )
+
+    await css('.toggle-vapor-slot-in-vdom-default').click()
+
+    await css('input').fill('bye')
+    expect(css('.vapor-prop')).toHaveTextContent('bye')
+    expect(css('.vdom-slot-in-vapor-default')).toHaveTextContent('bye')
+    expect(css('.vdom-prop')).toHaveTextContent('bye')
+    expect(css('.vapor-slot-in-vdom-default')).toHaveTextContent('bye')
   })
-
-  afterAll(() => {
-    server.close()
-  })
-
-  test(
-    'should work',
-    async () => {
-      const baseUrl = `http://localhost:${port}/interop/`
-      await page().goto(baseUrl)
-
-      expect(await text('.vapor > h2')).toContain('Vapor component in VDOM')
-
-      expect(await text('.vapor-prop')).toContain('hello')
-
-      const t = await text('.vdom-slot-in-vapor-default')
-      expect(t).toContain('slot prop: slot prop')
-      expect(t).toContain('component prop: hello')
-
-      await click('.change-vdom-slot-in-vapor-prop')
-      expect(await text('.vdom-slot-in-vapor-default')).toContain(
-        'slot prop: changed',
-      )
-
-      expect(await text('.vdom-slot-in-vapor-test')).toContain('A test slot')
-
-      await click('.toggle-vdom-slot-in-vapor')
-      expect(await text('.vdom-slot-in-vapor-test')).toContain(
-        'fallback content',
-      )
-
-      await click('.toggle-vdom-slot-in-vapor')
-      expect(await text('.vdom-slot-in-vapor-test')).toContain('A test slot')
-
-      expect(await text('.vdom > h2')).toContain('VDOM component in Vapor')
-
-      expect(await text('.vdom-prop')).toContain('hello')
-
-      const tt = await text('.vapor-slot-in-vdom-default')
-      expect(tt).toContain('slot prop: slot prop')
-      expect(tt).toContain('component prop: hello')
-
-      await click('.change-vapor-slot-in-vdom-prop')
-      expect(await text('.vapor-slot-in-vdom-default')).toContain(
-        'slot prop: changed',
-      )
-
-      expect(await text('.vapor-slot-in-vdom-test')).toContain('fallback')
-
-      await click('.toggle-vapor-slot-in-vdom-default')
-      expect(await text('.vapor-slot-in-vdom-default')).toContain(
-        'default slot fallback',
-      )
-
-      await click('.toggle-vapor-slot-in-vdom-default')
-
-      await enterValue('input', 'bye')
-      expect(await text('.vapor-prop')).toContain('bye')
-      expect(await text('.vdom-slot-in-vapor-default')).toContain('bye')
-      expect(await text('.vdom-prop')).toContain('bye')
-      expect(await text('.vapor-slot-in-vdom-default')).toContain('bye')
-    },
-    E2E_TIMEOUT,
-  )
 })
