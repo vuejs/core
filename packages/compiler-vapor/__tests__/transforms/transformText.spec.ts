d80e25e6b4830401b46407a2ee904aa89bcca97f
zhiyuanzmj <260480378@qq.com>
2025-10-02 15:39:19 +0800

    chore: update

--- packages/compiler-vapor/__tests__/transforms/transformText.spec.ts
+++ packages/compiler-vapor/__tests__/transforms/transformText.spec.ts
@@ -62,10 +62,11 @@ describe('compiler: text transform', () => {
           {{ (2) }}
           {{ \`foo\` }}
           {{ 1 }}
+          {{ 1n }}
           {{ '1' }}
         </div>`,
     )
-    expect(code).includes(`_template("<div>2 foo1 1 1</div>", true)`)
+    expect(code).includes(`_template("<div>2 foo 1 1 1</div>", true)`)
     expect(code).toMatchSnapshot()
   })
 })
