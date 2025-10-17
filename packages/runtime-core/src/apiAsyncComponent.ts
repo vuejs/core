9498a8b19727cc35f4197eb5160e31d9403e83cd
indykoning <15870933+indykoning@users.noreply.github.com>
2025-10-17 14:01:58 +0200

    Pass props and children to loadingComponent

--- packages/runtime-core/src/apiAsyncComponent.ts
+++ packages/runtime-core/src/apiAsyncComponent.ts
@@ -241,7 +241,7 @@ export function defineAsyncComponent<
             error: error.value,
           })
         } else if (loadingComponent && !delayed.value) {
-          return createVNode(loadingComponent)
+          return createInnerComp(loadingComponent, instance)
         }
       }
     },
