702adcbc28f0af4f4020cc6612a3d68e6109238e
indykoning <15870933+indykoning@users.noreply.github.com>
2025-10-17 17:27:07 +0200

    Added types
    Co-authored-by: edison <daiwei521@126.com>

--- packages/runtime-core/src/apiAsyncComponent.ts
+++ packages/runtime-core/src/apiAsyncComponent.ts
@@ -241,7 +241,7 @@ export function defineAsyncComponent<
             error: error.value,
           })
         } else if (loadingComponent && !delayed.value) {
-          return createInnerComp(loadingComponent, instance)
+          return createInnerComp(loadingComponent as ConcreteComponent, instance)
         }
       }
     },
