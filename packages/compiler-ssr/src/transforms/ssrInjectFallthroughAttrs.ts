b58dfa9c631c1a4c79b4aa7cc9537e725e3a5cd5
daiwei <daiwei521@126.com>
2025-11-24 11:23:15 +0800

    refactor(compiler-ssr): simplify child node filtering by using isCommentOrWhitespace utility

--- packages/compiler-ssr/src/transforms/ssrInjectFallthroughAttrs.ts
+++ packages/compiler-ssr/src/transforms/ssrInjectFallthroughAttrs.ts
@@ -7,13 +7,12 @@ import {
   type TemplateChildNode,
   createSimpleExpression,
   findDir,
+  isCommentOrWhitespace,
   locStub,
 } from '@vue/compiler-dom'
 
 const filterChild = (node: ParentNode) =>
-  node.children.filter(
-    n => n.type !== NodeTypes.COMMENT && n.type !== NodeTypes.TEXT,
-  )
+  node.children.filter(n => !isCommentOrWhitespace(n))
 
 const hasSingleChild = (node: ParentNode): boolean =>
   filterChild(node).length === 1
