6e0cfd19d79b88a60ec6efaf032aa02613036824
daiwei <daiwei521@126.com>
2025-11-26 14:58:33 +0800

    test(scopeId): add tests for slot wrapping with `withVaporCtx`

--- packages/compiler-vapor/__tests__/scopeId.spec.ts
+++ packages/compiler-vapor/__tests__/scopeId.spec.ts
@@ -1,3 +1,58 @@
-// import { compile } from '../src/compile'
+import type { RootNode } from '@vue/compiler-dom'
+import { type CompilerOptions, compile as _compile } from '../src'
 
-describe.todo('scopeId compiler support', () => {})
+function compile(template: string | RootNode, options: CompilerOptions = {}) {
+  let { code } = _compile(template, {
+    ...options,
+    mode: 'module',
+    prefixIdentifiers: true,
+  })
+  return code
+}
+
+/**
+ * Ensure all slot functions are wrapped with `withVaporCtx`
+ * which sets the `currentInstance` to owner when rendering
+ * the slot.
+ */
+describe('scopeId compiler support', () => {
+  test('should wrap default slot', () => {
+    const code = compile(`<Child><div/></Child>`)
+    expect(code).toMatch(`"default": _withVaporCtx(() => {`)
+    expect(code).toMatchSnapshot()
+  })
+
+  test('should wrap named slots', () => {
+    const code = compile(
+      `<Child>
+        <template #foo="{ msg }">{{ msg }}</template>
+        <template #bar><div/></template>
+        </Child>
+        `,
+      {
+        mode: 'module',
+        scopeId: 'test',
+      },
+    )
+    expect(code).toMatch(`"foo": _withVaporCtx((_slotProps0) => {`)
+    expect(code).toMatch(`"bar": _withVaporCtx(() => {`)
+    expect(code).toMatchSnapshot()
+  })
+
+  test('should wrap dynamic slots', () => {
+    const code = compile(
+      `<Child>
+        <template #foo v-if="ok"><div/></template>
+        <template v-for="i in list" #[i]><div/></template>
+      </Child>
+        `,
+      {
+        mode: 'module',
+        scopeId: 'test',
+      },
+    )
+    expect(code).toMatch(/name: "foo",\s+fn: _withVaporCtx\(/)
+    expect(code).toMatch(/name: i,\s+fn: _withVaporCtx\(/)
+    expect(code).toMatchSnapshot()
+  })
+})
