ba68414e80bc25228727411580acd3de18b523bc
xienan <1633875789@qq.com>
2025-12-01 10:46:58 +0800

    test: move issue 13935 test to setupHelpers.test-d.ts

--- packages-private/dts-test/setupHelpers.test-d.ts
+++ packages-private/dts-test/setupHelpers.test-d.ts
@@ -632,3 +632,22 @@ describe('defineOptions', () => {
     expose: ['expose'],
   })
 })
+
+describe('defineEmits w/ type declaration (Issue #13935)', () => {
+  const emit = defineEmits<{
+    open: [payload: number]
+    close: [payload: string]
+  }>()
+
+  // Correct usages
+  emit('open', 123)
+  emit('close', 'abc')
+
+  // Incorrect usages (should fail type check)
+  // @ts-expect-error
+  emit('open', 'string')
+  // @ts-expect-error
+  emit('close', 123)
+  // @ts-expect-error
+  emit('unknown', 123)
+})
