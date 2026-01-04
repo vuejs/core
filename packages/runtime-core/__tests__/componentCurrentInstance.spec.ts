4dc72d029eacdc7e4fbab0fc77dd1d4c2a645eea
kazuya kawaguchi <kawakazu80@gmail.com>
2026-01-04 12:46:23 +0900

    fix(runtime-core): support `uid` key for `useInstanceOption`

---
+++ packages/runtime-core/__tests__/componentCurrentInstance.spec.ts
@@ -0,0 +1,82 @@
+import { h, nodeOps, render, useInstanceOption } from '@vue/runtime-test'
+
+describe('useInstanceOption', () => {
+  test(`'ce' key`, () => {
+    let hasInstance: boolean | undefined
+    let ce: any
+    const Comp = {
+      setup() {
+        const option = useInstanceOption('ce', true)
+        hasInstance = option.hasInstance
+        ce = option.value
+        return () => null
+      },
+    }
+    render(h(Comp), nodeOps.createElement('div'))
+    expect(hasInstance).toBe(true)
+    // custom element definition is not supported in test renderer, so check to access it is enough
+    expect(ce).toBeUndefined()
+  })
+
+  test(`'type' key`, () => {
+    let hasInstance: boolean | undefined
+    let type: any
+    const Comp = {
+      setup() {
+        const option = useInstanceOption('type', true)
+        hasInstance = option.hasInstance
+        type = option.value
+        return () => null
+      },
+      mounted() {},
+    }
+    render(h(Comp), nodeOps.createElement('div'))
+    expect(hasInstance).toBe(true)
+    expect('setup' in type).toBe(true)
+    expect('mounted' in type).toBe(true)
+  })
+
+  test(`'uid' key`, () => {
+    let hasInstance: boolean | undefined
+    let uid: any
+    const Comp = {
+      setup() {
+        const option = useInstanceOption('uid', true)
+        hasInstance = option.hasInstance
+        uid = option.value
+        return () => null
+      },
+    }
+    render(h(Comp), nodeOps.createElement('div'))
+    expect(hasInstance).toBe(true)
+    expect(typeof uid).toBe('number')
+  })
+
+  test('not allowed key', () => {
+    let hasInstance: boolean | undefined
+    let value: any
+    const Comp = {
+      setup() {
+        const option = useInstanceOption('foo' as any, true)
+        hasInstance = option.hasInstance
+        value = option.value
+        return () => null
+      },
+    }
+    render(h(Comp), nodeOps.createElement('div'))
+    expect(hasInstance).toBe(true)
+    expect(value).toBeUndefined()
+    expect(
+      `useInstanceOption only accepts  'ce', 'type', 'uid' as key, got 'foo'.`,
+    ).toHaveBeenWarned()
+  })
+
+  test('not active instance', () => {
+    const { hasInstance, value } = useInstanceOption('type')
+    expect(hasInstance).toBe(false)
+    expect(value).toBeUndefined()
+    expect(
+      'useInstanceOption called without an active component instance.',
+    ).toHaveBeenWarned()
+  })
+})
