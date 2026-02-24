18a7e35ab8eabdc070ab863ea1cfe32d53d4cb82
daiwei <daiwei521@126.com>
2026-02-24 10:58:22 +0800

    fix(server-renderer): render className as escaped string

--- packages/server-renderer/src/helpers/ssrRenderAttrs.ts
+++ packages/server-renderer/src/helpers/ssrRenderAttrs.ts
@@ -42,10 +42,14 @@ export function ssrRenderAttrs(
     const value = props[key]
     // force as attribute
     if (key.startsWith('^')) key = key.slice(1)
-    if (key === 'class' || key === 'className') {
+    if (key === 'class') {
       ret += ` class="${ssrRenderClass(value)}"`
     } else if (key === 'style') {
       ret += ` style="${ssrRenderStyle(value)}"`
+    } else if (key === 'className') {
+      // className should not go through ssrRenderClass which normalizes non-string
+      // values into strings. it should coerce directly into strings
+      ret += ` class="${escapeHtml(String(value))}"`
     } else {
       ret += ssrRenderDynamicAttr(key, value, tag)
     }
