70647b61b45a49580179fb4fd930ab75748f440d
edison <daiwei521@126.com>
2026-03-07 12:43:26 +0800

    fix(runtime-vapor): handle out-in transition with empty leaving branch (#14538)

---
+++ packages-private/vapor-e2e-test/transition/cases/mode/out-in-mode-with-if-else-if-empty-branch.vue
@@ -0,0 +1,21 @@
+<script setup vapor>
+import { ref } from 'vue'
+
+const n = ref(1)
+
+function addN() {
+  n.value = (n.value + 1) % 3
+}
+</script>
+
+<template>
+  <div class="out-in-if-else-if-empty">
+    <button @click="addN">addN - {{ n }}</button>
+    <div>
+      <Transition mode="out-in">
+        <div v-if="n == 0">text1</div>
+        <div v-else-if="n == 1">text2</div>
+      </Transition>
+    </div>
+  </div>
+</template>
