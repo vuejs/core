6aa20457b123e8cdb79e550629814c6b9d85aa16
Jack <1074489519@qq.com>
2026-03-15 00:32:53 +0800

    test(runtime-vapor): add transition-group test

---
+++ packages-private/vapor-e2e-test/transition-group/cases/vapor-transition-group/with-v-if.vue
@@ -0,0 +1,17 @@
+<script setup vapor>
+import { ref } from 'vue'
+const show = ref(false)
+const list = ref([0, 1])
+</script>
+
+<template>
+  <div class="with-v-if">
+    <button @click="show = !show">ToggleShow</button>
+
+    <div>
+      <TransitionGroup tag="ul">
+        <li v-if="show" v-for="value in list" :key="value">{{ value }}</li>
+      </TransitionGroup>
+    </div>
+  </div>
+</template>
