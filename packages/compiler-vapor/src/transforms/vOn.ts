bf7a0666253697a6dfbd92c34a4047c7328174d5
edison <daiwei521@126.com>
2026-03-23 09:21:50 +0800

    fix(compiler-vapor): avoid delegating same-event handlers when sibling uses `stop` modifiers (#14610)
    close #14609

--- packages/compiler-vapor/src/transforms/vOn.ts
+++ packages/compiler-vapor/src/transforms/vOn.ts
@@ -1,13 +1,16 @@
 import {
+  type ElementNode,
   ElementTypes,
   ErrorCodes,
+  NodeTypes,
+  type SimpleExpressionNode,
   createCompilerError,
   isKeyboardEvent,
   isStaticExp,
+  resolveModifiers,
 } from '@vue/compiler-dom'
 import type { DirectiveTransform } from '../transform'
 import { IRNodeTypes, type KeyOverride, type SetEventIRNode } from '../ir'
-import { resolveModifiers } from '@vue/compiler-dom'
 import { extend, makeMap } from '@vue/shared'
 import { resolveExpression } from '../utils'
 import { EMPTY_EXPRESSION } from './utils'
@@ -47,19 +50,16 @@ export const transformVOn: DirectiveTransform = (dir, node, context) => {
     if (keyOverride) {
       // TODO error here
     }
-    if (isStaticClick) {
-      arg = extend({}, arg, { content: 'mouseup' })
-    } else if (!arg.isStatic) {
+    if (!isStaticClick && !arg.isStatic) {
       keyOverride = ['click', 'mouseup']
     }
   }
   if (nonKeyModifiers.includes('right')) {
-    if (isStaticClick) {
-      arg = extend({}, arg, { content: 'contextmenu' })
-    } else if (!arg.isStatic) {
+    if (!isStaticClick && !arg.isStatic) {
       keyOverride = ['click', 'contextmenu']
     }
   }
+  arg = normalizeStaticEventArg(arg, nonKeyModifiers)
 
   // don't gen keys guard for non-keyboard events
   // if event name is dynamic, always wrap with keys guard
@@ -88,9 +88,13 @@ export const transformVOn: DirectiveTransform = (dir, node, context) => {
   // Only delegate if:
   // - no dynamic event name
   // - no event option modifiers (passive, capture, once)
+  // - no handlers for the same static event on this element that use .stop
   // - is a delegatable event
   const delegate =
-    arg.isStatic && !eventOptionModifiers.length && delegatedEvents(arg.content)
+    arg.isStatic &&
+    !eventOptionModifiers.length &&
+    !hasStopHandlerForStaticEvent(node, arg.content) &&
+    delegatedEvents(arg.content)
 
   const operation: SetEventIRNode = {
     type: IRNodeTypes.SET_EVENT,
@@ -108,4 +112,51 @@ export const transformVOn: DirectiveTransform = (dir, node, context) => {
   }
 
   context.registerEffect([arg], operation)
+}
+
+function normalizeStaticEventArg(
+  arg: SimpleExpressionNode,
+  nonKeyModifiers: string[],
+): SimpleExpressionNode {
+  if (!arg.isStatic) return arg
+
+  let normalized = arg
+  const isStaticClick = arg.content.toLowerCase() === 'click'
+
+  if (nonKeyModifiers.includes('middle') && isStaticClick) {
+    normalized = extend({}, normalized, { content: 'mouseup' })
+  }
+  if (nonKeyModifiers.includes('right') && isStaticClick) {
+    normalized = extend({}, normalized, { content: 'contextmenu' })
+  }
+
+  return normalized
+}
+
+function hasStopHandlerForStaticEvent(node: ElementNode, eventName: string) {
+  return node.props.some(prop => {
+    if (
+      prop.type !== NodeTypes.DIRECTIVE ||
+      prop.name !== 'on' ||
+      !prop.arg ||
+      prop.arg.type !== NodeTypes.SIMPLE_EXPRESSION
+    ) {
+      return false
+    }
+
+    const arg = resolveExpression(prop.arg)
+    if (!arg.isStatic) return false
+
+    const { nonKeyModifiers } = resolveModifiers(
+      `on${arg.content}`,
+      prop.modifiers,
+      null,
+      prop.loc,
+    )
+
+    return (
+      nonKeyModifiers.includes('stop') &&
+      normalizeStaticEventArg(arg, nonKeyModifiers).content === eventName
+    )
+  })
 }
