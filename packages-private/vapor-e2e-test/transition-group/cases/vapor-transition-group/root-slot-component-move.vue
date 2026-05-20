b81843533bf35a0b2ddb36fb9f02a2af60d980e6
daiwei <daiwei521@126.com>
2026-05-20 17:06:16 +0800

    fix(runtime-vapor): track root slot updates in TransitionGroup

---
+++ packages-private/vapor-e2e-test/transition-group/cases/vapor-transition-group/root-slot-component-move.vue
@@ -0,0 +1,20 @@
+<script setup vapor>
+import { ref } from 'vue'
+import RootSlot from '../../components/RootSlot.vue'
+
+const items = ref(['a', 'b', 'c'])
+const moveClick = () => (items.value = ['d', 'b', 'a'])
+</script>
+
+<template>
+  <div class="root-slot-component-move">
+    <button @click="moveClick">root slot button</button>
+    <div>
+      <transition-group name="group">
+        <RootSlot>
+          <div v-for="item in items" :key="item" class="test">{{ item }}</div>
+        </RootSlot>
+      </transition-group>
+    </div>
+  </div>
+</template>
