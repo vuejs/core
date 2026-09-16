b651ddb382196a64ca95fde9dc40d28a22c6879e
Valentin <115924965+ValentinYoushkevich@users.noreply.github.com>
2026-09-16 05:40:12 +0300

    fix(runtime-vapor): cache current KeepAlive branch when include/exclude changes (#15525)

--- packages/runtime-vapor/__tests__/components/KeepAlive.spec.ts
+++ packages/runtime-vapor/__tests__/components/KeepAlive.spec.ts
@@ -1594,9 +1594,9 @@ describe('VaporKeepAlive', () => {
   })
 
   describe('cache invalidation', () => {
-    function setup() {
+    function setup(include = 'one,two') {
       const viewRef = ref('one')
-      const includeRef = ref('one,two')
+      const includeRef = ref(include)
       define({
         setup() {
           return createComponent(
@@ -1611,9 +1611,9 @@ describe('VaporKeepAlive', () => {
       return { viewRef, includeRef }
     }
 
-    function setupExclude() {
+    function setupExclude(exclude = '') {
       const viewRef = ref('one')
-      const excludeRef = ref('')
+      const excludeRef = ref(exclude)
       define({
         setup() {
           return createComponent(
@@ -1696,6 +1696,47 @@ describe('VaporKeepAlive', () => {
       assertHookCalls(oneHooks, [1, 1, 2, 1, 0])
       // two should be pruned
       assertHookCalls(twoHooks, [1, 1, 1, 1, 1])
+    })
+
+    test('on include change matching current view', async () => {
+      const { viewRef, includeRef } = setup('two')
+
+      includeRef.value = 'one,two'
+      await nextTick()
+      viewRef.value = 'two'
+      await nextTick()
+      assertHookCalls(oneHooks, [1, 1, 0, 1, 0])
+
+      viewRef.value = 'one'
+      await nextTick()
+      assertHookCalls(oneHooks, [1, 1, 1, 1, 0])
+    })
+
+    test('on exclude change matching current view', async () => {
+      const { viewRef, excludeRef } = setupExclude('one')
+
+      excludeRef.value = ''
+      await nextTick()
+      viewRef.value = 'two'
+      await nextTick()
+      assertHookCalls(oneHooks, [1, 1, 0, 1, 0])
+
+      viewRef.value = 'one'
+      await nextTick()
+      assertHookCalls(oneHooks, [1, 1, 1, 1, 0])
+    })
+
+    test('on include change matching current view + view switch', async () => {
+      const { viewRef, includeRef } = setup('two')
+
+      includeRef.value = 'one,two'
+      viewRef.value = 'two'
+      await nextTick()
+      assertHookCalls(oneHooks, [1, 1, 0, 0, 1])
+
+      viewRef.value = 'one'
+      await nextTick()
+      assertHookCalls(oneHooks, [2, 2, 1, 0, 1])
     })
 
     test('should not prune current active instance', async () => {
@@ -4745,5 +4786,34 @@ describe('VaporKeepAlive', () => {
     await nextTick()
     expect(host.textContent).toContain('A')
     expect(onEnter).toHaveBeenCalledTimes(1)
+  })
+
+  test('should unmount a deactivated component cached after an include change', async () => {
+    const data = ref({ include: 'two', view: 'one' })
+    const App = compile(
+      `<template>
+        <KeepAlive :include="data.include">
+          <component :is="components[data.view]" />
+        </KeepAlive>
+      </template>`,
+      data,
+      views,
+    )
+    const { app, host } = define(App).render()
+    expect(host.textContent).toBe('one')
+    expect(oneHooks.mounted).toHaveBeenCalledTimes(1)
+
+    data.value.include = 'one,two'
+    await nextTick()
+
+    data.value.view = 'two'
+    await nextTick()
+    expect(host.textContent).toBe('two')
+    expect(oneHooks.deactivated).toHaveBeenCalledTimes(1)
+    expect(oneHooks.unmounted).not.toHaveBeenCalled()
+
+    app.unmount()
+    await nextTick()
+    expect(oneHooks.unmounted).toHaveBeenCalledTimes(1)
   })
 })
